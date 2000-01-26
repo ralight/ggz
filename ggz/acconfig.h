75e3bf4b53912d71963615662e6eca4510005d54
bmh <devnull@localhost>
2000-01-26 08:59:31 -0800

    [svn] - Added support files (no source yet)

---
+++ ggz/acconfig.h
@@ -0,0 +1,38 @@
+/* acconfig.h
+   This file is in the public domain.
+
+   Descriptive text for the C preprocessor macros that
+   the distributed Autoconf macros can define.
+   No software package will use all of them; autoheader copies the ones
+   your configure.in uses into your configuration header file templates.
+
+   The entries are in sort -df order: alphabetical, case insensitive,
+   ignoring punctuation (such as underscores).  Although this order
+   can split up related entries, it makes it easier to check whether
+   a given entry is in the file.
+
+   Leave the following blank line there!!  Autoheader needs it.  */
+
+
+/* defined by automake */
+
+#undef HAVE_LIBHISTORY
+#undef WAIT_ANY
+#undef DEBUG
+#undef DEBUG_SCREEN
+#undef DEBUG_SOCKET
+#undef DEBUG_TAUNT
+#undef DEBUG_MEM
+#undef DEBUG_BID
+#undef DEBUG_PLAY
+#undef PACKAGE_LOCALE_DIR
+#undef PACKAGE_DATA_DIR
+#undef PACKAGE_SOURCE_DIR
+#undef SYSCONFDIR
+#undef _XOPEN_SOURCE
+    
+
+/* Leave that blank line there!!  Autoheader needs it.
+   If you're adding to this file, keep in mind:
+   The entries are in sort -df order: alphabetical, case insensitive,
+   ignoring punctuation (such as underscores).  */
