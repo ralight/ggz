99fe3e7d119f6377e313bf230406fe543ae74fef
rgade <devnull@localhost>
2000-05-08 08:59:49 -0700

    [svn] Convert the square size (from opt dialog) to dot size before sending to server

--- ggz/game_clients/dots/cb_opt.c
+++ ggz/game_clients/dots/cb_opt.c
@@ -23,8 +23,13 @@ on_opt_btn_ok_clicked                  (GtkButton       *button,
 	sb_x = gtk_object_get_data(GTK_OBJECT(opt_dialog), "opt_spin_x");
 	sb_y = gtk_object_get_data(GTK_OBJECT(opt_dialog), "opt_spin_y");
 
+	/* Get the number of squares wide and high */
 	board_width = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(sb_x));
 	board_height = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(sb_y));
+
+	/* Convert from squares to dots */
+	board_width++;
+	board_height++;
 
 	allow_dlg_opt_delete = TRUE;
 
@@ -40,6 +45,7 @@ on_dlg_opt_delete_event                (GtkWidget       *widget,
                                         GdkEvent        *event,
                                         gpointer         user_data)
 {
+	/* Do not allow this dialog to close before we've sent the options */
 	if(allow_dlg_opt_delete)
 		return FALSE;
 	else
