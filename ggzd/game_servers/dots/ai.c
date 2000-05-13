91598d2bc750293c016d2f92c85a2cee9f346fa5
rgade <devnull@localhost>
2000-05-13 00:45:13 -0700

    [svn] - AI skill upgraded to medium, now gives away least possible number of squares   if unable to make a safe move (1 move lookahead)

--- ggzd/game_servers/dots/ai.c
+++ ggzd/game_servers/dots/ai.c
@@ -27,15 +27,18 @@
 #include <ggz.h>
 #include <game.h>
 #include <protocols.h>
+#include <string.h>
 
 /* Global game variables */
 struct dots_game_t dots_game;
 
 
 /* Private functions */
-static unsigned char ai_find_random_line(unsigned char *, unsigned char *);
 static char ai_find_scoring_line(unsigned char *, unsigned char *);
 static char ai_find_safe_line(unsigned char *, unsigned char *);
+static char ai_find_best_giveaway(unsigned char *, unsigned char *);
+static char ai_find_random_line(unsigned char *, unsigned char *);
+static unsigned char ai_lookahead(unsigned char, unsigned char, unsigned char);
 static void ai_handle_move(int, unsigned char *, unsigned char *);
 
 
@@ -44,17 +47,22 @@ unsigned char ai_move(unsigned char *x, unsigned char *y)
 	char dir;
 
 	/* Find a score first */
+	ggz_debug("AI: Looking for a score");
 	dir = ai_find_scoring_line(x, y);
 
 	/* If that fails... */
-	if(dir == -1)
+	if(dir == -1) {
 		/* Find a safe move */
+		ggz_debug("AI: Looking for a safe move");
 		dir = ai_find_safe_line(x, y);
+	}
 
-	/* If all else fails... */
-	if(dir == -1)
-		/* Move like a dumdum */
-		dir = ai_find_random_line(x, y);
+	/* If we have no safe moves... */
+	if(dir == -1) {
+		/* Find the one that gives the least squares away */
+		ggz_debug("AI: Looking for least painful move");
+		dir = ai_find_best_giveaway(x, y);
+	}
 
 	ai_handle_move(dir, x, y);
 
@@ -222,8 +230,99 @@ static char ai_find_safe_line(unsigned char *x, unsigned char *y)
 }
 
 
+/* This chooses the move that gives away the fewest squares */
+static char ai_find_best_giveaway(unsigned char *x, unsigned char *y)
+{
+	unsigned char try_dir, try_x, try_y, try_squares;
+	unsigned char best_dir=0, best_x=0, best_y=0, best_squares=255;
+
+	try_dir = 0;
+	for(try_x=0; try_x<dots_game.board_width; try_x++)
+		for(try_y=0; try_y<dots_game.board_height-1; try_y++) {
+			if(!dots_game.vert_board[try_x][try_y]) {
+				try_squares = ai_lookahead(try_dir,try_x,try_y);
+				if(try_squares < best_squares) {
+					best_dir = try_dir;
+					best_x = try_x;
+					best_y = try_y;
+					best_squares = try_squares;
+				}
+				/* If only 1 square, it's as good as it gets */
+				if(best_squares == 1)
+					goto accept_move;
+			}
+		}
+
+	try_dir = 1;
+	for(try_x=0; try_x<dots_game.board_width-1; try_x++)
+		for(try_y=0; try_y<dots_game.board_height; try_y++) {
+			if(!dots_game.horz_board[try_x][try_y]) {
+				try_squares = ai_lookahead(try_dir,try_x,try_y);
+				if(try_squares < best_squares) {
+					best_dir = try_dir;
+					best_x = try_x;
+					best_y = try_y;
+					best_squares = try_squares;
+				}
+				/* If only 1 square, it's as good as it gets */
+				if(best_squares == 1)
+					goto accept_move;
+			}
+		}
+
+accept_move:
+	*x = best_x;
+	*y = best_y;
+	return best_dir;
+}
+
+
+/* This looks ahead at all opponent moves to determine how */
+/* many squares we will end up giving away with this move  */
+static unsigned char ai_lookahead(unsigned char dir,
+				  unsigned char x, unsigned char y)
+{
+	unsigned char squares = 0;
+	unsigned char vb_backup[MAX_BOARD_WIDTH][MAX_BOARD_HEIGHT-1];
+	unsigned char hb_backup[MAX_BOARD_WIDTH-1][MAX_BOARD_HEIGHT];
+	char try_dir = 0;
+	unsigned char try_x, try_y;
+
+	/* First we backup the game board so we can restore our state */
+	memcpy(vb_backup, dots_game.vert_board, sizeof(vb_backup));
+	memcpy(hb_backup, dots_game.horz_board, sizeof(hb_backup));
+
+	/* Pretend we've made our move */
+	if(dir == 0)
+		dots_game.vert_board[x][y] = 1;
+	else
+		dots_game.horz_board[x][y] = 1;
+
+	/* Count all moves which generate a score, keep */
+	/* going until all scoring is done              */
+	while(try_dir != -1) {
+		try_dir = ai_find_scoring_line(&try_x, &try_y);
+		if(try_dir == 0)
+			dots_game.vert_board[try_x][try_y] = 1;
+		else if(try_dir == 1)
+			dots_game.horz_board[try_x][try_y] = 1;
+		if(try_dir != -1)
+			squares++;
+	}
+
+	/* Restore the board state to normal */
+	memcpy(dots_game.vert_board, vb_backup, sizeof(vb_backup));
+	memcpy(dots_game.horz_board, hb_backup, sizeof(hb_backup));
+
+	/* Return the number of squares we would give away with this move */
+	ggz_debug("AI: Considered %s move at %d, %d (%d squares)",
+		  dir == 0 ? "vertical" : "horizontal", x, y, squares);
+	return squares;
+}
+
+
 /* This chooses a random line to draw */
-static unsigned char ai_find_random_line(unsigned char *x, unsigned char *y)
+static char ai_find_random_line(unsigned char *x, unsigned char *y)
 {
 	unsigned char try_dir, try_x, try_y;
 	char not_valid = 1;
