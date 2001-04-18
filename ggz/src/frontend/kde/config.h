dfc6cbe9ffe851d41795a93b75c212f3db22072b
dr_maux <devnull@localhost>
2001-04-18 13:07:35 -0700

    [svn] The third one. Hm, I must have been coding :)

--- ggz/src/frontend/kde/config.h
+++
@@ -1,5 +0,0 @@
-// Configuration for KGGZ
-
-// instdir:
-// installation directory of kggz, with trailing slash
-#define instdir "/usr/local/share/kggz/"
--- ggz/src/frontend/kde/kggz_internal.cpp
+++
@@ -1,9 +0,0 @@
-/* Class declaration */
-#include "kggz_internal.h"
-
-/* System includes */
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
-// TODO !!!!
