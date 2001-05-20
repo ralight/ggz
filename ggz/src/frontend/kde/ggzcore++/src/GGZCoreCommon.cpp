60062e65823c3d1d312709aaf006a91bc7bac782
dr_maux <devnull@localhost>
2001-05-20 07:06:30 -0700

    [svn] - forgot new file

---
+++ ggz/src/frontend/kde/ggzcore++/src/GGZCoreCommon.cpp
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////////
+//                                                                 ///
+// GGZCore++ - C++ API wrapper for the ggzcore library              ///
+// (C) 2001 Josef Spillner                                           ///
+// dr_maux@users.sourceforge.net                                      ///
+// The MindX Open Source Project                                      ///
+// http://mindx.sourceforge.net/                                     ///
+//                                                                  ///
+// This library may be distributed under the terms of the GNU GPL. ///
+//                                                                ///
+////////////////////////////////////////////////////////////////////
+
+///// GGZ Common /////////////////////////////////////////////////
+
+#include "GGZCoreCommon.h"
+
+#ifdef USE_DEBUGDUMMY
+int debugdummy(const char *x, ...){return 0;}
+#endif
+
