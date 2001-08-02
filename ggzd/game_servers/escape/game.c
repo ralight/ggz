7fe2d7f564c4cecef4cf8fd48b2309a08139fb50
oojah <devnull@localhost>
2001-08-02 06:50:35 -0700

    [svn] Another repeat move fix attempt.

--- ggzd/game_servers/escape/game.c
+++ ggzd/game_servers/escape/game.c
@@ -291,7 +291,7 @@ int game_send_players(void)
 /* Send out move for player: num */
 int game_send_move(int num, int event, char direction)
 {
-	int fd = ggz_seats[(escape_game.turn+1)%2].fd;
+	int fd = ggz_seats[(num+1)%2].fd;
 	int i;
 
 	ggz_debug("game_send_move(%d, %d, %d)\n",num, event, direction);	
@@ -301,7 +301,7 @@ int game_send_move(int num, int event, char direction)
 		return 0;
 
 	ggz_debug("\tSending player %d's move to player %d",
-		   num, (escape_game.turn+1)%2);
+		   num, (num+1)%2);
 
 	if(es_write_int(fd, ESCAPE_MSG_MOVE) < 0
 	   || es_write_char(fd, direction) < 0){
