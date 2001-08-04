9f578c918f33578ec24423457b767ad806a5cb0b
dr_maux <devnull@localhost>
2001-08-03 20:03:01 -0700

    [svn] - autodownload XML files off the web

--- utils/guru/data/rdf.pl
+++ utils/guru/data/rdf.pl
@@ -1,19 +1,54 @@
 #!/usr/bin/perl
 
-if($ARGV[0] ne "news"){
+use Net::Telnet;
+
+if($ARGV[1] ne "news"){
 	exit;
+}
+if($ARGV[2] eq ""){
+	print "From what news source my friend?\n";
+	exit();
 }
 
 sub xml{
-	$arg = shift(@_);
+	my $arg = shift(@_);
 	@ar1 = split(/\>/, $arg);
 	@ar2 = split(/\</, $ar1[1]);
 	$ret = $ar2[0];
 	return $ret;
 }
 
+sub download
+{
+	$host = shift(@_);
+	$document = shift(@_);
+
+	$conn = new Net::Telnet(Host => $host, Port => 80);
+	$conn->print("GET $document\n");
+	$data = $conn->get(Timeout => 5);
+	open(FILE, ">$ARGV[2].rdf");
+	print FILE "$data\n";
+	close(FILE);
+	$downloaded = 1;
+}
+
+$downloaded = 0;
+if($ARGV[2] eq "slashdot"){download("slashdot.org", "/slashdot.rdf");}
+if($ARGV[2] eq "register"){download("theregister.co.uk", "/tonys/slashdot.rdf");}
+if($ARGV[2] eq "happypenguin"){download("happypenguin.org", "/html/news.rdf");}
+if($ARGV[2] eq "heise"){download("heise.de", "/newsticker/heise.rdf");}
+
+if(!$downloaded){
+	print "News source not available.\n";
+	exit();
+}
+
 $r = rand(100) % 10 + 2;
-open(FILE, "../data/slashdot.rdf") or die "File not found!\n";
+$ret = open(FILE, $ARGV[2] . ".rdf");
+if(!$ret){
+	print "Sorry, no more news on $ARGV[2].\n";
+	exit();
+}
 while(<FILE>){
 	if($_ =~ /\<title\>/){
 		$title = xml($_);
