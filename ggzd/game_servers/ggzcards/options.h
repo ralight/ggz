865fe53653b1aadcfee210e8bed99895b73c195a
jdorje <devnull@localhost>
2001-08-23 02:02:56 -0700

    [svn] Added yet more comments to options.h.
    Now if only all my code was this documented...

--- ggzd/game_servers/ggzcards/options.h
+++ ggzd/game_servers/ggzcards/options.h
@@ -4,7 +4,7 @@
  * Project: GGZCards Server
  * Date: 07/06/2001
  * Desc: Functions and data for game options system
- * $Id: options.h 2191 2001-08-23 08:26:46Z jdorje $
+ * $Id: options.h 2195 2001-08-23 09:02:56Z jdorje $
  *
  * GGZCards has a rather nifty option system.  Each option has a name as
  * its "key".  Each option has a certain number of possible values, in
@@ -38,7 +38,8 @@
 
 /** @return TRUE iff all options have been set.
  *  Options are set after get_options is called and then (if
- *  necessary) handle_options handle's the client's response. */
+ *  necessary) handle_options handle's the client's response.
+ *  @note Only needed by common code. */
 int options_set();
 
 /** Reads a list of options from the client as sent by the
@@ -46,6 +47,7 @@ int options_set();
  *  @param num_options the number of options in the list
  *  @param options array options are placed in
  *  @return 0 on success, -1 on failure
+ *  @note Only called by common code.
  *  @see handle_options */
 int rec_options(int num_options, int *options);
 
@@ -53,12 +55,14 @@ int rec_options(int num_options, int *options);
  *  @param key the key string for the option
  *  @param num the number of values [0..num-1] the option can have
  *  @param dflt the default value of the option
- *  @param ... the list of option strings, one for each value */
+ *  @param ... the list of option strings, one for each value
+ *  @see game_get_options */
 void add_option(char *key, int num, int dflt, ...);
 
 /** sets an option
  *  @param key the key string of the option
- *  @value the value to which the option shall be set */
+ *  @value the value to which the option shall be set
+ *  @note Only called by common code. */
 void set_option(char *key, int value);
 
 /** Request the current list of options from the client.
@@ -66,14 +70,17 @@ void set_option(char *key, int value);
  *  what options to use.  Options that have already been set
  *  are not asked about.
  *  @return Returns (void) immediately, upon success or failure.
- *  @note If there are no pending options, no request is sent. */
+ *  @note If there are no pending options, no request is sent.
+ *  @note Only called by common code. */
 void get_options();
 
 /** Receives options from the host client and puts them in the option list.
  *  Should be called when an option message is received in response
- *  to a get_options() option request. */
+ *  to a get_options() option request.
+ *  @note Only called by common code. */
 void handle_options();
 
 /** Calls the game's option handler for each option.  Must be
- *  called manually at the start of a game. */
+ *  called manually at the start of a game.
+ *  @note Only called by common code. */
 void finalize_options();
