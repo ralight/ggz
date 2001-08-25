046722ccb5f1e3bdee281265c52d1aeec3442048
jdorje <devnull@localhost>
2001-08-25 14:30:32 -0700

    [svn] Added a lot more doxygen-style documentation.  Even without doxygen, having such documentation in the header file is probably a good idea.

--- ggz-client-libs/ggzmod/ggz_client.h
+++ ggz-client-libs/ggzmod/ggz_client.h
@@ -3,7 +3,7 @@
  * Author: GGZ Development Team
  * Project: GGZMod library
  * Desc: GGZ game module functions
- * $Id: ggz_client.h 2247 2001-08-25 19:57:49Z jdorje $
+ * $Id: ggz_client.h 2249 2001-08-25 21:30:32Z jdorje $
  *
  * Copyright (C) 2000 GGZ devel team
  *
@@ -24,48 +24,84 @@
 
 /**
  * @file ggz_client.h
- * @brief Interface for ggzmod library.
+ * @brief The interface for the ggzmod library used by game clients.
  *
  * This file contains all libggzmod functions used by game clients to
  * interface with GGZ.  Just include ggz_client.h and make sure your program
  * is linked with libggzmod.  Then use the functions below as appropriate.
+ *
+ * Here is an example of how a GTK client may connect:
+ * @code
+ *     int fd;
+ *
+ *     // this function reads input from the socket (server) and handles it
+ *     void game_handle_io(gpointer data, gint source, GdkInputCondition cond) {
+ *         ...
+ *     }
+ *
+ *     int main(int argc, char** argv) {
+ *         fd = ggz_connect();      // connect to GGZ
+ *         if (fd < 0) return -1;
+ *         gtk_init(&argc, &argv);
+ *         gdk_input_add(game.fd, GDK_INPUT_READ, game_handle_io, NULL);
+ *         gtk_main();
+ *         ggz_disconnect();            // disconnect from GGZ
+ *         return 0;
+ *     }
+ * @endcode
+ *
+ * For more information, see the documentation at http://ggz.sf.net/.
+ *
+ * @todo There should be some example code showing how to use ggzmod.
  */
 
 #ifndef __GGZ_CLIENT_GGZ_H
 #define __GGZ_CLIENT_GGZ_H
 
 /**
- * This does the physical work of connecting the game client to the GGZ client
- * (which will then relay information through the ggz server to your game 
- * server).  It should be called by your game at the appropriate time.
+ * @brief Connects to GGZ.
+ *
+ * This does the physical work of connecting to GGZ.  It should be called
+ * by the game client upon startup.
  *
  * @return The file descriptor for the TCP communications socket (or -1 on failure).
+ * @see ggz_get_sock
+ * @todo Should it just return 0 on success, -1 on failure?
  */
 int ggz_connect(void);
 
 /**
- * This destroys all of ggzmod's internal data.
+ * @brief Disconnects from GGZ.
+ *
+ * This disconnects from GGZ and destroys all internal data.  It
+ * should be called by the game client before exiting.
  *
  * @return 0 on success; -1 on failure.
  */
 int ggz_disconnect(void);
 
 /**
- *  This returns the file descriptor of the TCP communications
- *  socket.  This socket can be used for two-way communication to
- *  the game server through TCP.
+ * @brief Returns the GGZ TCP communications socket.
+ *
+ * This returns the file descriptor of the TCP communications
+ * socket.  The socket can be used for two-way communication to
+ * the game server through TCP.
  *
  * @return The FD integer, or -1 on no connection.
  */
 int ggz_get_sock(void);
 
 /**
- *  This returns a file descriptor of a UDP communications
- *  socket.  This socket can be used for two-way communication to
- *  the game server through UDP.
+ * @brief Returns the GGZ UDP communications socket.
  *
- *  @return The FD integer, or -1 on no connection.
- *  @todo This functionality is not yet implemented. */
+ * This returns the file descriptor of the UDP communications
+ * socket.  The socket can be used for two-way communication to
+ * the game server through UDP.
+ *
+ * @return The FD integer, or -1 on no connection.
+ * @todo Is this a good way to access the UDP socket?
+ * @todo This functionality is not yet implemented; it will return -1.
+ */
 int ggz_get_udp_sock(void);
 
 #endif /* __GGZ_CLIENT_GGZ_H */
