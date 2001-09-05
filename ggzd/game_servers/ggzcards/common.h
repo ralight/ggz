a7462eb568357fe720b6c6955fef8992a40d0bd9
jdorje <devnull@localhost>
2001-09-05 09:15:11 -0700

    [svn] Some cleanup, reformatting, and restructuring of the header file data.

--- ggzd/game_servers/ggzcards/common.h
+++ ggzd/game_servers/ggzcards/common.h
@@ -1,10 +1,10 @@
-/*
+/* 
  * File: common.h
  * Author: Jason Short
  * Project: GGZCards Server
  * Date: 06/20/2001
  * Desc: Functions and data common to all games
- * $Id: common.h 2367 2001-09-05 06:57:10Z jdorje $
+ * $Id: common.h 2369 2001-09-05 16:15:11Z jdorje $
  *
  * This file contains code that controls the flow of a general
  * trick-taking game.  Game states, event handling, etc. are all
@@ -46,94 +46,113 @@
 
 /* GGZCards server game states */
 typedef enum {
-	WH_STATE_PRELAUNCH,	/* before the launch happens */
-	WH_STATE_NOTPLAYING,	/* no game started */
-	WH_STATE_WAITFORPLAYERS,	/* waiting for players */
-	WH_STATE_NEXT_HAND,	/* creating a new hand */
-	WH_STATE_FIRST_BID,	/* about to have the first bid */
-	WH_STATE_NEXT_BID,	/* asking for new bid */
-	WH_STATE_WAIT_FOR_BID,	/* waiting for a bid */
-	WH_STATE_FIRST_TRICK,	/* about to have the first trick of a hand */
-	WH_STATE_NEXT_TRICK,	/* time for the next trick */
-	WH_STATE_NEXT_PLAY,	/* asking for a new play */
-	WH_STATE_WAIT_FOR_PLAY	/* waiting for a play */
+	WH_STATE_PRELAUNCH,	/**< before the launch happens */
+	WH_STATE_NOTPLAYING,	/**< no game started */
+	WH_STATE_WAITFORPLAYERS,	/**< waiting for players; saved state */
+	WH_STATE_NEXT_HAND,	/**< ready to create a new hand */
+	WH_STATE_FIRST_BID,	/**< about to have the first bid */
+	WH_STATE_NEXT_BID,	/**< asking for new bid */
+	WH_STATE_WAIT_FOR_BID,	/**< waiting for a bid */
+	WH_STATE_FIRST_TRICK,	/**< about to have the first trick of a hand */
+	WH_STATE_NEXT_TRICK,	/**< time for the next trick */
+	WH_STATE_NEXT_PLAY,	/**< asking for a new play */
+	WH_STATE_WAIT_FOR_PLAY	/**< waiting for a play */
 } server_state_t;
 
 /* Data structure for generic trick-taking card game */
 struct wh_game_t {
-	game_type_t which_game;	/* the game; currently defined in cards.h */
-	struct game_function_pointers *funcs;	/* game-specific functions */
-	ai_type_t ai_type;	/* the type of AI we're using */
-	deck_type_t deck_type;	/* the type of deck used, as defined in cards.h */
-	char *name;		/* the name of the game */
-	char *rules_url;	/* the URL of where to read the game's rules */
+	/* Game meta-data */
+	game_type_t which_game;	/**< the type of game game */
+	struct game_function_pointers *funcs;	/**< an array of game-specific functions */
+	ai_type_t ai_type;	/**< the type of AI we're using */
+	deck_type_t deck_type;	/**< the type of deck used */
 
-	int initted;		/* has the game been initialized? */
-	player_t host;		/* the host of the table; cannot be an AI */
+	/* misc. game data */
+	char *name;		/**< the name of the game */
+	char *rules_url;	/**< the URL of where to read the game's rules */
 
-	server_state_t state;	/* the current state of the game (see WH_STATE, above) */
-	server_state_t saved_state;	/* any time while waiting, the state we _would_ be in if we weren't waiting */
+	/* misc. state data */
+	int initted;		/**< has the game been initialized? */
+	player_t host;		/**< the host of the table; cannot be an AI */
 
-	/* these next few are general game-specific options that are used by the game-independent code */
-	int must_overtrump;	/* if this is set, then a player must trump/overtrump if possible (i.e. La Pocha) */
-	int must_break_trump;	/* if this is set, then trump can't be lead until it's been broken (i.e. spades) */
-	int target_score;	/* after someone reaches this score, the game is over (if used) */
-	int last_trick;		/* should the last trick be sent to all the players? */
-	int last_hand;		/* should the last hand be sent to all the players? */
-	int cumulative_scores;	/* should the cumulative score be sent to all the players? */
-	int bid_history;	/* should a complete history of the hand's bidding be shown? */
+	/* state functions */
+	server_state_t state;	/**< the current state of the game */
+	server_state_t saved_state;	/**< any time while waiting, the state we _would_ be in if we weren't waiting */
 
-	int open_hands;		/* are we playing with open hands? */
+	/* these next few are general game-specific options that are used by
+	   the game-independent code */
+	int must_overtrump;	/**< if this is set, then a player must trump/overtrump if possible (i.e. La Pocha) */
+	int must_break_trump;	/**< if this is set, then trump can't be lead until it's been broken (i.e. Spades) */
+	int target_score;	/**< after someone reaches this score, the game is over (0=unused) */
+	int last_trick;		/**< should the last trick be sent to all the players? */
+	int last_hand;		/**< should the last hand be sent to all the players? */
+	int cumulative_scores;	/**< should the cumulative score be sent to all the players? */
+	int bid_history;	/**< should a complete history of the hand's bidding be shown? */
 
-	card_t lead_card;	/* the card that was lead this trick */
-	char trump;		/* the suit of trump; 0-3 or other for none */
+	/* Table options */
+	int open_hands;		/**< are we playing with open hands? */
 
-	int hand_num;		/* the number of the current hand (counting from ?) */
-	int hand_size;		/* the size of the hand */
-	int max_hand_length;	/* the largest possible hand */
-	player_t dealer;	/* who dealt this hand (starts at random, then rotates) */
+	/* State-tracking data: dealing */
+	int hand_num;		/**< the number of the current hand (counting
+				   from ?) */
+	int hand_size;		/**< the size of the hand */
+	int max_hand_length;	/**< the largest possible hand */
+	player_t dealer;	/**< who dealt this hand (starts at random,
+				   then rotates) */
 
-	int bid_count;		/* how many bids there have been this bidding cycle */
-	int bid_total;		/* how many bids there will be this bidding cycle */
-	int bid_rounds;		/* number of *rounds* of bidding */
-	int max_bid_rounds;	/* the maximum number of bid rounds we can hold (increased at need) */
-	player_t prev_bid;	/* who made the _previous_ bid? */
-	player_t next_bid;	/* current/next bidder */
+	/* State-tracking data: bidding */
+	int bid_count;		/**< how many bids there have been this bidding
+				   cycle */
+	int bid_total;		/**< how many bids there will be this bidding
+				   cycle */
+	int bid_rounds;		/**< number of *rounds* of bidding */
+	int max_bid_rounds;	/**< the maximum number of bid rounds we can
+				   hold (increased at need) */
+	player_t prev_bid;	/**< who made the _previous_ bid? */
+	player_t next_bid;	/**< current/next bidder */
 
-	int play_count;		/* how many plays there have been this trick */
-	int play_total;		/* how many plays there will be this trick */
-	player_t next_play;	/* current/next player */
-	player_t curr_play;	/* current player, tracked automatically by req_bid */
-	/* Note: the difference between these two is subtle, but important.  next_play is used
-	 * to track the player whose hand is being played from.  curr_play is used automatically
-	 * to remember who is supposed to be playing now.  In the case of bridge, next_play
-	 * will go around the table (0, 1, 2, 3) for each play.  However, when the dummy is
-	 * playing curr_play will point to the declarer, since they're the one we request the play
-	 * from. */
-	seat_t play_seat;	/* the seat being played from */
+	/* State-tracking data: playing */
+	card_t lead_card;	/**< the card that was lead this trick */
+	char trump;		/**< the suit of trump; 0-3 or other for none */
+	int play_count;		/**< how many plays there have been this trick */
+	int play_total;		/**< how many plays there will be this trick */
+	player_t next_play;	/**< current/next player */
+	player_t curr_play;	/**< current player, tracked automatically by req_bid */
+	/* Note: the difference between these two is subtle, but important.
+	   next_play is used to track the player whose hand is being played
+	   from.  curr_play is used automatically to remember who is supposed 
+	   to be playing now.  In the case of bridge, next_play will go
+	   around the table (0, 1, 2, 3) for each play.  However, when the
+	   dummy is playing curr_play will point to the declarer, since
+	   they're the one we request the play from. */
+	seat_t play_seat;	/**< the seat being played from */
 
-	player_t winner;	/* who won last trick */
-	player_t leader;	/* who leads next trick/lead this trick */
+	/* State-tracking data: tricks */
+	player_t winner;	/**< who won last trick */
+	player_t leader;	/**< who leads next trick/lead this trick */
+	int trick_count;	/**< how many tricks have been played this hand */
+	int trick_total;	/**< how many tricks there will be this hand */
+	int trump_broken;	/**< has trump been broken this hand?  See must_break_trump, above. */
 
-	int trick_count;	/* how many tricks have been played this hand */
-	int trick_total;	/* how many tricks there will be this hand */
-	int trump_broken;	/* has trump been broken this hand?  See must_break_trump, above. */
+	/* table data: players */
+	player_t num_players;	/**< the number of players in the game */
+	int player_count;	/**< the number of human players who have joined, in total */
+	struct game_player_t *players;	/**< data for each player, allocated in game_init */
 
-	player_t num_players;	/* the number of players in the game */
-	int player_count;	/* the number of human players who have joined, in total */
-	struct game_player_t *players;	/* data for each player, allocated in game_init */
+	/* table data: seats */
+	seat_t num_seats;	/**< the number of "seats" in the table (which includes fake non-players */
+	struct game_seat_t *seats;	/**< data for each seat, allocated in game_init */
 
-	seat_t num_seats;	/* the number of "seats" in the table (which includes fake non-players */
-	struct game_seat_t *seats;	/* data for each seat, allocated in game_init */
+	/* Global messages */
+	global_message_list_t *message_head;	/**< global message list head */
+	global_message_list_t *message_tail;	/**< global message list tail */
 
-	global_message_list_t *message_head;	/* global messages */
-	global_message_list_t *message_tail;
-
-	void *specific;		/* any extra game-specific data */
+	/* Game-specific data */
+	void *specific;		/**< any extra game-specific data */
 
 };
 
-/* convert an "absolute" seat number s to the
+/** convert an "absolute" seat number s to the
  * "relative" seat number used by player p */
 #define CONVERT_SEAT(s, p) (((s) - game.players[(p)].seat + game.num_seats) % game.num_seats)
 
@@ -185,8 +204,8 @@ extern struct wh_game_t game;
 
 
 /* accessor functions */
-/* I made these when switching over to a new ggzdmod.  I don't think
- * it's safe to access the data directly. */
+/* I made these when switching over to a new ggzdmod.  I don't think it's
+   safe to access the data directly. */
 const char *get_seat_name(seat_t s);
 ggzd_assign_t get_seat_status(seat_t s);
 
