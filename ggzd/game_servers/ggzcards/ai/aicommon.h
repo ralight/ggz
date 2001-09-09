51f4b0bcf43e8cb3f7e710a43baa7b9a082708c7
jdorje <devnull@localhost>
2001-09-08 21:50:01 -0700

    [svn] Added stub files for AI common code.

---
+++ ggzd/game_servers/ggzcards/ai/aicommon.h
@@ -0,0 +1,31 @@
+/*
+ * File: ai/aicommon.h
+ * Author: Jason Short
+ * Project: GGZCards Server
+ * Date: 07/03/2001
+ * Desc: useful functions for AI bots
+ * $Id: aicommon.h 2420 2001-09-09 04:50:01Z jdorje $
+ *
+ * This file contains the AI functions for playing any game.
+ * The AI routines follow the none-too-successful algorithm of
+ * always picking a random move.  At least they'll work with any
+ * game!
+ *
+ * Copyright (C) 2001 GGZ Dev Team.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+/* nothing yet */
