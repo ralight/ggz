862a1d07c681396a62bb11e087b6039a073c6bd2
rgade <devnull@localhost>
2001-09-25 12:11:19 -0700

    [svn] - Added functions to add or edit a user db entry

--- ggzd/ggzd/ggzduedit.c
+++ ggzd/ggzd/ggzduedit.c
@@ -83,10 +83,76 @@ void list_players(void)
 }
 
 
-void show_player(void)
+void add_player(void)
 {
 	int i, rc;
 	char time_asc[128];
+
+	printf("Adding new user\n");
+	printf("User handle:   ");
+	getline();
+	for(i=0; i<MAX_USER_NAME_LEN; i++) {
+		if(lb[i] == '\0')
+			break;
+		lb[i] = tolower(lb[i]);
+	}
+	lb[i] = '\0';
+	strcpy(pe.handle, lb);
+
+	printf("Real name:     ");
+	getline();
+	strncpy(pe.name, lb, 32);
+	pe.name[32] = '\0';
+
+	printf("Email address: ");
+	getline();
+	strncpy(pe.email, lb, 32);
+	pe.email[32] = '\0';
+
+	printf("Password:      ");
+	getline();
+	strncpy(pe.password, lb, 16);
+	pe.password[16] = '\0';
+
+	printf("Permissions:   0x");
+	getline();
+	sscanf(lb, "%x", &pe.perms);
+
+	pe.last_login = 0;
+	strftime(time_asc, 128, "%a, %d %b %Y %H:%M:%S %Z",
+		 gmtime(&pe.last_login));
+	printf("\nAdding entry for %s\n", pe.handle);
+	printf("Real name:     [%s]\n", pe.name);
+	printf("Email address: [%s]\n", pe.email);
+	printf("Password:      [%s]\n", pe.password);
+	printf("Permissions:   0x%08X\n", pe.perms);
+	printf("Last login:    %s\n", time_asc);
+
+	lb[0] = '\0';
+	while(lb[0] != 'A' && lb[0] != 'a') {
+		printf("A)ccept or C)ancel: ");
+		getline();
+		if(lb[0] == 'C' || lb[0] == 'c') {
+			printf("\nNew user entry canceled\n");
+			return;
+		}
+	}
+
+	if((rc = _ggzdb_player_add(&pe)) != 0)
+		if(rc == GGZDB_ERR_DUPKEY)
+			printf("\nError: user handle already exists\n\n");
+		else
+			fprintf(stderr, "_ggzdb_player_add() rc=%d\n", rc);
+	else
+		printf("\nNew user entry added\n\n");
+}
+
+
+void edit_player(int edit)
+{
+	int i, rc;
+	char time_asc[128];
+	int accept=0;
 
 	printf("Player handle: ");
 	getline();
@@ -109,12 +175,88 @@ void show_player(void)
 	strftime(time_asc, 128, "%a, %d %b %Y %H:%M:%S %Z",
 		 gmtime(&pe.last_login));
 	printf("\n");
-	printf("User entry for %s\n", pe.handle);
-	printf("Real name:     [%s]\n", pe.name);
-	printf("Email address: [%s]\n", pe.email);
-	printf("Password:      [%s]\n", pe.password);
-	printf("Permissions:   0x%08X\n", pe.perms);
-	printf("Last login:    %s\n\n", time_asc);
+	if(!edit) {
+		printf("User entry for %s\n", pe.handle);
+		printf("Real name:     [%s]\n", pe.name);
+		printf("Email address: [%s]\n", pe.email);
+		printf("Password:      [%s]\n", pe.password);
+		printf("Permissions:   0x%08X\n", pe.perms);
+		printf("Last login:    %s\n\n", time_asc);
+		return;
+	}
+
+	printf("Editing user entry for %s\n", pe.handle);
+	printf("1) Real name:     [%s]\n", pe.name);
+	printf("2) Email address: [%s]\n", pe.email);
+	printf("3) Password:      [%s]\n", pe.password);
+	printf("4) Permissions:   0x%08X\n", pe.perms);
+	printf("   Last login:    %s\n", time_asc);
+	printf("   A) Accepts     C) Cancels\n");
+	while(!accept) {
+		printf("===> ");
+		getline();
+		switch((int)lb[0]) {
+			case '1':
+				printf("[%s] >", pe.name);
+				getline();
+				if(!lb[0])
+					break;
+				strncpy(pe.name, lb, 32);
+				pe.name[32] = '\0';
+				printf("Real name:     [%s]\n", pe.name);
+				break;
+			case '2':
+				printf("[%s] >", pe.email);
+				getline();
+				if(!lb[0])
+					break;
+				strncpy(pe.email, lb, 32);
+				pe.email[32] = '\0';
+				printf("Email address: [%s]\n", pe.email);
+				break;
+			case '3':
+				printf("[%s] >", pe.password);
+				getline();
+				if(!lb[0])
+					break;
+				strncpy(pe.password, lb, 16);
+				pe.password[16] = '\0';
+				printf("Password:      [%s]\n", pe.password);
+				break;
+			case '4':
+				printf("[0x%08X] >0x", pe.perms);
+				getline();
+				if(!lb[0])
+					break;
+				sscanf(lb, "%x", &pe.perms);
+				printf("Permissions:   0x%08X\n", pe.perms);
+				break;
+			case 'A':
+			case 'a':
+				accept = 1;
+				break;
+			case 'C':
+			case 'c':
+				printf("\nUser entry unchanged\n\n");
+				return;
+				break;
+			default:
+				printf("Editing user entry for %s\n",pe.handle);
+				printf("1) Real name:     [%s]\n", pe.name);
+				printf("2) Email address: [%s]\n", pe.email);
+				printf("3) Password:      [%s]\n", pe.password);
+				printf("4) Permissions:   0x%08X\n", pe.perms);
+				printf("   Last login:    %s\n", time_asc);
+				printf("   A) Accepts     C) Cancels\n");
+				printf("Huh?\007 ");
+				break;
+		}
+	}
+
+	if((rc = _ggzdb_player_update(&pe)) != 0)
+		fprintf(stderr, "_ggzdb_player_update() rc=%d\n", rc);
+	else
+		printf("\nUser entry updated\n\n");
 }
 
 
@@ -122,7 +264,9 @@ int main_menu(void)
 {
 	printf("*** MAIN MENU ***\n");
 	printf("1) List all known player handles\n");
-	printf("2) Show a single user's information\n");
+	printf("2) Add a new user\n");
+	printf("3) Show a single user's information\n");
+	printf("4) Edit a user's information\n");
 	printf("Q) Quit\n");
 	while(1) {
 		printf("===> ");
@@ -134,7 +278,13 @@ int main_menu(void)
 			case '1':	list_players();
 					return 1;
 					break;
-			case '2':	show_player();
+			case '2':	add_player();
+					return 1;
+					break;
+			case '3':	edit_player(0);
+					return 1;
+					break;
+			case '4':	edit_player(1);
 					return 1;
 					break;
 			default:	printf("Huh?\007 ");
