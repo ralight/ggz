5047f3cbeafef5992b93a8b1d055134ed94008ac
dr_maux <devnull@localhost>
2001-10-12 17:42:14 -0700

    [svn] - add extra module: handle 'grubby say xxx' commands and reactions on greeting   messages

---
+++ grubby/grubby/modules/extra.c
@@ -0,0 +1,100 @@
+/*******************************************************************
+*
+* Guru - functional example of a next-generation grubby
+* Copyright (C) 2001 Josef Spillner, <dr_maux@users.sourceforge.net>
+* Published under GNU GPL conditions - see 'COPYING' for details
+*
+********************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "gurumod.h"
+
+#define COMMAND_SAY 1
+#define COMMAND_HI 2
+#define COMMAND_NONE 0
+
+/* React on hello messages and say-commands */
+Guru *gurumod_exec(Guru *message)
+{
+	int command;
+	static char *buf = NULL;
+	int i, len;
+
+	command = COMMAND_NONE;
+	if((message->list)
+	&& (message->list[0])
+	&& (message->list[1])
+	&& (!strcmp(message->list[0], message->guru)))
+	{
+		if(message->list[2])
+			if(!strcmp(message->list[1], "say"))
+				command = COMMAND_SAY;
+		if(!message->list[2])
+			if((!strcasecmp(message->list[1], "hi"))
+			|| (!strcasecmp(message->list[1], "hello"))
+			|| (!strcasecmp(message->list[1], "moin"))
+			|| (!strcasecmp(message->list[1], "welcome"))
+			|| (!strcasecmp(message->list[1], "greetings")))
+				command = COMMAND_HI;
+	}
+	if((message->list)
+	&& (message->list[0])
+	&& (message->list[1]))
+	{
+		if(((!strcmp(message->list[0], "hi"))
+		|| (!strcasecmp(message->list[0], "hello"))
+		|| (!strcasecmp(message->list[0], "moin"))
+		|| (!strcasecmp(message->list[0], "welcome"))
+		|| (!strcasecmp(message->list[0], "greetings")))
+		&& (!strcmp(message->list[1], message->guru)))
+		{
+			command = COMMAND_HI;
+		}
+	}
+
+	if(buf)
+	{
+		free(buf);
+		buf = NULL;
+	}
+
+	switch(command)
+	{
+		case COMMAND_NONE:
+			return NULL;
+			break;
+		case COMMAND_HI:
+			buf = (char*)malloc(strlen(message->player) + 10);
+			sprintf(buf, "Hi %s", message->player);
+			message->message = buf;
+			return message;
+			break;
+		case COMMAND_SAY:
+			i = 2;
+			len = 0;
+			while((message->list) && (message->list[i]))
+			{
+				buf = (char*)realloc(buf, len + strlen(message->list[i]) + 2);
+				if(!len) strcpy(buf, message->list[i]);
+				else
+				{
+					strcat(buf, " ");
+					strcat(buf, message->list[i]);
+				}
+				len += strlen(message->list[i]);
+				i++;
+			}
+			message->message = buf;
+			return message;
+			break;
+	}
+	return NULL;
+}
+
+/* Empty init */
+void gurumod_init()
+{
+}
+
