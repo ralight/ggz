59bdcd76d1e5168c1b21cf2069f0c0a0881af22a
rgade <devnull@localhost>
2001-11-04 00:50:08 -0800

    [svn] - Added detailed documentation for ggz_conf routines

--- libggz/src/ggz.h
+++ libggz/src/ggz.h
@@ -146,51 +146,212 @@ char * _ggz_strdup(const char *, char *, int);
 
 
 /**
- * @defgroup conf Config file parsing
+ * @defgroup conf Configuration file parsing
+ * Configuration file routines to store and retrieve values.
+ * Configuration file parsing begins by calling ggz_conf_parse() to open
+ * a config file.  The file can be created automatically if CONF_CREATE
+ * is specified.  The returned handle uniquely identifies the configuration
+ * file, so multiple files can be open at one time.
  *
- * (currently exists in server and ggzcore )
+ * If the same configuration file is opened multiple times, the original
+ * handle will be returned and only one copy will be retained within memory.
+ * One use of this feature is that a file may be opened read only initially and
+ * later have writing enabled by merely reparsing the file using the same
+ * pathname.  Note that this feature can be fooled if the same file is
+ * opened using different pathnames.  Chaos may or may not result in this
+ * case, and it is considered a feature not a bug.
  *
+ * Values are stored using a system of section and keys.  A key must be
+ * unique within a section (you cannot store both an integer and a string
+ * under the same key.  Section and key names may contain any characters
+ * (including whitespace) other than an equals sign.  Although keys may not
+ * have leading or trailing whitespace, section names may.  It is suggested
+ * that any whitespace (other than possibly internal spaces) be avoided when
+ * specifying section and key names.  Alphabetic case is preserved, and must
+ * be matched.
+ *
+ * An important caveat to remember when using the configuration functions
+ * is that writing a value only caches the value in memory.  In order to
+ * write the values to the physical file, ggz_conf_commit() must be called
+ * at some point.  This makes writing multiple values in rapid succession
+ * more efficient, as the entire file must be regenerated in order to be
+ * written to the flat-file format of the configuration file.
+ *
+ * The string and list reading functions return dynamically allocated
+ * memory to the caller.  The user is responsible for calling free() on
+ * this memory when they no longer need the returned values.
+ *
+ * All memory used internally by the configuration functions will be
+ * released when ggz_conf_cleanup() is called.  Note that this does NOT
+ * commit any changes made to the configuration files.
+ * The user is expected to call this at program termination, but it may
+ * be called at any time earlier than termination and new files may
+ * be subsequently opened.
+ *
+ * @bug The maximum length of a configuration file line is fixed at 1024
+ * characters.  Exceeding this length will result in parsing errors when
+ * the file is parsed.  No internal problems should (hopefully) result,
+ * but values will be lost.
  * @{
  */
+
+/** 
+ * Specifies opening a configuration file in read-only mode
+ * @see ggz_conf_parse()
+ */
 #define CONF_RDONLY	((unsigned char) 0x01)
+/** 
+ * Specifies opening a configuration file for both reading and writing
+ * @see ggz_conf_parse()
+ */
 #define CONF_RDWR	((unsigned char) 0x02)
+/** 
+ * Specifies creating the configuration file if it does not already exist
+ * @see ggz_conf_parse()
+ */
 #define CONF_CREATE	((unsigned char) 0x04)
 
-int ggz_conf_initialize	(const char	*g_path,
-			 const char	*u_path);
+/**
+ * Closes all open configuration files.
+ * @note This does not automatically commit changed values.  Any non-committed
+ * values written will be lost.
+ */
 void ggz_conf_cleanup (void);
 
+/**
+ * Opens a configuration file and parses the variables so they can
+ * be retrieved with the access functions.
+ * @param path A string specifying the filename to be parsed
+ * @param options An or'ed set of CONF_* option bits
+ * @return An integer configuration file handle or -1 on error
+ * @see CONF_RDONLY, CONF_RDWR, CONF_CREATE
+ */
 int ggz_conf_parse		(const char *path,
 			 const unsigned char options);
+
+/**
+ * Commits any changed variables to the configuration file. The configuration
+ * file remains open and may continue to be written to.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @return 0 if successful, -1 on failure
+ */
 int ggz_conf_commit		(int handle);
+
+/**
+ * Writes a string value to a section and key in an open configuration file.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name to write to
+ * @param key A string variable key name to write to
+ * @param value The string value to write
+ * @return 0 if successful, -1 on failure
+ */
 int ggz_conf_write_string	(int	handle,
 			 const char	*section,
 			 const char	*key,
 			 const char	*value);
+
+/**
+ * Writes an integer value to a section and key in an open configuration file.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name to write to
+ * @param key A string variable key name to write to
+ * @param value The integer value to write
+ * @return 0 if successful, -1 on failure
+ */
 int ggz_conf_write_int	(int	handle,
 			 const char	*section,
 			 const char	*key,
 			 int	value);
+
+/**
+ * Writes a list of string values to a section and key in an open
+ * configuration file.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name to write to
+ * @param key A string variable key name to write to
+ * @param argc The number of string array entries in argv
+ * @param argv An array of strings to create a list from
+ * @return 0 if successful, -1 on failure
+ */
 int ggz_conf_write_list	(int	handle,
 			 const char	*section,
 			 const char	*key,
 			 int	argc,
 			 char	**argv);
+
+/**
+ * Reads a string value from an open configuration file.  If the section/key
+ * combination is not found, the default entry is returned.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name to read from
+ * @param key A string variable key name to read from
+ * @param def A value to be returned if the entry does not exist (may be NULL)
+ * @return A dynamically allocated copy of the stored (or default) value
+ * or NULL
+ * @note The copy is allocated via a standard malloc() call and the caller
+ * is expected to be responsible for calling free() on the returned value
+ * when they no longer need the value.  No memory is allocated if a default
+ * value of NULL is returned.
+ */
 char * ggz_conf_read_string	(int	handle,
 			 const char	*section,
 			 const char	*key,
 			 const char	*def);
+
+/**
+ * Reads an integer value from an open configuration file.  If the section/key
+ * combination is not found, the default entry is returned.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name to read from
+ * @param key A string variable key name to read from
+ * @param def A value to be returned if the entry does not exist
+ * @return The integer value stored in the configuration file, or the default
+ */
 int ggz_conf_read_int	(int	handle,
 			 const char	*section,
 			 const char	*key,
 			 int	def);
+
+/**
+ * Reads a list of string values from an open configuration file.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name to read from
+ * @param key A string variable key name to read from
+ * @param argcp A pointer to an integer which will receive the number of
+ * list entries read
+ * @param argvp A pointer to a string array.  This will receive a value
+ * pointing to a dynamically allocated array of string values.
+ * @return 0 on success, -1 on failure
+ * @note The array is allocated via standard malloc() calls and the caller
+ * is expected to be responsible for calling free() on the string values
+ * and the associated array structure when they no longer need the list.
+ * If the section/key combination is not found -1 will be returned, argcp
+ * is set to a value of zero, no memory will be allocated, and
+ * argvp will retain it's (possibly undefined) value.
+ */
 int ggz_conf_read_list	(int	handle,
 			 const char	*section,
 			 const char	*key,
 			 int	*argcp,
 			 char	***argvp);
+
+/**
+ * This will remove an entire section and all it's associated keys from
+ * a configuration file.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name to remove
+ * @return 0 on success, -1 on failure
+ */
 int ggz_conf_remove_section	(int	handle,
 			 const char	*section);
+
+/**
+ * This will remove a single key from a configuration file.
+ * @param handle A valid handle returned by ggz_conf_parse()
+ * @param section A string section name the key is located in
+ * @param key A string key name to remove
+ * @return 0 on success, -1 on failure
+ */
 int ggz_conf_remove_key	(int	handle,
 			 const char	*section,
 			 const char	*key);
