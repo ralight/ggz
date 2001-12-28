47453cc92a004929f0a08b2d0eac04c319ed1c23
dr_maux <devnull@localhost>
2001-12-28 14:29:11 -0800

    [svn] - force --ggz option - added and install module file

--- kde-games/muehle/src/main.cpp
+++ kde-games/muehle/src/main.cpp
@@ -3,16 +3,31 @@
 #include <kaboutdata.h>
 #include "toplevel.h"
 #include <kcmdlineargs.h>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
 	Toplevel *top;
 	KAboutData *about;
+	KCmdLineArgs *args;
+	static const KCmdLineOptions op[] = {
+		{"ggz", I18N_NOOP("Request GGZ game explicitely"), 0},
+		{0, 0, 0}};
 
 	about = new KAboutData("muehle", "KDE Muehle Game", "0.1", "Muehle game for KDE",
 		KAboutData::License_GPL, "Copyright (C) 2001 Josef Spillner", "This game supports multiple variants of Muehle.",
 		"http://games.kde.org", "dr_maux@users.sourceforge.net");
+
 	KCmdLineArgs::init(argc, argv, about);
+	KCmdLineArgs::addCmdLineOptions(op);
+	args = KCmdLineArgs::parsedArgs();
+
+	if(!args->isSet("ggz"))
+	{
+		cout << "Sorry, this game does only work in GGZ mode so far." << endl;
+		return -1;
+	}
+
 	KApplication a;
 
 	top = new Toplevel();
