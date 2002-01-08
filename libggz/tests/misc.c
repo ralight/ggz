56de7c1f6169c2202ae92c238bc0848b27498fc5
bmh <devnull@localhost>
2002-01-08 10:20:13 -0800

    [svn] - Correct test to match new behavior of ggz_xml_[un]escape

--- libggz/tests/misc.c
+++ libggz/tests/misc.c
@@ -2,33 +2,43 @@
 
 #include <ggz.h>
 
-#define TESTSTR "\"' &quot; >< &&amp;\""
-int main(void) 
+#define TESTSTR1 "\"' &quot; >< &&amp;\""
+#define TESTSTR2 "This test requires no conversion" 
+
+int main(void)
 {
 	int errs = 0;
 	char *instr, *outstr;
 
-	instr = TESTSTR;
+	instr = TESTSTR1;
 	outstr = ggz_xml_escape(instr);
 	printf("Input string = %s\n", instr);
 	printf("Output string = %s\n", outstr);
 	instr = ggz_xml_unescape(outstr);
 	printf("Back to input string = %s\n", instr);
-	if(strcmp(instr, TESTSTR)) {
+	if(strcmp(instr, TESTSTR1)) {
 		printf("Output doesn't match original!\n");
 		errs++;
 	}
 	ggz_free(instr);
 	ggz_free(outstr);
 
-	instr = "This test requires no conversion";
+	instr = TESTSTR2;
 	outstr = ggz_xml_escape(instr);
-	if(outstr != NULL) {
-		printf("Erroneous production from string = %s\n", instr);
-		printf("To produce = %s\n", outstr);
-		ggz_free(outstr);
+	printf("Input string = %s\n", instr);
+	printf("Output string = %s\n", outstr);
+	if(strcmp(outstr, instr)) {
+		printf("Conversion doesn't match original!\n");
 		errs++;
 	}
+	instr = ggz_xml_unescape(outstr);
+	printf("Back to input string = %s\n", instr);
+	if(strcmp(instr, TESTSTR2)) {
+		printf("Output doesn't match original!\n");
+		errs++;
+	}
+	ggz_free(instr);
+	ggz_free(outstr);
 
 	ggz_memory_check();
 
@@ -38,3 +48,5 @@ int main(void)
 		printf("Failed tests = %d\n", errs);
 	return errs;
 }
+
+
