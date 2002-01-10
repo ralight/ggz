a674fa91c72be26178994a4eadc1f8e140e48e4d
dr_maux <devnull@localhost>
2002-01-10 11:17:26 -0800

    [svn] - discourage launching from outside GGZ

--- kde-games/kdots/main.cpp
+++ kde-games/kdots/main.cpp
@@ -15,29 +15,41 @@
 #include <kcmdlineargs.h>
 #include <kaboutdata.h>
 #include <klocale.h>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-	KApplication *a;
 	KDotsWin *kdotswin;
 	KAboutData *about;
+	KCmdLineArgs *args;
+	static const KCmdLineOptions op[] = {
+		{"ggz", I18N_NOOP("Request GGZ game explicitely"), 0},
+		{0, 0, 0}};
 
 	about = new KAboutData("kdots",
+		I18N_NOOP("KDots"),
+		"0.0.5pre",
 		I18N_NOOP("Connect The Dots for KDE"),
-		"0.0.4",
-		I18N_NOOP("test."),
 		KAboutData::License_GPL,
-		"Copyright (C) 2001 Josef Spillner",
+		"Copyright (C) 2001, 2002 Josef Spillner",
 		I18N_NOOP("This game is part of the GGZ Gaming Zone."),
 		"http://mindx.sourceforge.net",
 		"dr_maux@users.sourceforge.net");
 	about->addAuthor("Josef Spillner", I18N_NOOP("Author."), "dr_maux@users.sourceforge.net", "http://mindx.sourceforge.net");
 
 	KCmdLineArgs::init(argc, argv, about);
+	KCmdLineArgs::addCmdLineOptions(op);
+	args = KCmdLineArgs::parsedArgs();
 
-	a = new KApplication();
+	if(!args->isSet("ggz"))
+	{
+		std::cout << "Sorry, this game does only work in GGZ mode so far." << std::endl;
+		return -1;
+	}
+
+	KApplication a;
 	kdotswin = new KDotsWin("KDotsWin");
-	a->setMainWidget(kdotswin);
-	return a->exec();
+	a.setMainWidget(kdotswin);
+	return a.exec();
 }
 
