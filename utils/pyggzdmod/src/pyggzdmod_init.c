6d0efebf7fc3147ff7d3226e7c55828d781c98c7
dr_maux <devnull@localhost>
2002-01-11 13:57:57 -0800

    [svn] - make python wrapper for ggzdmod compile again - this does not really work yet (same here: the Seat structure), but when   starting from the command line it's the same experience as with plain C/C++   :-)

--- utils/pyggzdmod/src/pyggzdmod_init.c
+++ utils/pyggzdmod/src/pyggzdmod_init.c
@@ -1,7 +1,7 @@
 /**********************************************/
 /*                                            */
 /* PyGGZDMod - Python wrapper for libggzdmod  */
-/* Copyright (C) 2001 Josef Spillner          */
+/* Copyright (C) 2001, 2002 Josef Spillner    */
 /* dr_maux@users.sourceforge.net              */
 /* Published under GNU GPL conditions         */
 /*                                            */
@@ -12,205 +12,90 @@
 /**********************************************/
 
 #include <Python.h>
-#include <ggz_server.h>
+#include <ggzdmod.h>
 
 /**********************************************/
 /* Function prototypes                        */
 /**********************************************/
 
-void pyggzd_cb_launch_hook(ggzd_event_t event_id, void *handler_data);
-void pyggzd_cb_join_hook(ggzd_event_t event_id, void *handler_data);
-void pyggzd_cb_leave_hook(ggzd_event_t event_id, void *handler_data);
-void pyggzd_cb_quit_hook(ggzd_event_t event_id, void *handler_data);
-void pyggzd_cb_player_hook(ggzd_event_t event_id, void *handler_data);
-static PyObject *pyggzd_test(PyObject *self, PyObject *args);
+static void pyggzdmod_cb_state_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data);
+static void pyggzdmod_cb_join_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data);
+static void pyggzdmod_cb_leave_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data);
+static void pyggzdmod_cb_log_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data);
+static void pyggzdmod_cb_player_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data);
+static void pyggzdmod_cb_error_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data);
+static PyObject *pyggzdmod_test(PyObject *self, PyObject *args);
 
 /**********************************************/
 /* Global variables                           */
 /**********************************************/
 
-static PyObject *pyggzd_cb_launch = NULL;
-static PyObject *pyggzd_cb_join = NULL;
-static PyObject *pyggzd_cb_leave = NULL;
-static PyObject *pyggzd_cb_quit = NULL;
-static PyObject *pyggzd_cb_player = NULL;
+static GGZdMod *ggzdmod;
+
+static PyObject *pyggzdmod_cb_state = NULL;
+static PyObject *pyggzdmod_cb_join = NULL;
+static PyObject *pyggzdmod_cb_leave = NULL;
+static PyObject *pyggzdmod_cb_log = NULL;
+static PyObject *pyggzdmod_cb_player = NULL;
+static PyObject *pyggzdmod_cb_error = NULL;
 
 /**********************************************/
 /* GGZDMod object methods just like in C       */
 /**********************************************/
 
-static PyObject *pyggzd_get_seat_status(PyObject *self, PyObject *args)
+static PyObject *pyggzdmod_get_num_seats(PyObject *self, PyObject *args)
 {
-	int seat;
+	int seats;
+
+	if(!PyArg_ParseTuple(args, "")) return NULL;
+	seats = ggzdmod_get_num_seats(ggzdmod);
+	return Py_BuildValue("i", seats);
+}
+
+/*static PyObject *pyggzdmod_seats_open(PyObject *self, PyObject *args)
+{
+	int seats;
+
+	if(!PyArg_ParseTuple(args, "")) return NULL;
+	seats = ggzdmod_seats_open(ggzdmod);
+	return Py_BuildValue("i", seats);
+}*/
+
+static PyObject *pyggzdmod_connect(PyObject *self, PyObject *args)
+{
 	int ret;
 
-	if(!PyArg_ParseTuple(args, "i", &seat)) return NULL;
-	ret = ggzd_get_seat_status(seat);
+	if(!PyArg_ParseTuple(args, "")) return NULL;
+	ret = ggzdmod_connect(ggzdmod);
 	return Py_BuildValue("i", ret);
 }
 
-static PyObject *pyggzd_set_seat_status(PyObject *self, PyObject *args)
+static PyObject *pyggzdmod_disconnect(PyObject *self, PyObject *args)
 {
-	int seat, status;
 	int ret;
 
-	if(!PyArg_ParseTuple(args, "ii", &seat, &status)) return NULL;
-	ret = ggzd_set_seat_status(seat, status);
+	if(!PyArg_ParseTuple(args, "")) return NULL;
+	ret = ggzdmod_disconnect(ggzdmod);
 	return Py_BuildValue("i", ret);
 }
 
-static PyObject *pyggzd_get_player_name(PyObject *self, PyObject *args)
+static PyObject *pyggzdmod_dispatch(PyObject *self, PyObject *args)
 {
-	const char *name;
-	int seat;
-
-	if(!PyArg_ParseTuple(args, "i", &seat)) return NULL;
-	name = ggzd_get_player_name(seat);
-	return Py_BuildValue("s", name);
-}
-
-static PyObject *pyggzd_set_player_name(PyObject *self, PyObject *args)
-{
-	char *name;
-	int seat, ret;
-
-	if(!PyArg_ParseTuple(args, "is", &seat, &name)) return NULL;
-	ret = ggzd_set_player_name(seat, name);
-	return Py_BuildValue("i", ret);
-}
-
-static PyObject *pyggzd_get_player_socket(PyObject *self, PyObject *args)
-{
-	int seat, socket;
-
-	if(!PyArg_ParseTuple(args, "i", &seat)) return NULL;
-	socket = ggzd_get_player_socket(seat);
-	return Py_BuildValue("i", socket);
-}
-
-static PyObject *pyggzd_get_player_udp_socket(PyObject *self, PyObject *args)
-{
-	int seat, socket;
-
-	if(!PyArg_ParseTuple(args, "i", &seat)) return NULL;
-	socket = ggzd_get_player_udp_socket(seat);
-	return Py_BuildValue("i", socket);
-}
-
-static PyObject *pyggzd_seats_num(PyObject *self, PyObject *args)
-{
-	int seats;
-
 	if(!PyArg_ParseTuple(args, "")) return NULL;
-	seats = ggzd_seats_num();
-	return Py_BuildValue("i", seats);
-}
-
-static PyObject *pyggzd_seats_open(PyObject *self, PyObject *args)
-{
-	int seats;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	seats = ggzd_seats_open();
-	return Py_BuildValue("i", seats);
-}
-
-static PyObject *pyggzd_seats_bot(PyObject *self, PyObject *args)
-{
-	int seats;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	seats = ggzd_seats_bot();
-	return Py_BuildValue("i", seats);
-}
-
-static PyObject *pyggzd_seats_reserved(PyObject *self, PyObject *args)
-{
-	int seats;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	seats = ggzd_seats_reserved();
-	return Py_BuildValue("i", seats);
-}
-
-static PyObject *pyggzd_seats_human(PyObject *self, PyObject *args)
-{
-	int seats;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	seats = ggzd_seats_human();
-	return Py_BuildValue("i", seats);
-}
-
-static PyObject *pyggzd_gameover(PyObject *self, PyObject *args)
-{
-	int status;
-
-	if(!PyArg_ParseTuple(args, "i", &status)) return NULL;
-	ggzd_gameover(status);
+	ggzdmod_dispatch(ggzdmod);
 	return Py_BuildValue("");
 }
 
-static PyObject *pyggzd_get_gameover(PyObject *self, PyObject *args)
+static PyObject *pyggzdmod_loop(PyObject *self, PyObject *args)
 {
 	int ret;
 
 	if(!PyArg_ParseTuple(args, "")) return NULL;
-	ret = ggzd_get_gameover();
+	ret = ggzdmod_loop(ggzdmod);
 	return Py_BuildValue("i", ret);
 }
 
-static PyObject *pyggzd_connect(PyObject *self, PyObject *args)
-{
-	int ret;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	ret = ggzd_connect();
-	return Py_BuildValue("i", ret);
-}
-
-static PyObject *pyggzd_disconnect(PyObject *self, PyObject *args)
-{
-	int ret;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	ret = ggzd_disconnect();
-	return Py_BuildValue("i", ret);
-}
-
-static PyObject *pyggzd_dispatch(PyObject *self, PyObject *args)
-{
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	ggzd_dispatch();
-	return Py_BuildValue("");
-}
-
-static PyObject *pyggzd_io_is_pending(PyObject *self, PyObject *args)
-{
-	int ret;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	ret = ggzd_io_is_pending();
-	return Py_BuildValue("i", ret);
-}
-
-static PyObject *pyggzd_io_read_all(PyObject *self, PyObject *args)
-{
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	ggzd_io_read_all();
-	return Py_BuildValue("");
-}
-
-static PyObject *pyggzd_main_loop(PyObject *self, PyObject *args)
-{
-	int ret;
-
-	if(!PyArg_ParseTuple(args, "")) return NULL;
-	ret = ggzd_main_loop();
-	return Py_BuildValue("i", ret);
-}
-
-static PyObject *pyggzd_set_handler(PyObject *self, PyObject *args)
+static PyObject *pyggzdmod_set_handler(PyObject *self, PyObject *args)
 {
 	int id;
 	PyObject *result = NULL;
@@ -220,30 +105,35 @@ static PyObject *pyggzd_set_handler(PyObject *self, PyObject *args)
 	Py_XINCREF(temp);
 	switch(id)
 	{
-		case GGZ_EVENT_LAUNCH:
-			Py_XDECREF(pyggzd_cb_launch);
-			pyggzd_cb_launch = temp;
-			ggzd_set_handler(GGZ_EVENT_LAUNCH, pyggzd_cb_launch_hook);
+		case GGZDMOD_EVENT_STATE:
+			Py_XDECREF(pyggzdmod_cb_state);
+			pyggzdmod_cb_state = temp;
+			ggzdmod_set_handler(ggzdmod, GGZDMOD_EVENT_STATE, pyggzdmod_cb_state_hook);
 			break;
-		case GGZ_EVENT_JOIN:
-			Py_XDECREF(pyggzd_cb_join);
-			pyggzd_cb_join = temp;
-			ggzd_set_handler(GGZ_EVENT_JOIN, pyggzd_cb_join_hook);
+		case GGZDMOD_EVENT_JOIN:
+			Py_XDECREF(pyggzdmod_cb_join);
+			pyggzdmod_cb_join = temp;
+			ggzdmod_set_handler(ggzdmod, GGZDMOD_EVENT_JOIN, pyggzdmod_cb_join_hook);
 			break;
-		case GGZ_EVENT_LEAVE:
-			Py_XDECREF(pyggzd_cb_leave);
-			pyggzd_cb_leave = temp;
-			ggzd_set_handler(GGZ_EVENT_LEAVE, pyggzd_cb_leave_hook);
+		case GGZDMOD_EVENT_LEAVE:
+			Py_XDECREF(pyggzdmod_cb_leave);
+			pyggzdmod_cb_leave = temp;
+			ggzdmod_set_handler(ggzdmod, GGZDMOD_EVENT_LEAVE, pyggzdmod_cb_leave_hook);
 			break;
-		case GGZ_EVENT_QUIT:
-			Py_XDECREF(pyggzd_cb_quit);
-			pyggzd_cb_quit = temp;
-			ggzd_set_handler(GGZ_EVENT_QUIT, pyggzd_cb_quit_hook);
+		case GGZDMOD_EVENT_LOG:
+			Py_XDECREF(pyggzdmod_cb_log);
+			pyggzdmod_cb_log = temp;
+			ggzdmod_set_handler(ggzdmod, GGZDMOD_EVENT_LOG, pyggzdmod_cb_log_hook);
 			break;
-		case GGZ_EVENT_PLAYER:
-			Py_XDECREF(pyggzd_cb_player);
-			pyggzd_cb_player = temp;
-			ggzd_set_handler(GGZ_EVENT_PLAYER, pyggzd_cb_player_hook);
+		case GGZDMOD_EVENT_PLAYER_DATA:
+			Py_XDECREF(pyggzdmod_cb_player);
+			pyggzdmod_cb_player = temp;
+			ggzdmod_set_handler(ggzdmod, GGZDMOD_EVENT_PLAYER_DATA, pyggzdmod_cb_player_hook);
+			break;
+		case GGZDMOD_EVENT_ERROR:
+			Py_XDECREF(pyggzdmod_cb_error);
+			pyggzdmod_cb_error = temp;
+			ggzdmod_set_handler(ggzdmod, GGZDMOD_EVENT_ERROR, pyggzdmod_cb_error_hook);
 			break;
 	}
 	Py_INCREF(Py_None);
@@ -255,29 +145,16 @@ static PyObject *pyggzd_set_handler(PyObject *self, PyObject *args)
 /* Function lookup table                      */
 /**********************************************/
 
-static PyMethodDef pyggzd_methods[] =
+static PyMethodDef pyggzdmod_methods[] =
 {
-	{"getSeatStatus", pyggzd_get_seat_status, METH_VARARGS},
-	{"setSeatStatus", pyggzd_set_seat_status, METH_VARARGS},
-	{"getPlayerName", pyggzd_get_player_name, METH_VARARGS},
-	{"setPlayerName", pyggzd_set_player_name, METH_VARARGS},
-	{"getPlayerSocket", pyggzd_get_player_socket, METH_VARARGS},
-	{"getPlayerUDPSocket", pyggzd_get_player_udp_socket, METH_VARARGS},
-	{"seatsNum", pyggzd_seats_num, METH_VARARGS},
-	{"seatsOpen", pyggzd_seats_open, METH_VARARGS},
-	{"seatsBot", pyggzd_seats_bot, METH_VARARGS},
-	{"seatsReserved", pyggzd_seats_reserved, METH_VARARGS},
-	{"seatsHuman", pyggzd_seats_human, METH_VARARGS},
-	{"gameOver", pyggzd_gameover, METH_VARARGS},
-	{"getGameOver", pyggzd_get_gameover, METH_VARARGS},
-	{"connect", pyggzd_connect, METH_VARARGS},
-	{"disconnect", pyggzd_disconnect, METH_VARARGS},
-	{"dispatch", pyggzd_dispatch, METH_VARARGS},
-	{"ioIsPending", pyggzd_io_is_pending, METH_VARARGS},
-	{"ioReadAll", pyggzd_io_read_all, METH_VARARGS},
-	{"mainLoop", pyggzd_main_loop, METH_VARARGS},
-	{"setHandler", pyggzd_set_handler, METH_VARARGS},
-	{"test", pyggzd_test, METH_VARARGS},
+	{"getNumSeats", pyggzdmod_get_num_seats, METH_VARARGS},
+	/*{"seatsOpen", pyggzdmod_seats_open, METH_VARARGS},*/
+	{"connect", pyggzdmod_connect, METH_VARARGS},
+	{"disconnect", pyggzdmod_disconnect, METH_VARARGS},
+	{"dispatch", pyggzdmod_dispatch, METH_VARARGS},
+	{"mainLoop", pyggzdmod_loop, METH_VARARGS},
+	{"setHandler", pyggzdmod_set_handler, METH_VARARGS},
+	{"test", pyggzdmod_test, METH_VARARGS},
 	{NULL, NULL}
 };
 
@@ -285,12 +162,12 @@ static PyMethodDef pyggzd_methods[] =
 /* Internal callbacks                         */
 /**********************************************/
 
-void pyggzd_cb_launch_hook(ggzd_event_t event_id, void *handler_data)
+void pyggzdmod_cb_state_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data)
 {
 	PyObject *arg, *res;
 
 	arg = Py_BuildValue("()");
-	res = PyEval_CallObject(pyggzd_cb_launch, arg);
+	res = PyEval_CallObject(pyggzdmod_cb_state, arg);
 	if(res == NULL)
 	{
 		printf("----------------------------\n");
@@ -301,12 +178,12 @@ void pyggzd_cb_launch_hook(ggzd_event_t event_id, void *handler_data)
 	Py_DECREF(arg);
 }
 
-void pyggzd_cb_join_hook(ggzd_event_t event_id, void *handler_data)
+void pyggzdmod_cb_join_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data)
 {
 	PyObject *arg, *res;
 
 	arg = Py_BuildValue("(i)", *(int*)handler_data);
-	res = PyEval_CallObject(pyggzd_cb_join, arg);
+	res = PyEval_CallObject(pyggzdmod_cb_join, arg);
 	if(res == NULL)
 	{
 		printf("----------------------------\n");
@@ -317,12 +194,12 @@ void pyggzd_cb_join_hook(ggzd_event_t event_id, void *handler_data)
 	Py_DECREF(arg);
 }
 
-void pyggzd_cb_leave_hook(ggzd_event_t event_id, void *handler_data)
+void pyggzdmod_cb_leave_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data)
 {
 	PyObject *arg, *res;
 
 	arg = Py_BuildValue("(i)", *(int*)handler_data);
-	res = PyEval_CallObject(pyggzd_cb_leave, arg);
+	res = PyEval_CallObject(pyggzdmod_cb_leave, arg);
 	if(res == NULL)
 	{
 		printf("----------------------------\n");
@@ -333,12 +210,12 @@ void pyggzd_cb_leave_hook(ggzd_event_t event_id, void *handler_data)
 	Py_DECREF(arg);
 }
 
-void pyggzd_cb_quit_hook(ggzd_event_t event_id, void *handler_data)
+void pyggzdmod_cb_log_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data)
 {
 	PyObject *arg, *res;
 
 	arg = Py_BuildValue("()");
-	res = PyEval_CallObject(pyggzd_cb_quit, arg);
+	res = PyEval_CallObject(pyggzdmod_cb_log, arg);
 	if(res == NULL)
 	{
 		printf("----------------------------\n");
@@ -349,12 +226,12 @@ void pyggzd_cb_quit_hook(ggzd_event_t event_id, void *handler_data)
 	Py_DECREF(arg);
 }
 
-void pyggzd_cb_player_hook(ggzd_event_t event_id, void *handler_data)
+void pyggzdmod_cb_player_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data)
 {
 	PyObject *arg, *res;
 
 	arg = Py_BuildValue("(i)", *(int*)handler_data);
-	res = PyEval_CallObject(pyggzd_cb_player, arg);
+	res = PyEval_CallObject(pyggzdmod_cb_player, arg);
 	if(res == NULL)
 	{
 		printf("----------------------------\n");
@@ -365,13 +242,29 @@ void pyggzd_cb_player_hook(ggzd_event_t event_id, void *handler_data)
 	Py_DECREF(arg);
 }
 
-static PyObject *pyggzd_test(PyObject *self, PyObject *args)
+void pyggzdmod_cb_error_hook(GGZdMod *ggzdmod, GGZdModEvent event, void *handler_data)
+{
+	PyObject *arg, *res;
+
+	arg = Py_BuildValue("(i)", *(int*)handler_data);
+	res = PyEval_CallObject(pyggzdmod_cb_error, arg);
+	if(res == NULL)
+	{
+		printf("----------------------------\n");
+		printf("ERROR in pyggzdmod callback!\n");
+		PyErr_Print();
+		printf("----------------------------\n");
+	}
+	Py_DECREF(arg);
+}
+
+static PyObject *pyggzdmod_test(PyObject *self, PyObject *args)
 {
 	int z;
 
 	z = 0;
-	pyggzd_cb_launch_hook(GGZ_EVENT_LAUNCH, NULL);
-	pyggzd_cb_join_hook(GGZ_EVENT_JOIN, (void*)(&z));
+	pyggzdmod_cb_state_hook(ggzdmod, GGZDMOD_EVENT_STATE, NULL);
+	pyggzdmod_cb_join_hook(ggzdmod, GGZDMOD_EVENT_JOIN, (void*)(&z));
 	return Py_None;
 }
 
@@ -383,7 +276,7 @@ void initggzdmod(void)
 {
 	PyObject *mod;
 
-	mod = Py_InitModule("ggzdmod", pyggzd_methods);
+	mod = Py_InitModule("ggzdmod", pyggzdmod_methods);
 
 	PyModule_AddIntConstant(mod, "SEAT_OPEN", GGZ_SEAT_OPEN);
 	PyModule_AddIntConstant(mod, "SEAT_BOT", GGZ_SEAT_BOT);
@@ -391,10 +284,21 @@ void initggzdmod(void)
 	PyModule_AddIntConstant(mod, "SEAT_NONE", GGZ_SEAT_NONE);
 	PyModule_AddIntConstant(mod, "SEAT_PLAYER", GGZ_SEAT_PLAYER);
 
-	PyModule_AddIntConstant(mod, "EVENT_LAUNCH", GGZ_EVENT_LAUNCH);
-	PyModule_AddIntConstant(mod, "EVENT_JOIN", GGZ_EVENT_JOIN);
-	PyModule_AddIntConstant(mod, "EVENT_LEAVE", GGZ_EVENT_LEAVE);
-	PyModule_AddIntConstant(mod, "EVENT_QUIT", GGZ_EVENT_QUIT);
-	PyModule_AddIntConstant(mod, "EVENT_PLAYER", GGZ_EVENT_PLAYER);
+	PyModule_AddIntConstant(mod, "EVENT_STATE", GGZDMOD_EVENT_STATE);
+	PyModule_AddIntConstant(mod, "EVENT_JOIN", GGZDMOD_EVENT_JOIN);
+	PyModule_AddIntConstant(mod, "EVENT_LEAVE", GGZDMOD_EVENT_LEAVE);
+	PyModule_AddIntConstant(mod, "EVENT_LOG", GGZDMOD_EVENT_LOG);
+	PyModule_AddIntConstant(mod, "EVENT_PLAYER", GGZDMOD_EVENT_PLAYER_DATA);
+	PyModule_AddIntConstant(mod, "EVENT_ERROR", GGZDMOD_EVENT_ERROR);
+
+	PyModule_AddIntConstant(mod, "STATE_CREATED", GGZDMOD_STATE_CREATED);
+	PyModule_AddIntConstant(mod, "STATE_WAITING", GGZDMOD_STATE_WAITING);
+	PyModule_AddIntConstant(mod, "STATE_PLAYING", GGZDMOD_STATE_PLAYING);
+	PyModule_AddIntConstant(mod, "STATE_DONE", GGZDMOD_STATE_DONE);
+
+	PyModule_AddIntConstant(mod, "TYPE_GGZ", GGZDMOD_GGZ);
+	PyModule_AddIntConstant(mod, "TYPE_GAME", GGZDMOD_GAME);
+
+	ggzdmod = ggzdmod_new(GGZDMOD_GAME);
 }
 
--- utils/pyggzdmod/src/test.py
+++ utils/pyggzdmod/src/test.py
@@ -1,12 +1,12 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python
 #
 # Test game server for the GGZ Gaming Zone
 # Written in Python
 
 import ggzdmod;
 
-def hook_launch ():
-	print "* launch"
+def hook_state ():
+	print "* state"
 
 def hook_join (seat):
 	print "* join: ", seat
@@ -16,19 +16,23 @@ def hook_leave (seat):
 	print "* leave: ", seat
 	print "(Name: " + ggzdmod.getPlayerName(seat) + ")"
 
-def hook_quit ():
-	print "* quit"
+def hook_log ():
+	print "* log"
 
 def hook_player (seat):
 	print "* player: ", seat
 	print "(Name: " + ggzdmod.getPlayerName(seat) + ")"
 
+def hook_error (arg):
+	print "* error"
+
 print ">> register callbacks"
-ggzdmod.setHandler(ggzdmod.EVENT_LAUNCH, hook_launch)
+ggzdmod.setHandler(ggzdmod.EVENT_STATE, hook_state)
 ggzdmod.setHandler(ggzdmod.EVENT_JOIN, hook_join)
 ggzdmod.setHandler(ggzdmod.EVENT_LEAVE, hook_leave)
-ggzdmod.setHandler(ggzdmod.EVENT_QUIT, hook_quit)
+ggzdmod.setHandler(ggzdmod.EVENT_LOG, hook_log)
 ggzdmod.setHandler(ggzdmod.EVENT_PLAYER, hook_player)
+ggzdmod.setHandler(ggzdmod.EVENT_ERROR, hook_error)
 
 print ">> launch the game server"
 ggzdmod.connect()
