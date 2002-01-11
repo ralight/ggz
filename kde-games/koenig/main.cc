49d8693193913f9628380d9c18e2d26307f2070f
dr_maux <devnull@localhost>
2002-01-11 03:38:14 -0800

    [svn] - prepare koenig for 0.0.5 - add copyrights - change URL to point to our chess page now

--- kde-games/koenig/main.cc
+++ kde-games/koenig/main.cc
@@ -1,3 +1,21 @@
+// Koenig - KDE client for the GGZ chess game
+// Copyright (C) 2001 Tobias König, tokoe82@yahoo.de
+// Copyright (C) 2001, 2002 Josef Spillner, dr_maux@users.sourceforge.net
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 #include <kaboutdata.h>
 #include <kapp.h>
 #include <kcmdlineargs.h>
@@ -15,14 +33,14 @@ int main(int argc, char **argv)
 {
 	KCmdLineArgs *args;
 	KAboutData aboutData("koenig",
-						"Koenig",
-						"0.0.1",
-						I18N_NOOP("GGZ Chess Frontend for KDE"),
-						KAboutData::License_GPL_V2,
-						"Copyright (C) 2001 Tobias Koenig & Josef Spillner",
-						NULL,
-						"http://www.geocities.com/linuxcode/kchess",
-						"dr_maux@users.sourceforge.net");
+		"Koenig",
+		"0.0.1",
+		I18N_NOOP("GGZ Chess Frontend for KDE"),
+		KAboutData::License_GPL_V2,
+		"Copyright (C) 2001, 2002 Tobias Koenig & Josef Spillner",
+		I18N_NOOP("This game is part of the GGZ Gaming Zone."),
+		"http://ggz.sourceforge.net/games/koenig/",
+		"dr_maux@users.sourceforge.net");
 
 	aboutData.addAuthor("Tobias Koenig", "Creator", "tokoe82@yahoo.de", "http://www.geocities.com/linuxcode");
 	aboutData.addAuthor("Josef Spillner", "Maintainer", "dr_maux@users.sourceforge.net", "http://mindx.sourceforge.net");
@@ -31,13 +49,11 @@ int main(int argc, char **argv)
 	KCmdLineArgs::addCmdLineOptions(op);
 	args = KCmdLineArgs::parsedArgs();
 
-	KApplication *app = new KApplication;
+	KApplication app;
 	TopLevel *toplevel = new TopLevel;
 	if(args->isSet("ggz")) toplevel->initGameSocket();
 	toplevel->show();
-	app->setMainWidget(toplevel);
-	int retval = app->exec();
-	delete app;
-	return retval;	
+	app.setMainWidget(toplevel);
+	return app.exec();
 }
 
