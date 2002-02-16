545473f401af56b6acda92f7b116cd888b20cfa4
bmh <devnull@localhost>
2002-02-15 19:04:36 -0800

    [svn] - Add GGZTableState to common header

--- libggz/src/ggz_common.h
+++ libggz/src/ggz_common.h
@@ -3,7 +3,7 @@
  * Author: GGZ Dev Team
  * Project: GGZ Common Library
  * Date: 01/13/2002
- * $Id: ggz_common.h 3227 2002-02-03 19:19:15Z jdorje $
+ * $Id: ggz_common.h 3367 2002-02-16 03:04:36Z bmh $
  *
  * This provides GGZ-specific functionality that is common to
  * some or all of the ggz-server, game-server, ggz-client, and
@@ -46,6 +46,22 @@ typedef enum {
 	GGZ_SEAT_PLAYER,	/**< The seat has a regular player in it. */
 	GGZ_SEAT_RESERVED,	/**< The seat is reserved for a player. */
 } GGZSeatType;
+
+
+/** @brief Table state values.
+ *
+ * GGZ tables take one of the following states. They are used by the
+ * GGZ client and GGZ server.  This is *not* necessarily the same as
+ * the game state kept by libggzdmod
+ */
+typedef enum {
+	GGZ_TABLE_ERROR   = -1,  /**< There is some error with the table */
+	GGZ_TABLE_CREATED = 0,   /**< Initial created state for the table. */
+	GGZ_TABLE_WAITING = 1,   /**< Waiting for enough players to join before playing */
+	GGZ_TABLE_PLAYING = 2,   /**< Playing a game */
+	GGZ_TABLE_DONE    = 3    /**< The game session is finished and the table will soon exit */
+} GGZTableState;
+	
 
 /** @brief Get a string identifier for the GGZSeatType.
  *
