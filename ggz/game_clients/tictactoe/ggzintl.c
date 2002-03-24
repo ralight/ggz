6dce554124b43042f49283a9c1b91335e18f4c11
dr_maux <devnull@localhost>
2002-03-24 09:10:28 -0800

    [svn] - use LOCALEDIR

--- ggz/game_clients/tictactoe/ggzintl.c
+++ ggz/game_clients/tictactoe/ggzintl.c
@@ -1,11 +1,13 @@
 #include "ggzintl.h"
 
+#include "config.h"
+
 #include <stdio.h>
 
 void ggz_intl_init(const char *modulename)
 {
 #ifdef ENABLE_NLS
-	bindtextdomain(modulename, NULL);
+	bindtextdomain(modulename, LOCALEDIR);
 	textdomain(modulename);
 	setlocale(LC_ALL, "");
 #endif
