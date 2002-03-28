1622a303580ea8c725908d9982a992c318da01d1
jdorje <devnull@localhost>
2002-03-28 00:27:20 -0800

    [svn] Added GGZ-style header comments to the (non-glade-controlled) callback files in Chess.

--- ggz/game_clients/chess/callbacks.h
+++ ggz/game_clients/chess/callbacks.h
@@ -1,3 +1,27 @@
+/*
+ * File: callbacks.h
+ * Author: Ismael Orenstein
+ * Project: GGZ Chess game module
+ * Desc: GTK Callback functions
+ * $Id: callbacks.h 3710 2002-03-28 08:27:20Z jdorje $
+ *
+ * Copyright (C) 2000-2002 Ismael Orenstein.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
 #include <gtk/gtk.h>
 #include "board.h"
 
