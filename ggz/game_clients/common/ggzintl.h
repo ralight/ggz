12fb23a3bdbaa5f6de123c70a99d13598c7c029a
jdorje <devnull@localhost>
2002-04-03 09:18:31 -0800

    [svn] - Added header comment to ggzintl.[ch]. - Moved "#include <config.h>" up to the top of ggzintl.c, so it'll be   included first and will affect all other inclusions. - Added "#ifdef HAVE_CONFIG_H" check. - Removed #inclusion of stdio.h, which does not seem necessary.

--- ggz/game_clients/common/ggzintl.h
+++ ggz/game_clients/common/ggzintl.h
@@ -1,3 +1,29 @@
+/*
+ * File: ggzintl.h
+ * Author: GGZ Development Team
+ * Project: GGZ Gtk Games
+ * $Id: ggzintl.h 3719 2002-04-03 17:18:31Z jdorje $
+ *
+ * This file contains common functions and data for i18n of the
+ * gtk-games package.
+ *
+ * Copyright (C) 2002 GGZ Development Team.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
 #ifndef GGZ_INTL_H
 #define GGZ_INTL_H
 
@@ -16,5 +42,5 @@
 
 void ggz_intl_init(const char *modulename);
 
-#endif
+#endif /* GGZ_INTL_H */
 
