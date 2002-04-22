5018e00497a56ca9a7b47dd353ae6c2cdc652652
jdorje <devnull@localhost>
2002-04-21 18:07:22 -0700

    [svn] Return the correct name in get_***_name functions, based on the cardset in use.

--- ggz/game_clients/ggzcards/common/cards.c
+++ ggz/game_clients/ggzcards/common/cards.c
@@ -4,7 +4,7 @@
  * Project: GGZCards Server/Client
  * Date: 02/25/2002
  * Desc: Card data for the GGZCards client and server
- * $Id: cards.c 4046 2002-04-22 00:04:41Z jdorje $
+ * $Id: cards.c 4047 2002-04-22 01:07:22Z jdorje $
  *
  * This contains card definitions common to both GGZCards client
  * and server.
@@ -54,100 +54,199 @@ const card_t UNKNOWN_CARD = {face: UNKNOWN_FACE,
                              suit: UNKNOWN_SUIT,
                              deck: UNKNOWN_DECK};
 
+/* FIXME: a better system for returning names is needed.  It should
+   be i18n'able, at least at the client. */
+
 const char *get_suit_name(char suit)
 {
-	switch (suit) {
-	case CLUBS:
-		return "clubs";
-	case DIAMONDS:
-		return "diamonds";
-	case HEARTS:
-		return "hearts";
-	case SPADES:
-		return "spades";
+	switch (cardset_type) {
+	case CARDSET_FRENCH:
+		switch (suit) {
+		case CLUBS:
+			return "clubs";
+		case DIAMONDS:
+			return "diamonds";
+		case HEARTS:
+			return "hearts";
+		case SPADES:
+			return "spades";
+		}
+		break;
+	case CARDSET_DOMINOES:
+		switch (suit) {
+		case 0:
+			return "blanks";
+		case 1:
+			return "ones";
+		case 2:
+			return "dueces";
+		case 3:
+			return "treys";
+		case 4:
+			return "fours";
+		case 5:
+			return "fives";
+		case 6:
+			return "sixes";
+		}
+		break;
+	case UNKNOWN_CARDSET:
+		break;
 	}
 	return "[invalid suit]";
 }
 
 const char *get_short_suit_name(char suit)
 {
-	switch (suit) {
-	case 0:
-		return "C";
-	case 1:
-		return "D";
-	case 2:
-		return "H";
-	case 3:
-		return "S";
+	switch (cardset_type) {
+	case CARDSET_FRENCH:
+		switch (suit) {
+		case CLUBS:
+			return "C";
+		case DIAMONDS:
+			return "D";
+		case HEARTS:
+			return "H";
+		case SPADES:
+			return "S";
+		}
+		break;
+	case CARDSET_DOMINOES:
+		switch (suit) {
+		case 0:
+			return "0";
+		case 1:
+			return "1";
+		case 2:
+			return "2";
+		case 3:
+			return "3";
+		case 4:
+			return "4";
+		case 5:
+			return "5";
+		case 6:
+			return "6";
+		}
+		break;
+	case UNKNOWN_CARDSET:
+		break;
 	}
 	return "X";
 }
 
 const char *get_face_name(char face)
 {
-	switch (face) {
-	case ACE_LOW:
-	case ACE_HIGH:
-		return "ace";
-	case JACK:
-		return "jack";
-	case QUEEN:
-		return "queen";
-	case KING:
-		return "king";
-	case 2:
-		return "two";
-	case 3:
-		return "three";
-	case 4:
-		return "four";
-	case 5:
-		return "five";
-	case 6:
-		return "six";
-	case 7:
-		return "seven";
-	case 8:
-		return "eight";
-	case 9:
-		return "nine";
-	case 10:
-		return "ten";
+	switch (cardset_type) {
+	case CARDSET_FRENCH:
+		switch (face) {
+		case ACE_LOW:
+		case ACE_HIGH:
+			return "ace";
+		case JACK:
+			return "jack";
+		case QUEEN:
+			return "queen";
+		case KING:
+			return "king";
+		case 2:
+			return "two";
+		case 3:
+			return "three";
+		case 4:
+			return "four";
+		case 5:
+			return "five";
+		case 6:
+			return "six";
+		case 7:
+			return "seven";
+		case 8:
+			return "eight";
+		case 9:
+			return "nine";
+		case 10:
+			return "ten";
+		}
+		break;
+	case CARDSET_DOMINOES:
+		switch (face) {
+		case 0:
+			return "blank";
+		case 1:
+			return "one";
+		case 2:
+			return "duece";
+		case 3:
+			return "trey";
+		case 4:
+			return "four";
+		case 5:
+			return "five";
+		case 6:
+			return "six";
+		}
+		break;
+	case UNKNOWN_CARDSET:
+		break;	
 	}
 	return "[unknown face]";
 }
 
 const char *get_short_face_name(char face)
 {
-	switch (face) {
-	case ACE_LOW:
-	case ACE_HIGH:
-		return "A";
-	case JACK:
-		return "J";
-	case QUEEN:
-		return "Q";
-	case KING:
-		return "K";
-	case 2:
-		return "2";
-	case 3:
-		return "3";
-	case 4:
-		return "4";
-	case 5:
-		return "5";
-	case 6:
-		return "6";
-	case 7:
-		return "7";
-	case 8:
-		return "8";
-	case 9:
-		return "9";
-	case 10:
-		return "10";
+	switch (cardset_type) {
+	case CARDSET_FRENCH:
+		switch (face) {
+		case ACE_LOW:
+		case ACE_HIGH:
+			return "A";
+		case JACK:
+			return "J";
+		case QUEEN:
+			return "Q";
+		case KING:
+			return "K";
+		case 2:
+			return "2";
+		case 3:
+			return "3";
+		case 4:
+			return "4";
+		case 5:
+			return "5";
+		case 6:
+			return "6";
+		case 7:
+			return "7";
+		case 8:
+			return "8";
+		case 9:
+			return "9";
+		case 10:
+			return "10";
+		}
+		break;
+	case CARDSET_DOMINOES:
+		switch (face) {
+		case 0:
+			return "0";
+		case 1:
+			return "1";
+		case 2:
+			return "2";
+		case 3:
+			return "3";
+		case 4:
+			return "4";
+		case 5:
+			return "5";
+		case 6:
+			return "6";
+		}
+		break;
+	case UNKNOWN_CARDSET:
+		break;	
 	}
 	return "X";
 }
