7853068656cdd78b308c727ba048c4ddf337dc87
dr_maux <devnull@localhost>
2002-04-24 08:15:11 -0700

    [svn] - move the not yet complete ggzcommgen to the utils module

---
+++ utils/ggzcomm/ggzcommgen.h
@@ -0,0 +1,7 @@
+#ifndef GGZCOMMGEN_H
+#define GGZCOMMGEN_H
+
+void ggzcommgen_generate(const char *protocol, const char *task, const char *format, const char *library, const char *mode);
+
+#endif
+
