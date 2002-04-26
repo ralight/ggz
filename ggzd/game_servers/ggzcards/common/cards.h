b0b895a5dcee236ec7691c51f41e98fbbd46831b
jdorje <devnull@localhost>
2002-04-25 23:23:13 -0700

    [svn] Copied over changed code from the gtk-games module.

--- ggzd/game_servers/ggzcards/common/cards.h
+++ ggzd/game_servers/ggzcards/common/cards.h
@@ -4,7 +4,7 @@
  * Project: GGZCards Server/Client
  * Date: 02/25/2002
  * Desc: Card data for the GGZCards client and server
- * $Id: cards.h 4046 2002-04-22 00:04:41Z jdorje $
+ * $Id: cards.h 4084 2002-04-26 06:23:13Z jdorje $
  *
  * This contains card definitions common to both GGZCards client
  * and server.
@@ -105,6 +105,8 @@ typedef struct card_t {
 	/** The deck number of the card.
 	 *  @see card_deck_enum */
 	char deck;
+	/** Can be used to store any transitory meta-data. */
+	char meta;
 } card_t;
 
 const char *get_suit_name(char suit);
