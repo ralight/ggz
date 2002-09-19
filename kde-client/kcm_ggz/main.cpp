12e8c7b24e21a0c31783e7dac769eb5b7fb83aba
dr_maux <devnull@localhost>
2002-09-19 09:24:56 -0700

    [svn] - use stock KAboutData so KInstance doesn't whine

--- kde-client/kcm_ggz/main.cpp
+++ kde-client/kcm_ggz/main.cpp
@@ -1,16 +1,25 @@
 #include <kapplication.h>
 #include <kcmdlineargs.h>
 #include <klocale.h>
+#include <kaboutdata.h>
+
 #include "kcm_ggz.h"
 
 int main(int argc, char **argv)
 {
 	KCMGGZ *kcmggz;
+	KAboutData about("kcmggz_test",
+		"KCMGGZ test program",
+		"0.0",
+		"Blah",
+		KAboutData::License_GPL,
+		"Blah",
+		"Blah");
 
-	KCmdLineArgs::init(argc, argv, "kcmggz", "", "");
+	KCmdLineArgs::init(argc, argv, &about);
 
 	KApplication a;
-	kcmggz = new KCMGGZ();
+	kcmggz = new KCMGGZ(NULL, "kcmggz");
 	a.setMainWidget(kcmggz);
 	kcmggz->show();
 	kcmggz->resize(500, 500);
