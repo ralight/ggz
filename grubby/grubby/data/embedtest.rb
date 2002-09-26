ad41b9d1de345d6593e81c37e1dae32d99020425
dr_maux <devnull@localhost>
2002-09-26 11:17:47 -0700

    [svn] - shut up debug information in mod_embed test script

--- grubby/grubby/data/embedtest.rb
+++ grubby/grubby/data/embedtest.rb
@@ -1,5 +1,7 @@
-puts "Hello World."
-puts "The message I got is: <" + $answer[0] + ">"
+# Test program simulating the capabilities of grubby's mod_embed
+
+#puts "Hello World."
+#puts "The message I got is: <" + $answer[0] + ">"
 $answer[0].gsub!(/[0-9]/, '***')
-puts "Let's return <" + $answer[0] + ">"
+#puts "Let's return <" + $answer[0] + ">"
 
