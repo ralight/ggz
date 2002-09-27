44893c10b8950a3471fc77a22bb10c236fb7c00f
dr_maux <devnull@localhost>
2002-09-27 01:58:31 -0700

    [svn] - new autogen

--- kde-client/autogen.sh
+++ kde-client/autogen.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+# GGZ Gaming Zone bootstrap script
+# To be used for CVS checkouts
 
 srcdir=`dirname $0`
-
 DIE=0
 
+if test -z "$*"; then
+  echo "** Warning: I am going to run 'configure' with no arguments."
+  echo "** If you wish to pass any to it, please specify them on the"
+  echo "** '$0' command line."
+  echo
+fi
+
+echo -n "Bootstrapping GGZ... "
+echo -n "[prerequisites]"
+
+# Make sure autoconf is installed
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
   echo
   echo "**Error**: You must have \`autoconf' installed to."
@@ -12,46 +24,35 @@ DIE=0
   DIE=1
 }
 
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`libtool' installed."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
+# Make sure libtool is installed
+(grep "^AM_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
+  (libtool --version) < /dev/null > /dev/null 2>&1 && need_libtool=1 || {
+    echo
+    echo "**Error**: You must have \`libtool' installed."
+    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.4.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+  }
 }
 
-(gettext --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`gettext' installed."
-  echo "Get ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-}
-
+# Make sure automake is installed
 (automake --version) < /dev/null > /dev/null 2>&1 || {
   echo
   echo "**Error**: You must have \`automake' installed."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.5.tar.gz"
   echo "(or a newer version if it is available)"
   DIE=1
   NO_AUTOMAKE=yes
 }
 
+
+# if no automake, don't bother testing for aclocal
 test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
   echo
   echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
   echo "installed doesn't appear recent enough."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.5.tar.gz"
   echo "(or a newer version if it is available)"
-  DIE=1
-}
-
-(perl --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`perl'. You need perl to extract"
-  echo "the meta objects information from the header files."
-  echo "This restriction does only apply to CVS sources."
-  echo "Get perl from http://www.perl.com."
   DIE=1
 }
 
@@ -59,71 +60,30 @@ if test "$DIE" -eq 1; then
   exit 1
 fi
 
-if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
-  echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
-  echo
+
+echo -n "[m4]"
+rm -f $srcdir/acinclude.m4
+if test -f $srcdir/acinclude.ggz; then
+	cat $srcdir/acinclude.ggz >> $srcdir/acinclude.m4;
 fi
+if test -f $srcdir/acinclude.kde; then
+	cat $srcdir/acinclude.kde >> $srcdir/acinclude.m4;
+fi
+if test "x$need_libtool" = "x1"; then
+	echo -n "[libtoolize]"
+	(cd $srcdir && libtoolize --force --copy) || { echo "libtoolize failed."; exit; }
+fi
+echo -n "[aclocal]"
+(cd $srcdir && aclocal) || { echo "aclocal failed."; exit; }
+echo -n "[autoheader]"
+autoheader -l $srcdir || { echo "autoheader failed."; exit; }
+echo -n "[automake]"
+(cd $srcdir && automake --add-missing --gnu 2>/dev/null) || { echo "automake failed."; exit; }
+echo -n "[autoconf]"
+autoconf -l $srcdir $srcdir/configure.in > $srcdir/configure && chmod +x $srcdir/configure || { echo "autoconf failed."; exit; }
+echo ""
 
-case $CC in
-xlc )
-  am_opt=--include-deps;;
-esac
-
-echo "Creating autoconf KDE and GGZ macros..."
-cp acinclude.kde acinclude.m4
-# SuSE sucks here
-chmod +w acinclude.m4
-cat acinclude.ggz >> acinclude.m4
-
-for coin in `find $srcdir -name configure.in -print`
-do 
-  dr=`dirname $coin`
-  if test -f $dr/NO-AUTO-GEN; then
-    echo skipping $dr -- flagged as no auto-gen
-  else
-    echo processing $dr
-    macrodirs=`sed -n -e 's,AM_ACLOCAL_INCLUDE(\(.*\)),\1,gp' < $coin`
-    ( cd $dr
-      aclocalinclude="$ACLOCAL_FLAGS"
-      for k in $macrodirs; do
-  	if test -d $k; then
-          aclocalinclude="$aclocalinclude -I $k"
-        fi
-      done
-      if grep "^AM_GNU_GETTEXT" configure.in >/dev/null; then
-	if grep "sed.*POTFILES" configure.in >/dev/null; then
-	  : do nothing -- we still have an old unmodified configure.in
-	else
-	  echo "Creating $dr/aclocal.m4 ..."
-	  test -r $dr/aclocal.m4 || touch $dr/aclocal.m4
-	  echo "Running gettextize...  Ignore non-fatal messages."
-	  echo "no" | gettextize --force --copy
-	  echo "Making $dr/aclocal.m4 writable ..."
-	  test -r $dr/aclocal.m4 && chmod u+w $dr/aclocal.m4
-        fi
-      fi
-      if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
-	echo "Running libtoolize..."
-	libtoolize --force --copy
-      fi
-      echo "Running aclocal $aclocalinclude ..."
-      aclocal $aclocalinclude
-      if grep "^AM_CONFIG_HEADER" configure.in >/dev/null; then
-	echo "Running autoheader..."
-	autoheader
-      fi
-      echo "Running automake --gnu $am_opt ..."
-      automake --add-missing --gnu $am_opt
-      echo "Running autoconf ..."
-      autoconf
-      echo "Expanding Qt meta objects information"
-      perl am_edit --foreign-libtool
-	  #perl am_edit
-    )
-  fi
-done
+#conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
 
 if test x$NOCONFIGURE = x; then
   echo Running $srcdir/configure $conf_flags "$@" ...
@@ -131,3 +91,4 @@ if test x$NOCONFIGURE = x; then
 else
   echo Skipping configure process.
 fi
+
