5b4af0e8525354fa7094b595155b4d4168e44b61
jdorje <devnull@localhost>
2002-10-27 15:16:47 -0800

    [svn] Restructure stats code to break off the individual stats calculations.

--- ggzd/ggzd/stats.c
+++ ggzd/ggzd/stats.c
@@ -4,7 +4,7 @@
  * Project: GGZ Server
  * Date: 10/27/2002
  * Desc: Functions for calculating statistics
- * $Id: stats.c 5071 2002-10-27 23:04:44Z jdorje $
+ * $Id: stats.c 5072 2002-10-27 23:16:47Z jdorje $
  *
  * Copyright (C) 2002 GGZ Development Team.
  *
@@ -46,12 +46,33 @@
 /* Server wide data structures*/
 extern struct GameInfo game_types[MAX_GAME_TYPES];
 
+static void calculate_records(ggzdbPlayerGameStats *stats,
+			      GGZdModGameReportData *report)
+{
+	int i;
+
+	for (i = 0; i < report->num_players; i++) {
+		switch (report->results[i]) {
+		case GGZ_GAME_WIN:
+			stats[i].wins++;
+			break;
+		case GGZ_GAME_LOSS:
+			stats[i].losses++;
+			break;
+		case GGZ_GAME_TIE:
+			stats[i].ties++;
+			break;
+		}
+	}
+}
+
 void report_statistics(int room, int gametype,
 		       GGZdModGameReportData *report)
 {
 	int i;
 	char game_name[MAX_GAME_NAME_LEN + 1];
 	unsigned char records;
+	ggzdbPlayerGameStats stats[report->num_players];
 
 	pthread_rwlock_rdlock(&game_types[gametype].lock);
 	strcpy(game_name, game_types[gametype].name);
@@ -62,45 +83,43 @@ void report_statistics(int room, int gametype,
 	if (!records)
 		return;
 
+	/* Retrieve the old stats. */
 	for (i = 0; i < report->num_players; i++) {
-		ggzdbPlayerGameStats stats;
 		GGZDBResult status;
-		const char *name = report->names[i];
-		GGZPlayer *player;
 
-		strcpy(stats.player, name);
-		strcpy(stats.game, game_name);
-		status = ggzdb_stats_lookup(&stats);
+		strcpy(stats[i].player, report->names[i]);
+		strcpy(stats[i].game, game_name);
+
+		status = ggzdb_stats_lookup(&stats[i]);
 
 		/* NOT_FOUND case is handled in lookup */
 		if (status != GGZDB_NO_ERROR) {
 			err_msg("DB error %d in table_game_report", status);
-			continue;
+			return;
 		}
+	}
 
-		/* There's a potential threading problem here, but in
-		   practice it shouldn't hurt.  Since we look up the
-		   stats, then change them, then write them back, if
-		   someone else tries to change them during this time
-		   someone's data will be lost.  But as long as players
-		   stats only change when *they* play, this will only
-		   be a problem for bots - an acceptable tradeoff. */
+	/* There's a potential threading problem here, but in
+	   practice it shouldn't hurt.  Since we look up the
+	   stats, then change them, then write them back, if
+	   someone else tries to change them during this time
+	   someone's data will be lost.  But as long as players
+	   stats only change when *they* play, this will only
+	   be a problem for bots - an acceptable tradeoff. */
 
-		if (records) {
-			switch (report->results[i]) {
-			case GGZ_GAME_WIN:
-				stats.wins++;
-				break;
-			case GGZ_GAME_LOSS:
-				stats.losses++;
-				break;
-			case GGZ_GAME_TIE:
-				stats.ties++;
-				break;
-			}
-		}
+	/* Calculate stats */
+	if (records)
+		calculate_records(stats, report);
 
-		ggzdb_stats_update(&stats);
+	/* Rewrite the stats to the database. */
+	for (i = 0; i < report->num_players; i++) {
+		ggzdb_stats_update(&stats[i]);
+	}
+
+	/* Queue updates for people in the room. */
+	for (i = 0; i < report->num_players; i++) {
+		const char *name = report->names[i];
+		GGZPlayer *player;
 
 		/* Send an update to the room. */
 		/* This isn't particularly elegant, but it should work at
@@ -115,9 +134,9 @@ void report_statistics(int room, int gametype,
 		}
 		pthread_rwlock_wrlock(&player->stats_lock);
 		if (records) {
-			player->wins = stats.wins;
-			player->losses = stats.losses;
-			player->ties = stats.ties;
+			player->wins = stats[i].wins;
+			player->losses = stats[i].losses;
+			player->ties = stats[i].ties;
 		}
 		pthread_rwlock_unlock(&player->stats_lock);
 		pthread_rwlock_unlock(&player->lock);
