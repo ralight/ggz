32638c7fa1aed782a6e0c461ea067b45f946ed8b
dr_maux <devnull@localhost>
2002-11-02 05:55:24 -0800

    [svn] - enumerate protocol opcodes

--- ggzd/game_servers/geekgame/protocol.h
+++ ggzd/game_servers/geekgame/protocol.h
@@ -3,13 +3,13 @@
 
 enum NetworkOpcodes
 {
-	op_server_greeting,
-	op_server_newplayer,
-	op_server_gamestart,
-	op_server_gamestop,
-	op_server_gameend,
+	op_server_greeting  = 100,
+	op_server_newplayer = 101,
+	op_server_gamestart = 102,
+	op_server_gamestop  = 103,
+	op_server_gameend   = 104,
 
-	op_client_presentation
+	op_client_presentation = 200
 };
 
 #endif
