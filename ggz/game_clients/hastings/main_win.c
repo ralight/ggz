4a371624a1fb8f01cd53781d4d63c3a1c95f36b5
dr_maux <devnull@localhost>
2002-11-17 08:57:55 -0800

    [svn] - set white border colour of hexagons explicitely to avoid drawing errors

--- ggz/game_clients/hastings/main_win.c
+++ ggz/game_clients/hastings/main_win.c
@@ -5,7 +5,7 @@
  * Project: GGZ Hastings1066 game module
  * Date: 09/13/00
  * Desc: Main window creation and callbacks
- * $Id: main_win.c 5233 2002-11-06 22:11:22Z dr_maux $
+ * $Id: main_win.c 5251 2002-11-17 16:57:55Z dr_maux $
  *
  * Copyright (C) 2000 - 2002 Josef Spillner
  *
@@ -63,7 +63,7 @@
 #include <frame_ll.xpm>
 #include <frame_lr.xpm>
 #include <frame_black.xpm>
-#include <bayeux.xpm>/*<map.xpm>*/
+#include /*<bayeux.xpm>*/<map.xpm>
 #include <shadow.xpm>
 
 /* GGZ includes */
@@ -167,9 +167,17 @@ static void hexagon(GtkWidget *widget, int offsetx, int offsety)
 	const int radius = 30;
 	int radx;
 	int rady;
+	GdkColor colour;
 
 	radx = (int)(3.0 * (float)radius / 6.0);
 	rady = (int)(5.0 * (float)radius / 6.0);
+
+	colour.pixel = 0;
+	colour.red = 0xFFFF;
+	colour.green = 0xFFFF;
+	colour.blue = 0xFFFF;
+	gdk_gc_set_foreground(widget->style->black_gc, &colour);
+	/*gdk_gc_set_background(widget->style->black_gc, &colour);*/
 
 	gdk_draw_line(hastings_buf, widget->style->white_gc,
 		offsetx - radx, offsety - rady, offsetx + radx, offsety - rady);
@@ -300,7 +308,7 @@ static void on_main_win_realize(GtkWidget* widget, gpointer user_data)
 		&mask, &style->bg[GTK_STATE_NORMAL], (gchar**)frame_black_xpm);
 
 	map_pix = gdk_pixmap_create_from_xpm_d(main_win->window,
-		&mask, &style->bg[GTK_STATE_NORMAL], (gchar**)/*map_xpm*/bayeux_xpm);
+		&mask, &style->bg[GTK_STATE_NORMAL], (gchar**)map_xpm/*bayeux_xpm*/);
 }
 
 /* Quit the game */
