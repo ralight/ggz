cf86f4c928f197fc8f6afa30eac56be55147d1b4
dr_maux <devnull@localhost>
2003-02-16 06:30:54 -0800

    [svn] - shut up compiler warnings

--- ggzd/game_servers/muehle/qtserv/qlist.h
+++ ggzd/game_servers/muehle/qtserv/qlist.h
@@ -10,8 +10,8 @@ template<class T> class QList : public std::list<T>
 		~QList(){m_list.clear();}
 		void append(T* t){m_list.push_back(*t);}
 		void setAutoDelete(bool del){m_autodelete=del;}
-		T* first() {T* foo; *foo = m_list.front(); return foo;}
-		T* next() {T* bar; *bar = m_list.back(); return bar;}
+		T* first() {T* foo = NULL; *foo = m_list.front(); return foo;}
+		T* next() {T* bar = NULL; *bar = m_list.back(); return bar;}
 	private:
 		std::list<T> m_list;
 		bool m_autodelete;
