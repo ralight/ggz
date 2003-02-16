03aeac2bd3358a3b526e07b69e5e81a43f6bde60
dr_maux <devnull@localhost>
2003-02-16 05:02:09 -0800

    [svn] - new autogen.sh

--- grubby/autogen.sh
+++ grubby/autogen.sh
@@ -15,54 +15,66 @@ fi
 echo -n "Bootstrapping GGZ... "
 echo -n "[prerequisites]"
 
-# Make sure autoconf is installed
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+# Make sure autotools are installed
+
+version_check()
+{
+log="$log\t++ Checking for $1,"
+($1 --version >/dev/null 2>&1) || { log="$log not found!\n"; bailout=1; }
+vline=`$1 --version | head -1`
+needed="$2"
+
+version=`echo $vline | sed 's/^[A-z-\.\ ()]*//;s/ .*$//'`
+vmajor="0`echo $version | cut -d . -f 1`"
+vminor="0`echo $version | cut -s -d . -f 2`"
+vmicro="0`echo $version | cut -s -d . -f 3`"
+nmajor="0`echo $needed | cut -d . -f 1`"
+nminor="0`echo $needed | cut -s -d . -f 2`"
+nmicro="0`echo $needed | cut -s -d . -f 3`"
+
+old=0
+if [ $vmajor -lt $nmajor ]; then old=1;
+elif [ $vminor -lt $nminor ]; then old=1;
+elif [ $vmicro -lt $nmicro ]; then old=1;
+fi
+
+vversion=`echo "$vmajor.$vminor.$vmicro" | perl -e "\\$_ = <STDIN>; \\$_ =~ s/(^|\.)0(\d+)/\\$1\\$2/g; print"`
+nversion=`echo "$nmajor.$nminor.$nmicro" | perl -e "\\$_ = <STDIN>; \\$_ =~ s/(^|\.)0(\d+)/\\$1\\$2/g; print"`
+
+log="$log need $nversion, found $vversion -"
+if test $old = "1"; then
+  log="$log too old!\n"
+  bailout=1
+else
+  log="$log ok.\n"
+fi
+}
+
+log=""
+bailout=0
+
+version_check "autoconf" "2.5"
+version_check "automake" "1.5"
+version_check "libtool" "1.4.3"
+version_check "gettext" "0.11"
+
+if test "x$bailout" = "x1"; then
   echo
-  echo "**Error**: You must have \`autoconf' installed to."
-  echo "Download the appropriate package for your distribution,"
-  echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-  DIE=1
-}
-
-# Make sure libtool is installed
-(grep "^AM_PROG_LIBTOOL" $srcdir/configure.ac >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 && need_libtool=1 || {
-    echo
-    echo "**Error**: You must have \`libtool' installed."
-    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.4.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
-  }
-}
-
-# Make sure automake is installed
-(automake --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: You must have \`automake' installed."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.5.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-  NO_AUTOMAKE=yes
-}
-
-
-# if no automake, don't bother testing for aclocal
-test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
-  echo
-  echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
-  echo "installed doesn't appear recent enough."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.5.tar.gz"
-  echo "(or a newer version if it is available)"
-  DIE=1
-}
+  echo "** Error: some prerequisites could not be found:"
+  echo -e $log
+  exit
+fi
 
 if test "$DIE" -eq 1; then
   exit 1
 fi
 
+# Run autotools suite
 
-echo -n "[m4]"
-cat $srcdir/m4/*.m4 > $srcdir/acinclude.m4
+if test -d $srcdir/m4; then
+	echo -n "[m4]"
+	cat $srcdir/m4/*.m4 > $srcdir/acinclude.m4
+fi
 if test "x$need_libtool" = "x1"; then
 	echo -n "[libtoolize]"
 	(cd $srcdir && libtoolize --force --copy) || { echo "libtoolize failed."; exit; }
@@ -72,16 +84,16 @@ echo -n "[aclocal]"
 echo -n "[autoheader]"
 autoheader -I $srcdir || { echo "autoheader failed."; exit; }
 echo -n "[automake]"
-(cd $srcdir && automake --add-missing --gnu 2>/dev/null) || { echo "automake failed."; exit; }
-echo -n "[autoconf]"
+(cd $srcdir && automake --add-missing --gnu) || { echo "automake failed."; exit; }
 if test -f $srcdir/am_edit; then
 	echo -n "[am_edit]"
 	perl $srcdir/am_edit --foreign-libtool || { echo "am_edit failed."; exit; }
 fi
+echo -n "[autoconf]"
 autoconf -I $srcdir $srcdir/configure.ac > $srcdir/configure && chmod +x $srcdir/configure || { echo "autoconf failed."; exit; }
 echo ""
 
-#conf_flags="--enable-maintainer-mode --enable-compile-warnings" #--enable-iso-c
+# Run configuration
 
 if test x$NOCONFIGURE = x; then
   echo Running $srcdir/configure $conf_flags "$@" ...
