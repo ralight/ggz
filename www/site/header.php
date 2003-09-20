ff7acf6ea6e9cecc62ae6e2543602278867b56ce
dr_maux <devnull@localhost>
2003-09-20 02:08:12 -0700

    [svn] - smart translation of header and footer bar, so all pages can now be fully   i18n'ed - port of screenshots from old homepage - translation of 2 game pages

--- www/site/header.php
+++ www/site/header.php
@@ -1,108 +1,16 @@
-<html>
-<head>
-<title>
-GGZ Gaming Zone
-</title>
-<style type="text/css">
-.pane td {
-	background-color: #eeeeee;
-	background-image: url(/pics/site/bar.png);
-	background-repeat: no-repeat;
-	font-weight: bold;
-	text-align: center;
-	border-width: 1px;
-	border-color: #000000;
-	border-style: solid;
+<?php
 
+$lang = "en";
+$ar = split(", ", $HTTP_ACCEPT_LANGUAGE);
+
+foreach ($ar as $l)
+{
+	if(file_exists("$DOCUMENT_ROOT/site/header.php.$l")) :
+		$lang = $l;
+		break;
+	endif;
 }
-.content td {
-	background-color: #eeeeee;
-	background-image: url(/pics/site/bar.png);
-	background-repeat: no-repeat;
-	/*font-weight: bold;*/
-	/*text-align: center;*/
-	border-width: 1px;
-	border-color: #000000;
-	border-style: solid;
 
-	text-align: left;
-	padding: 5px;
-}
-.inner td {
-	background-color: #eeeeee;
-	/*background-image: url(/pics/site/bar.png);*/
-	background-repeat: no-repeat;
-	/*font-weight: bold;*/
-	/*text-align: center;*/
-	/*border-width: 1px;*/
-	border-color: #000000;
-	border-style: solid;
+include("header.php.$lang");
 
-	text-align: left;
-	border-width: 0px;
-	background-image: none;
-}
-.list hr {
-	margin: 4px;
-}
-.list {
-	line-height: 0px;
-	border-spacing: 0px;
-}
-.list td {
-	background-color: #eeeeee;
-	background-image: url(/pics/site/bar.png);
-	background-repeat: no-repeat;
-	/*font-weight: bold;*/
-	/*text-align: center;*/
-	border-width: 1px;
-	border-color: #000000;
-	border-style: solid;
-
-	text-align: center;
-	line-height: 5px;
-}
-.flag {
-	background-color: #ffffff;
-	vertical-align: center;
-	border-width: 0px;
-	border-color: #000000;
-	border-style: solid;
-	width: 16;
-}
-</style>
-</head>
-<body bgcolor="#dcdcdc" background="/pics/site/bg.png" text="#000000" link="#000000" vlink="#000000" alink="#000000">
-
-<table class="pane" width="100%">
-<tr>
-<td>
-<font size="6">
-<img src="/pics/site/ggzlogo.png" alt="GGZ logo" valign="middle">
-&nbsp;&nbsp;
-GGZ Gaming Zone
-</font>
-</td>
-</tr>
-</table>
-
-<table class="pane" width="100%">
-<tr>
-<td>
-<font size="3">
-<a href="/index.php">Front page</a>
-|
-<a href="/games.php">Games and Applications</a>
-|
-<a href="/developers.php">Developers</a>
-|
-<a href="/rankings.php">Rankings</a>
-|
-<a href="/live.php">Live Games</a>
-</font>
-</td>
-</tr>
-</table>
-
-<br>
-
+?>
---
+++ www/site/style.php
@@ -0,0 +1,68 @@
+<style type="text/css">
+.pane td {
+	background-color: #eeeeee;
+	background-image: url(/pics/site/bar.png);
+	background-repeat: no-repeat;
+	font-weight: bold;
+	text-align: center;
+	border-width: 1px;
+	border-color: #000000;
+	border-style: solid;
+
+}
+.content td {
+	background-color: #eeeeee;
+	background-image: url(/pics/site/bar.png);
+	background-repeat: no-repeat;
+	/*font-weight: bold;*/
+	/*text-align: center;*/
+	border-width: 1px;
+	border-color: #000000;
+	border-style: solid;
+
+	text-align: left;
+	padding: 5px;
+}
+.inner td {
+	background-color: #eeeeee;
+	/*background-image: url(/pics/site/bar.png);*/
+	background-repeat: no-repeat;
+	/*font-weight: bold;*/
+	/*text-align: center;*/
+	/*border-width: 1px;*/
+	border-color: #000000;
+	border-style: solid;
+
+	text-align: left;
+	border-width: 0px;
+	background-image: none;
+}
+.list hr {
+	margin: 4px;
+}
+.list {
+	line-height: 0px;
+	border-spacing: 0px;
+}
+.list td {
+	background-color: #eeeeee;
+	background-image: url(/pics/site/bar.png);
+	background-repeat: no-repeat;
+	/*font-weight: bold;*/
+	/*text-align: center;*/
+	border-width: 1px;
+	border-color: #000000;
+	border-style: solid;
+
+	text-align: center;
+	line-height: 5px;
+}
+.flag {
+	background-color: #ffffff;
+	vertical-align: center;
+	border-width: 0px;
+	border-color: #000000;
+	border-style: solid;
+	width: 16;
+}
+</style>
