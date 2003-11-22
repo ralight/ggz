80534e6c8b6fba837e35993510a432c4c98cc3c5
dr_maux <devnull@localhost>
2003-11-22 12:11:02 -0800

    [svn] More web pages in English and German :-) - grubby and the db2web stuff - games: escape, keepalive, kamikaze, geekgame - tools: ggzap, motd-editor, modsniff, compcal

--- www/site/error.php
+++ www/site/error.php
@@ -1,9 +1,9 @@
-<? include_once("/var/www/beta/site/header.php"); ?>
+<? include_once("/var/www/ggz/beta/site/header.php"); ?>
 
 The page you requested could not been found :(
 <br>
 <a href="<?=$HTTP_REFERER;?>">Go back</a>
 <br>
 
-<? include_once("/var/www/beta/site/footer.php"); ?>
+<? include_once("/var/www/ggz/beta/site/footer.php"); ?>
 
