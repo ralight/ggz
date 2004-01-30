98ad797a66d912342b4a2598926433538b95274e
josef <devnull@localhost>
2004-01-30 01:53:28 -0800

    [svn] - change minor version check from == 1 to >= 1
    This makes it work with all of libdb 4.0, 4.1 and 4.2 now.

--- ggzd/ggzd/database/ggzdb_db4.c
+++ ggzd/ggzd/database/ggzdb_db4.c
@@ -4,7 +4,7 @@
  * Project: GGZ Server
  * Date: 11/10/2000
  * Desc: Back-end functions for handling the db4 sytle database
- * $Id: ggzdb_db4.c 5750 2004-01-27 13:11:28Z josef $
+ * $Id: ggzdb_db4.c 5762 2004-01-30 09:53:28Z josef $
  *
  * Copyright (C) 2000 Brent Hendricks.
  *
@@ -61,7 +61,7 @@ GGZReturn _ggzdb_init(ggzdbConnection connection, int set_standalone)
 		standalone = 1;
 	} else
 		flags = DB_CREATE | DB_INIT_MPOOL | DB_INIT_LOCK | DB_THREAD;
-#if DB_VERSION_MINOR == 1
+#if DB_VERSION_MINOR >= 1
 	if (db_env_create(&db_e, 0) != 0) {
 		err_sys("db_env_create_4001() failed in _ggzdb_init()");
 #else
@@ -120,7 +120,7 @@ GGZDBResult _ggzdb_init_player(char *datadir)
 	/* Open the database file */
 	if(db_create(&db_p, db_e, 0) != 0)
 		err_sys_exit("db_create() failed in _ggzdb_init_player()");
-#if DB_VERSION_MINOR == 1
+#if DB_VERSION_MINOR >= 1
 	/*flags |= DB_AUTO_COMMIT;*/
 	if (db_p->open(db_p, NULL, "player.db", NULL, DB_BTREE, flags, 0600) != 0)
 #else
@@ -338,7 +338,7 @@ GGZDBResult _ggzdb_init_stats(ggzdbConnection connection)
 	/* Open the database file */
 	if (db_create(&db_s, db_e, 0) != 0)
 		err_sys_exit("db_create() failed in _ggzdb_init_stats()");
-#if DB_VERSION_MINOR == 1
+#if DB_VERSION_MINOR >= 1
 	/*flags |= DB_AUTO_COMMIT;*/
 	if (db_p->open(db_s, NULL, "stats.db", NULL, DB_BTREE, flags, 0600) != 0)
 #else
