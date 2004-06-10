a4ca7922778efb97e6e3cc7d77fda1930b6b17ec
josef <devnull@localhost>
2004-06-10 12:41:34 -0700

    [svn] - missingtranslation tool updates:  * find all files automatically, no "find . | ..." needed anymore  * ability to create foo.php.php files automatically    This is a workaround for apache's inability to define language priorities - little help file with tips for webmasters, should anyone care... - readme explaining what all the stuff is about

--- playground/maintenance/missingtranslation.pl
+++ playground/maintenance/missingtranslation.pl
@@ -1,19 +1,46 @@
 #!/usr/bin/perl
 #
-# call: find . | ./missingtranslation.pl
+# call: ./missingtranslation.pl in webroot
+# content negotiation fix: add --defaults
 
 use strict;
 
 my @files;
+my @allfiles;
 my %langs;
 my %missing;
+my $defaults;
 
-while(<STDIN>){
-	chomp;
-	if(/\.php\./){
-		push @files, $_;
-		my $lang = $_;
-		$lang =~ s/[a-z0-9\.\/]+\.php\.//;
+if($ARGV[0] eq "--defaults"){
+	$defaults = 1;
+	$langs{"php"} = 1;
+}
+
+sub dirlist{
+	my $dir = shift(@_);
+	my @paths;
+
+	opendir(DIR, $dir);
+	while(my $entry = readdir(DIR)){
+		push @paths, "$dir/$entry";
+	}
+	closedir(DIR);
+	foreach my $path(@paths){
+		if(-f $path){
+			push @allfiles, $path;
+		}elsif(!($path =~ /\/*\.+$/)){
+			dirlist($path);
+		}
+	}
+}
+
+dirlist(".");
+
+foreach my $file(@allfiles){
+	if($file =~ /\.php\./){
+		push @files, $file;
+		my $lang = $file;
+		$lang =~ s/[a-z0-9\.\/\-]+\.php\.//;
 		$langs{$lang} = 1;
 		#print "add $lang\n";
 	}
@@ -23,17 +50,36 @@ foreach my $file(sort(@files)){
 	#print "* $file\n";
 	my $lang = $file;
 	my $base = $file;
-	$lang =~ s/[a-z0-9\.\/]+\.php\.//;
-	$base =~ s/\.[a-z]+$//;
+	$lang =~ s/[a-z0-9\.\/\-]+\.php\.//;
+	$base =~ s/\.[a-z\-]+$//;
 	#print "($base :: $lang)\n";
 	$missing{$base}{$lang} = 1;
 }
 
-foreach my $base(keys(%missing)){
+foreach my $base(sort(keys(%missing))){
+	if(!$missing{$base}{"php"}){
+		my $slang = "";
+		if($missing{$base}{"en"}){
+			$slang = "en";
+		}else{
+			foreach my $lang(keys(%langs)){
+				if($missing{$base}{$lang}){
+					$slang = $lang;
+				}
+			}
+		}
+		print "# generating: $base default from $slang\n";
+		system("cp $base.$slang $base.php");
+	}
+}
+
+foreach my $base(sort(keys(%missing))){
+	print "# missing: $base ( ";
 	foreach my $lang(keys(%langs)){
 		if(!$missing{$base}{$lang}){
-			print "# missing: $lang for $base\n";
+			print "$lang ";
 		}
 	}
+	print ")\n";
 }
 
