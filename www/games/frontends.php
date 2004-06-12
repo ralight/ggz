64d2225c943b50e37cc2227338365757242e4fd4
josef <devnull@localhost>
2004-06-12 09:00:29 -0700

    [svn] - new index page for all games (not very pretty yet though) - game frontend redirector: if /games/tttxt/ is received, offer /games/tictactoe/   unless TTTXT gets its own homepage etc.

---
+++ www/games/frontends.php
@@ -0,0 +1,33 @@
+<?php
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$uri = preg_replace("/\/games\//", "", $uri);
+$uri = str_replace("/", "", $uri);
+
+$origuri = $uri;
+switch($uri)
+{
+	case "ktictactux": $uri = "tictactoe"; break;
+	case "ttt3d": $uri = "tictactoe"; break;
+	case "guruttt": $uri = "tictactoe"; break;
+	case "tttxt": $uri = "tictactoe"; break;
+	case "kdots": $uri = "dots"; break;
+	case "fyrdman": $uri = "hastings"; break;
+	case "kreversi": $uri = "reversi"; break;
+	case "koenig": $uri = "chess"; break;
+	case "kcc": $uri = "ccheckers"; break;
+	case "escape-sdl": $uri = "escape"; break;
+}
+
+if ($origuri != $uri) :
+	include($_SERVER['DOCUMENT_ROOT'] . "/site/header.php");
+	echo "The game $origuri is a frontend for the game engine $uri.<br>\n";
+	echo "It does not yet have its own home page.<br>\n";
+	echo "Please visit the <a href='/games/$uri/'>common $uri page</a>.<br>\n";
+	include($_SERVER['DOCUMENT_ROOT'] . "/site/footer.php");
+else:
+	include($_SERVER['DOCUMENT_ROOT'] . "/site/error.php.en");
+endif;
+
+?>
