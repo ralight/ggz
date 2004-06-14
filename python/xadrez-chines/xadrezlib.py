cb5ca65452df4324f7f68e5e16562acc3b2e28c8
josef <devnull@localhost>
2004-06-14 13:49:39 -0700

    [svn] - completion of single player mode - ai improvement: detect blocked moves (such as knights or cannons) - implemented basic client-server interaction

--- python/xadrez-chines/xadrezlib.py
+++ python/xadrez-chines/xadrezlib.py
@@ -5,6 +5,53 @@
 
 from Numeric import *
 import random
+import socket
+
+class Network:
+	def __init__(self):
+		self.sock = None
+		self.gamefd = -1
+
+		#self.game = Game()
+
+	def init(self, fd):
+		self.sock = socket.fromfd(fd, socket.AF_INET, socket.SOCK_STREAM)
+
+	def getbyte(self):
+		opstr = self.sock.recv(4)
+		op = ord(opstr[0]) * 256 * 256 * 256
+		op += ord(opstr[1]) * 256 * 256
+		op += ord(opstr[2]) * 256
+		op += ord(opstr[3])
+		return op
+
+	def getchar(self):
+		opstr = self.sock.recv(1)
+		op = ord(opstr[0])
+		return op
+
+	def getstring(self):
+		length = self.getbyte()
+		opstr = self.sock.recv(length)
+		return opstr
+
+	def sendbyte(self, byte):
+		nbyte = socket.htonl(byte)
+		c1 = (nbyte >> 0) & 0xFF
+		c2 = (nbyte >> 8) & 0xFF
+		c3 = (nbyte >> 16) & 0xFF
+		c4 = (nbyte >> 24) & 0xFF
+		self.sock.send(chr(c1))
+		self.sock.send(chr(c2))
+		self.sock.send(chr(c3))
+		self.sock.send(chr(c4))
+
+	def sendchar(self, char):
+		self.sock.send(chr(char))
+
+	def sendstring(self, str):
+		self.sendbyte(len(str))
+		self.sock.send(str)
 
 class Proto:
 	def __init__(self):
@@ -16,9 +63,49 @@ class Xadrez:
 		self.proto = Proto()
 		self.field = {}
 		self.setup()
+		self.aicol = "white"
+		self.win = ""
+		self.aituple = ()
 
-	def ai(self, x):
-		pass
+	def setaicolor(self, color):
+		self.aicolor = color
+
+	def aicolor(self):
+		return self.aicol
+
+	def winner(self):
+		return self.win
+
+	def ai(self):
+		ret = self.proto.move_invalid
+		tries = 0
+		while ret == self.proto.move_invalid and tries < 1000:
+			x = random.randint(0, 8)
+			y = random.randint(0, 9)
+			oldx = random.randint(0, 8)
+			oldy = random.randint(0, 9)
+			(figure, color, options) = self.components(self.field[oldx][oldy])
+			if color == self.aicol:
+				ret = self.validate(oldx, oldy, x, y)
+			tries += 1
+		if ret == self.proto.move_ok:
+			self.aituple = (oldx, oldy, x, y)
+		return ret
+
+	def aimove(self):
+		return self.aituple
+
+	def components(self, fieldcell):
+		if fieldcell.count(":") == 2:
+			(figure, color, options) = fieldcell.split(":")
+		elif fieldcell.count(":") == 1:
+			(figure, color) = fieldcell.split(":")
+			options = ""
+		else:
+			figure = ""
+			color = ""
+			options = ""
+		return (figure, color, options)
 
 	def validate(self, oldx, oldy, x, y):
 		proto = self.proto
@@ -26,23 +113,9 @@ class Xadrez:
 
 		ret = proto.move_invalid
 
-		oldfigure = ""
-		oldcolor = ""
-		if field[oldx][oldy] is not "":
-			if field[oldx][oldy].count(":") == 2:
-				(oldfigure, oldcolor, oldoptions) = field[oldx][oldy].split(":")
-			elif field[oldx][oldy].count(":") == 1:
-				(oldfigure, oldcolor) = field[oldx][oldy].split(":")
-				oldoptions = ""
+		(oldfigure, oldcolor, oldoptions) = self.components(field[oldx][oldy])
 		print "go with", oldfigure, "of color", oldcolor, "and options", oldoptions
-		figure = ""
-		color = ""
-		if field[x][y] is not "":
-			if field[x][y].count(":") == 2:
-				(figure, color, options) = field[x][y].split(":")
-			elif field[x][y].count(":") == 1:
-				(figure, color) = field[x][y].split(":")
-				options = ""
+		(figure, color, options) = self.components(field[x][y])
 		print "to", figure, "of color", color
 
 		if oldfigure == "":
@@ -68,7 +141,6 @@ class Xadrez:
 
 		if oldfigure == "pawn":
 			if rivercross:
-				# XXX: promote
 				field[oldx][oldy] = oldfigure + ":" + oldcolor + ":" + oldoptions + ",promoted"
 			if oldcolor == "white":
 				if x == oldx and y == oldy + 1:
@@ -82,18 +154,54 @@ class Xadrez:
 				if y == oldy and x == oldx + 1:
 					ret = proto.move_ok
 
+		inbetween = 0
+		if oldfigure in ("cannon", "rock", "bishop"):
+			len = 0
+			if abs(y - oldy) < abs(x - oldx):
+				len = x - oldx
+			else:
+				len = y - oldy
+			stepsize = 1
+			#if oldfigure == "bishop":
+			#	stepsize = 2
+			for i in range(stepsize, abs(len), stepsize):
+				xn = oldx + i * (x - oldx) / abs(len)
+				yn = oldy + i * (y - oldy) / abs(len)
+				print "try", xn, yn
+				(tmpfigure, tmpcolor, tmpoptions) = self.components(field[xn][yn])
+				if tmpfigure is not "":
+					inbetween += 1
+
+		if oldfigure in ("knight"):
+			if abs(x - oldx) == 1 and y - oldy != 0:
+				xn = oldx
+				yn = oldy + (y - oldy) / abs(y - oldy)
+			elif x - oldx != 0:
+				xn = oldx + (x - oldx) / abs(x - oldx)
+				yn = oldy
+			else:
+				xn = oldx
+				yn = oldy
+			print "try", xn, yn
+			(tmpfigure, tmpcolor, tmpoptions) = self.components(field[xn][yn])
+			if tmpfigure is not "":
+				inbetween = 1
+				
+		if inbetween == 1:
+			print "figure is blocked!"
+
 		if oldfigure == "knight":
-			# XXX can be blocked!
-			if abs(oldy - y) == 2 and abs(oldx - x) == 1:
-				ret = proto.move_ok
-			if abs(oldy - y) == 1 and abs(oldx - x) == 2:
-				ret = proto.move_ok
+			if not inbetween:
+				if abs(oldy - y) == 2 and abs(oldx - x) == 1:
+					ret = proto.move_ok
+				if abs(oldy - y) == 1 and abs(oldx - x) == 2:
+					ret = proto.move_ok
 
 		if oldfigure == "bishop":
-			# XXX: can be blocked!
 			if not rivercross:
-				if abs(oldx - x) == 2 and abs(oldy - y) == 2:
-					ret = proto.move_ok
+				if not inbetween:
+					if abs(oldx - x) == 2 and abs(oldy - y) == 2:
+						ret = proto.move_ok
 
 		if oldfigure == "bodyguard":
 			if insidecastle:
@@ -101,14 +209,18 @@ class Xadrez:
 					ret = proto.move_ok
 
 		if oldfigure == "cannon":
-			# XXX: jumps!
 			if oldx == x or oldy == y:
-				ret = proto.move_ok
+				if figure == "":
+					if not inbetween:
+						ret = proto.move_ok
+				else:
+					if inbetween == 1:
+						ret = proto.move_ok
 
 		if oldfigure == "rock":
-			# XXX: can be blocked!
 			if oldx == x or oldy == y:
-				ret = proto.move_ok
+				if not inbetween:
+					ret = proto.move_ok
 
 		if oldfigure == "king":
 			if insidecastle:
@@ -116,6 +228,13 @@ class Xadrez:
 					ret = proto.move_ok
 				if abs(oldy - y) == 1 and x == oldx:
 					ret = proto.move_ok
+
+		if ret == proto.move_ok:
+			if figure == "king":
+				self.win = color
+			tmp = field[oldx][oldy]
+			field[oldx][oldy] = ""
+			field[x][y] = tmp
 
 		return ret
 
