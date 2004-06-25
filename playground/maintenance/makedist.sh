8c748de4560bcc6afe9f7e1d864a24c7edfbe66b
josef <devnull@localhost>
2004-06-25 06:31:05 -0700

    [svn] - some random bits

--- playground/maintenance/makedist.sh
+++ playground/maintenance/makedist.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+version=0.0.9
 base=$PWD
 
-echo "GGZ 0.0.8 Distribution"
+echo "GGZ $version Distribution"
 for i in *; do
 	if test -d $i && test -f $i/autogen.sh; then
 		echo $i...
 		cd $i
 		./autogen.sh >/dev/null
 		make DESTDIR=/tmp/dist-$i distcheck >/dev/null
-		cp *.tar.gz ..
+		cp *-$version.tar.gz ..
 		cd $base
 	fi
 done
