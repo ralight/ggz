57e8156d21363aea9e9d037bdcabfc293263eed2
josef <devnull@localhost>
2004-07-17 09:20:16 -0700

    [svn] - bump ggz protocol version number
    This is especially important since the room changes affect every user.

--- ggz/src/ggzcore/protocol.h
+++ ggz/src/ggzcore/protocol.h
@@ -4,7 +4,7 @@
  * Project: GGZ
  * Date: 10/18/99
  * Desc: Protocol enumerations, etc.
- * $Id: protocol.h 5589 2003-05-17 08:14:14Z dr_maux $
+ * $Id: protocol.h 6144 2004-07-17 16:20:16Z josef $
  *
  * Copyright (C) 1999 Brent Hendricks.
  *
@@ -36,6 +36,6 @@ typedef enum {
 /* I thought of putting this into ggz_common (in libggz) along with the
    protocol opcodes, but it really needs to stay tied to the network code
    itself (in ggzd and ggzcore). */
-#define GGZ_CS_PROTO_VERSION 9
+#define GGZ_CS_PROTO_VERSION 10
 
 #endif /*__PROTOCOL_H__*/
