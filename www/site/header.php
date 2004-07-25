9c914f9209f38aea17ed73e237682d4f7325877f
josef <devnull@localhost>
2004-07-25 14:05:27 -0700

    [svn] - some more i18n magic: extract base language from browser user agent   (only works for Mozilla right now, otherwise it keeps the value 'en') - some legacy fixes: always force utf8 also via html-meta-header   (this will become obsolete with xhtml)

--- www/site/header.php
+++ www/site/header.php
@@ -1,9 +1,18 @@
 <?php
 
 $lang = "en";
+
+$matches = array();
+// Mozilla/5.0 (X11; U; Linux i686; pt-BR; rv:1.6) Gecko/20040413 Debian/1.6-5 
+if(preg_match("/^Mozilla\/5.0 \(\S+; U; \S+ \S+; (\S+); \S+\) .*/", $_SERVER['HTTP_USER_AGENT'], $matches)) :
+	$lang = $matches[1];
+endif;
+
 $langs = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 $langs = str_replace(" ", "", $langs);
 $langs = preg_replace("/;[^,]*/", "", $langs);
+
+$langs = "$lang,$langs";
 $ar = split(",", $langs);
 
 foreach ($ar as $l)
