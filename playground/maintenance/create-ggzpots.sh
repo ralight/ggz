0746f024afad5f31446f2efb4e31fe7972729535
josef <devnull@localhost>
2004-08-23 16:50:53 -0700

    [svn] - creation of catalog of translatable strings

---
+++ playground/maintenance/create-ggzpots.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -rf ggzpots .ggzpots.files ggzpots.tar.gz
+
+find . -name *.pot -exec echo {} > .ggzpots.files \;
+
+mkdir ggzpots
+for i in `cat .ggzpots.files`; do
+	#cp --parents $i ggzpots;
+	cp $i ggzpots;
+done
+
+tar cvzf ggzpots.tar.gz ggzpots
+
