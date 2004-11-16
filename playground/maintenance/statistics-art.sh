6ea53aa2d8be10ecf7799bf19b2ba3925ca214d6
josef <devnull@localhost>
2004-11-16 04:09:44 -0800

    [svn] - fixed version of the script

--- playground/maintenance/statistics-art.sh
+++ playground/maintenance/statistics-art.sh
@@ -2,23 +2,28 @@
 
 formats="xpm png svg"
 
-for i in *-*; do
-	echo -n "$i";
-	for j in $formats; do
-		echo -n " $j="
-		num=`find $i -name "*.$j" | wc -l;`
-		echo -n $num
+for i in *; do
+	if test -d $i; then
+		echo -n "$i";
+		for j in $formats; do
+			echo -n " $j="
+			num=`find $i -name "*.$j" | wc -l;`
+			echo -n $num
 
-		find $i -name "*.$j" >> .artlist.$j
-	done
-	echo ""
+			find $i -name "*.$j" >> .artlist.$j
+		done
+		echo ""
+	fi
 done
 
 for j in $formats; do
 	mkdir ggzgraphics-$j;
 	for i in `cat .artlist.$j`; do
-		cp --parent $i ggzgraphics-$j;
+		if test `stat -c %s $i` -lt 50000; then
+			cp --parent $i ggzgraphics-$j;
+		fi
 	done
+	rm -f ggzgraphics-$j.tar.gz;
 	tar czf ggzgraphics-$j.tar.gz ggzgraphics-$j;
 
 	rm -rf ggzgraphics-$j;
