449be97bc57ecd234c3c9731ac7d227034479180
josef <devnull@localhost>
2004-11-22 06:26:46 -0800

    [svn] - Reversi is now implemented for single-player mode

--- python/ggzboard/module_reversi.py
+++ python/ggzboard/module_reversi.py
@@ -22,6 +22,7 @@ class Game:
 		self.board[8][8] = ("piece", "w")
 
 		self.isover = 0
+		self.lastmove = ""
 
 	def name(self):
 		return "Reversi"
@@ -47,6 +48,15 @@ class Game:
 		elif self.board[y][x] is not None:
 			valid = 0
 			reason = "cannot move to occupied square"
+		else:
+			valid = 0
+			for i in range(x - 1, x + 2):
+				for j in range(y - 1, y + 2):
+					if i >= 0 and i < self.width and j >= 0 and j < self.height:
+						if self.board[j][i]:
+							valid = 1
+			if valid == 0:
+				reason = "has to border an existing piece"
 
 		if valid == 0:
 			print reason
@@ -62,11 +72,11 @@ class Game:
 		if valid:
 			print "TRYMOVE", topos
 			ret = 1
+			self.lastmove = "w"
 		return ret
 
 	def aimove(self):
 		ret = 0
-		frompos = -1
 		topos = -1
 		value = 0
 		for j in range(self.height):
@@ -74,19 +84,65 @@ class Game:
 				f = self.board[j][i]
 				if not f:
 					if value < 1:
-						frompos = None
 						topos = (i, j)
-						print "AIMOVE(1)", frompos, topos
-						ret = 1
-						value = 1
+						if self.validatemove("b", topos):
+							print "AIMOVE(1)", topos
+							ret = 1
+							value = 1
 		if ret == 0:
 			self.isover = 1
-		return (ret, frompos, topos)
+		else:
+			self.lastmove = "b"
+		return (ret, None, topos)
 
 	def domove(self, frompos, topos):
-		if self.validatemove("", topos):
+		if self.validatemove(self.lastmove, topos):
 			(x, y) = topos
-			self.board[y][x] = ("piece", "w")
+			self.board[y][x] = ("piece", self.lastmove)
+
+			maxdist = max(self.width, self.height)
+			xmin = -maxdist
+			xmax = maxdist
+			ymin = -maxdist
+			ymax = maxdist
+			list = []
+			print "LIMIT", xmin, xmax, ymin, ymax
+			for pos in ((xmin, 0), (xmax, 0), (0, ymin), (0, ymax), (xmin, ymin), (xmin, ymax), (xmax, ymin), (xmax, ymax)):
+				(px, py) = pos
+				i = x
+				j = y
+				finished = 0
+				tmplist = []
+				print "limit-stuff", x, y, "until", x + px, y + py
+				while i != x + px or j != y + py:
+					if finished:
+						break
+					if px:
+						i = i + (px / abs(px))
+					if py:
+						j = j + (py / abs(py))
+					print "TRY", i, j
+					if i >= 0 and i < self.width and j >= 0 and j < self.height:
+						if not self.board[j][i]:
+							finished = 1
+						else:
+							(gfx, color) = self.board[j][i]
+							if color == self.lastmove:
+								for pos in tmplist:
+									list.append(pos)
+								finished = 1
+							else:
+								tmplist.append((i, j))
+								print "LIST", list
+
+			for pos in list:
+				(i, j) = pos
+				(gfx, color) = self.board[j][i]
+				if color == "w":
+					color = "b"
+				else:
+					color = "w"
+				self.board[j][i] = (gfx, color)
 
 	def over(self):
 		return self.isover
