88dd047f43dcfcc041026fb8b2e1a6b07d472160
josef <devnull@localhost>
2005-01-01 14:36:30 -0800

    [svn] - use the correct PNG files

--- ggz/src/frontend/gtk/playerlist.c
+++ ggz/src/frontend/gtk/playerlist.c
@@ -3,7 +3,7 @@
  * Author: GGZ Dev Team
  * Project: GGZ GTK Client
  * Date: 11/03/2002
- * $Id: playerlist.c 6391 2004-11-16 07:15:24Z jdorje $
+ * $Id: playerlist.c 6573 2005-01-01 22:36:30Z josef $
  * 
  * List of players in the current room
  * 
@@ -234,8 +234,8 @@ void update_player_list(void)
 			lag[i] = load_svg_pixbuf(name, ICON_SIZE, ICON_SIZE);
 		}
 		guest = load_pixbuf("ggz_gtk_guest");
-		registered = load_pixbuf("ggz_gtk_guest");
-		admin = load_pixbuf("ggz_gtk_guest");
+		registered = load_pixbuf("ggz_gtk_registered");
+		admin = load_pixbuf("ggz_gtk_admin");
 		pixmaps_initted = TRUE;
 	}
 
