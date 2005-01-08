3518912f3e2f5e0478981fb925b714f1d64e38e8
senko <devnull@localhost>
2005-01-08 15:35:24 -0800

    [svn] add file: web/common/acknowledgements.inc.sv

---
+++ community/web/common/acknowledgements.inc.sv
@@ -0,0 +1,14 @@
+<div id="acknowledgements">
+	<div class="note" style="float:left;">
+		&copy; 2005 <a href="http://www.ggzgamingzone.org/">GGZ Gaming Zone</a>
+	</div>
+	<div class="note" style="float:right;">
+		Design baserad på Steel-n-Leather &copy; 2004 av
+		<a href="mailto:brian.allbee@gmail.com" title="Comment on this design if you wish!">Brian D. Allbee</a>
+	</div>
+	<div class="note" style="float:left;">
+	<?php
+		Locale::availablelanguages();
+	?>
+	</div>
+</div>
