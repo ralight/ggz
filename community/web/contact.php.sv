7976331e6f93c47bb23673994a2e5d3f816a9cbe
josef <devnull@localhost>
2005-01-08 13:41:46 -0800

    [svn] - translations done by kettil

---
+++ community/web/contact.php.sv
@@ -0,0 +1,38 @@
+<?php include("top.inc"); ?>
+
+<div id="main">
+	<h1>
+		<span class="itemleader">:: </span>
+		Om GGZ-Kommunityportalen
+		<span class="itemleader"> :: </span>
+		<a name="about"></a>
+	</h1>
+	<div class="text">
+		<div>
+		Firandes sin femte födelsedag, har
+		<a href="http://www.ggzgamingzone.org/">GGZ Gaming Zone</a>
+		-projektet invigt GGZ Kommunity, en portal tillägnad
+		spelare (och blivande spelare) av GGZ-spel.
+		</div>
+	</div>
+
+	<h1>
+		<span class="itemleader">:: </span>
+		Kontakta oss
+		<span class="itemleader"> ::</span>
+		<a name="contact"></a>
+	</h1>
+	<div class="text">
+		Du kan skicka oss epost när helst du önskar, förutom spam
+		förståss :)
+		<br>
+		Addresen är:
+		<a href="mailto:info@ggzcommunity.org">info@ggzcommunity.org</a>
+		<br><br>
+		För frågor angående utvecklandet av GGZ, ta en titt på projektets
+		<a href="https://mail.ggzgamingzone.org/">mejllistor</a>.
+	</div>
+
+</div>
+
+<?php include("bottom.inc"); ?>
---
+++ community/web/index.php.sv
@@ -0,0 +1,35 @@
+<?php include("top.inc"); ?>
+
+<div id="main">
+	<h1>
+		<span class="itemleader">:: </span>
+		Jubileum: 5 år av GGZ Gaming Zone!
+		<span class="itemleader"> :: </span>
+		<a name="5years"></a>
+	</h1>
+	<div class="text">
+		<div style="float:right">
+		<img src="images/grubbyparty.png" alt="Grubby bjuder in dej!">
+		</div>
+		Det är otroligt - projektet blev 5 år gammalt idag!
+		Ta en titt på den speciellt tillägnade
+		<a href="/5years/" title="GGZs femte födelsedag">födelsedagssidan</a>.
+	</div>
+
+	<h1>
+		<span class="itemleader">:: </span>
+		Ny GGZ-Kommunityportal
+		<span class="itemleader"> ::</span>
+		<a name="newportal"></a>
+	</h1>
+	<div class="text">
+		Ersätter de gamla <a href="http://www.ggzgamingzone.org/backend/web/" title="">Ranking</a>
+		-sidorna, och siktar på mycket mer:
+		Statistik om allt spelrelaterat på GGZ, användarinsända nivåer och teman, forum och
+		bloggar, spelrecensioner, organiserade turneringar, och små underhållande webspel.
+		GGZ-servrar kan använda "ggz-community" paketet för att göra egna portaler.
+	</div>
+
+</div>
+
+<?php include("bottom.inc"); ?>
