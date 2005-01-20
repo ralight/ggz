10ad936f8733294a032c78b707cd0541af41caa6
jdorje <devnull@localhost>
2005-01-20 10:20:44 -0800

    [svn] Improve docs for room and game events.

--- ggz/src/ggzcore/ggzcore.h
+++ ggz/src/ggzcore/ggzcore.h
@@ -1,13 +1,13 @@
 /*
  * File: ggzcore.h
- * Author: Brent Hendricks
+ * Author: GGZ Development Team
  * Project: GGZ Core Client Lib
  * Date: 9/15/00
- * $Id: ggzcore.h 6768 2005-01-20 17:48:57Z josef $
+ * $Id: ggzcore.h 6770 2005-01-20 18:20:44Z jdorje $
  *
  * Interface file to be included by client frontends
  *
- * Copyright (C) 2000-2002 Brent Hendricks.
+ * Copyright (C) 2000-2005 GGZ Development Team.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -383,8 +383,10 @@ typedef struct {
  *  by a communication from the server.  When a room event occurs, the
  *  associated event handler will be called, and will be passed the event
  *  data (a void*), along with the (optional) user data.  All room events
- *  apply to the current room unless a room number is given.
+ *  apply to the current room unless a room number is given.  Room events
+ *  are almost always triggered by calling ggzcore_server_read_data.
  *  @see ggzcore_room_add_event_hook
+ *  @see ggzcore_server_read_data
  */
 typedef enum {
 	/** The list of players in a room has arrived.
@@ -468,20 +470,34 @@ typedef enum {
 } GGZRoomEvent;
 
 
+/** A GGZGameEvent is an event associated with the game, that is triggered
+ *  by a communication from the server or from the game.  When a game
+ *  event occurs, the associated event handle will be called, and will be
+ *  passed the event data (a void*) along with the (optional) user data.
+ *  All game events apply to the current game.  Game events are usually
+ *  triggered by calling ggzcore_server_read_data or ggzcore_game_read_data.
+ *  @see ggzcore_game_add_event_hook
+ *  @see ggzcore_server_read_data
+ */
 typedef enum {
-	/** A game was launched by the player (you).
+	/** A game was launched by the player (you).  After this the core
+	 *  client should call ggzcore_game_get_control_fd, monitor the
+	 *  socket that function returns, and call ggzcore_game_read_data
+	 *  when there is data pending.  This event is triggered inside of
+	 *  ggzcore_game_launch.
 	 *  @param data NULL
 	 *  @see ggzcore_game_launch */
 	GGZ_GAME_LAUNCHED,
 
-	/** Your game launch has failed.
+	/** Your game launch has failed.  Triggered instead of
+	 *  GGZ_GAME_LAUNCHED when there's a failure somewhere.
 	 *  @param data NULL
 	 *  @see GGZ_GAME_LAUNCHED */
 	GGZ_GAME_LAUNCH_FAIL,
 
 	/** Negotiation with server was successful.  This should happen
-	 *  some time after the launch succeeds.
-	 *  @todo This isn't handled correctly by ggzcore.
+	 *  some time after the launch succeeds.  The core client should
+	 *  create a channel at this point.
 	 *  @param data NULL */
 	GGZ_GAME_NEGOTIATED,
 
@@ -489,7 +505,9 @@ typedef enum {
 	 *  @todo Currently this can't actually happen... */
 	GGZ_GAME_NEGOTIATE_FAIL,
 
-	/** Game reached the 'playing' state.
+	/** Game reached the 'playing' state.  When this happens the core
+	 *  client should call ggzcore_room_launch_table or
+	 *  ggzcore_room_join_table to finalize the game join.
 	 *  @param data NULL */
 	GGZ_GAME_PLAYING
 } GGZGameEvent;
