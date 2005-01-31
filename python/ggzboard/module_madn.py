71cebd755c0bbafa69a2c1edf659ee4468819997
josef <devnull@localhost>
2005-01-31 06:18:58 -0800

    [svn] - rule checking for Ludo - basic dice support for all games

--- python/ggzboard/module_madn.py
+++ python/ggzboard/module_madn.py
@@ -90,8 +90,6 @@ class Game:
 		self.boardstyle[4][1] = (110, 110, 110)
 		self.boardstyle[4][2] = (110, 110, 110)
 		self.boardstyle[4][3] = (110, 110, 110)
-		self.boardstyle[4][4] = (110, 110, 110)
-		self.boardstyle[4][6] = (110, 110, 110)
 		self.boardstyle[4][7] = (110, 110, 110)
 		self.boardstyle[4][8] = (110, 110, 110)
 		self.boardstyle[4][9] = (110, 110, 110)
@@ -101,8 +99,6 @@ class Game:
 		self.boardstyle[6][1] = (110, 110, 110)
 		self.boardstyle[6][2] = (110, 110, 110)
 		self.boardstyle[6][3] = (110, 110, 110)
-		self.boardstyle[6][4] = (110, 110, 110)
-		self.boardstyle[6][6] = (110, 110, 110)
 		self.boardstyle[6][7] = (110, 110, 110)
 		self.boardstyle[6][8] = (110, 110, 110)
 		self.boardstyle[6][9] = (110, 110, 110)
@@ -135,6 +131,14 @@ class Game:
 		self.isover = 0
 		self.lastmove = ""
 
+		self.datapath = "./"
+
+		self.dice = tuple(array(2))
+		self.lastdice = None
+
+	def init(self, path):
+		self.datapath = path
+
 	def name(self):
 		return unicode("M-Ä-D-N", "utf-8")
 
@@ -151,24 +155,179 @@ class Game:
 		else:
 			colorstr = "black"
 		piecestr = "poeppel"
-		return "madn/" + piecestr + "-" + colorstr + ".svg"
+		return self.datapath + "madn/" + piecestr + "-" + colorstr + ".svg"
 
 	def validatemove(self, fromcolor, frompos, topos):
 		return 0
 
+	# returns 'None' if not found or illegal given
+	# returns 'topos' if given and legal, or calculated 'topos' if given as None
+	def find_and_validate(self, frompos, topos, steps):
+		#if topos:
+		#	(x, y) = topos
+		(oldx, oldy) = frompos
+		print "validate moves from", frompos, "to", topos, "counting", steps
+		(cx, cy) = (oldx, oldy)
+		performedsteps = 0
+		transitions = (
+			((0, 4), (3, 4), (1, 0), None),
+			((6, 4), (9, 4), (1, 0), None),
+			((10, 4), None, (0, 1), None),
+
+			((1, 6), (4, 6), (-1, 0), None),
+			((7, 6), (10, 6), (-1, 0), None),
+			((0, 6), None, (0, -1), None),
+
+			((4, 1), (4, 4), (0, -1), None),
+			((4, 7), (4, 10), (0, -1), None),
+			((4, 0), None, (1, 0), None),
+
+			((6, 0), (6, 3), (0, 1), None),
+			((6, 6), (6, 9), (0, 1), None),
+			((6, 10), None, (-1, 0), None),
+
+			((0, 5), (3, 5), (1, 0), "y"),
+			((10, 5), (9, 5), (-1, 0), "r"),
+			((5, 0), (5, 3), (0, 1), "b"),
+			((5, 10), (5, 9), (0, -1), "g"),
+
+			((0, 5), None, (0, -1), "brg"),
+			((10, 5), None, (0, 1), "bgy"),
+			((5, 0), None, (1, 0), "rgy"),
+			((5, 10), None, (-1, 0), "bry")
+		)
+		for step in range(steps):
+			transited = 0
+			for trans in transitions:
+				if not transited:
+					(first, last, rel, desc) = trans
+					if not last:
+						last = first
+					(fx, fy) = first
+					(lx, ly) = last
+					(rx, ry) = rel
+					if (cx >= fx and cx <= lx) or (cx >= lx and cx <= fx):
+						if (cy >= fy and cy <= ly) or (cy >= ly and cy <= fy):
+							valid = 1
+							if desc:
+								if color not in desc:
+									valid = 0
+							if valid:
+								cx += rx
+								cy += ry
+								transited = 1
+								performedsteps += 1
+								print "- transit to", (cx, cy)
+		print "arrived at", (cx, cy), "should be", topos
+		if not topos:
+			if performedsteps != steps:
+				print "steps", steps, "performed", performedsteps
+				return None
+		return (cx, cy)
+
 	def trymove(self, frompos, topos):
+		print "tryomove -- ", frompos, topos, "using last dice", self.lastdice
+		(x, y) = topos
+		(oldx, oldy) = frompos
+		if not self.board[oldy][oldx]:
+			return 0
+		if frompos == topos:
+			self.lastdice = None
+			return 0
+		(gfx, color) = self.board[oldy][oldx]
+		
+		if self.lastdice == 1:
+			if frompos == (1, 1) or frompos == (0, 1) or frompos == (1, 0) or frompos == (0, 0):
+				if topos == (0, 4):
+					#self.domove(frompos, topos)
+					return 1
+
+		(cx, cy) = self.find_and_validate(frompos, topos, self.lastdice)
+
+		if cx == x and cy == y:
+			print "goooooooal!"
+			return 1
+
+#			if cy == 4:
+#				if cx >= 0 and cx < 4:
+#					cx += 1
+#				elif cx == 4:
+#					cy -= 1
+#				elif cx >= 6 and cx < 10:
+#					cx += 1
+#				elif cx == 10:
+#					cy += 1
+#			elif cy == 6:
+#				if cx <= 10 and cx > 6:
+#					cx -= 1
+#				elif cx == 6:
+#					cy += 1
+#				elif cx <= 4 and cx > 0:
+#					cx -= 1
+#				elif cx == 0:
+#					cy -= 1
+		#if oldy == 4 and y == 4:
+		#	if oldx >= 0 and x <= 4 and x > oldx:
+		#		return 1
+		#	if oldx >= 6 and x <= 10 and x > oldx:
+		#		return 1
+		self.lastdice = None
 		return 0
 
 	def aimove(self):
-		pass
+		self.lastdice = None
+		self.rolldice()
+		return (0, None, None)
 
 	def domove(self, frompos, topos):
-		pass
+		(oldx, oldy) = frompos
+		(x, y) = topos
+		print "** DOMOVE", frompos, topos
+		print self.board[oldy][oldx]
+		self.board[y][x] = self.board[oldy][oldx]
+		self.board[oldy][oldx] = None
 
 	def over(self):
 		return self.isover
 
 	def toggleplayer(self):
 		pass
+
+	def rolldice(self):
+		if self.lastdice is not None:
+			return 0
+		print "### ROLL DICE ###"
+		#for dice in self.dice:
+		#	if dice == 0:
+		#		dice = random.randint(0, 6)
+		self.dice = []
+
+		for trial in range(3):
+			dice = random.randint(1, 6)
+			#self.dice = tuple(array(dice))
+			self.dice.append(dice)
+
+			usable = 0
+			for j in range(self.height):
+				for i in range(self.width):
+					field = self.board[j][i]
+					if field:
+						(gfx, color) = field
+						if color == "y":
+							print "CHECK move ability", i, j
+							ret = self.find_and_validate((i, j), None, dice)
+							print " =>", ret
+							if ret is not None:
+								usable = 1
+			if usable:
+				self.lastdice = dice
+				return 1
+			if dice == 1:
+				self.lastdice = dice
+				return 1
+			#if dice != 6:
+			#	return 0
+
+		return 0
 
 ggzboardgame = Game()
