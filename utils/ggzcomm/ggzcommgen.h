1f7d158fd59dbf4b3530ee81720dbe2599eb7f9b
josef <devnull@localhost>
2005-03-13 09:43:35 -0800

    [svn] - install ggzcommgen.rb - delete old C implementation

--- utils/ggzcomm/ggzcommgen.h
+++
@@ -1,7 +0,0 @@
-#ifndef GGZCOMMGEN_H
-#define GGZCOMMGEN_H
-
-void ggzcommgen_generate(const char *protocol, const char *task, const char *format, const char *library, const char *mode);
-
-#endif
-
