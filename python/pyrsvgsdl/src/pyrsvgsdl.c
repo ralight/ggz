193e504dca99d410aa916148bd97b7088525079d
josef <devnull@localhost>
2005-03-17 17:16:51 -0800

    [svn] - transformation of raw pixmap data into a nice object - this provides the former data as data(), in addition to width() and height() - this will break all current games, but not for long...

--- python/pyrsvgsdl/src/pyrsvgsdl.c
+++ python/pyrsvgsdl/src/pyrsvgsdl.c
@@ -1,7 +1,7 @@
 /**********************************************/
 /*                                            */
 /* PyRSVGSDL - Python/SDL bindings for rsvg   */
-/* Copyright (C) 2004 Josef Spillner          */
+/* Copyright (C) 2004, 2005 Josef Spillner    */
 /* josef@ggzgamingzone.org                    */
 /* Published under GNU GPL conditions         */
 /*                                            */
@@ -30,44 +30,115 @@
 
 static PyObject *rsvg = NULL;
 
-/*****************************************************/
-/* GGZChess object methods just like in C            */
-/*****************************************************/
+/**********************************************/
+/* Object definitions                         */
+/**********************************************/
+
+staticforward PyTypeObject pyrsvgsdl_SurfaceType;
+staticforward PyMethodDef pyrsvgsdl_surface_methods[];
+
+typedef struct
+{
+	PyObject_HEAD
+	GdkPixbuf *buf;
+}
+pyrsvgsdl_SurfaceObject;
+
+static void pyrsvgsdl_delete_surface(pyrsvgsdl_SurfaceObject *self)
+{
+	if(self->buf)
+	{
+		g_object_unref(self->buf);
+	}
+
+	PyObject_Del(self);
+}
+
+static PyTypeObject pyrsvgsdl_SurfaceType =
+{
+	PyObject_HEAD_INIT(NULL)
+	0,
+	"RSVG SDL surface",
+	sizeof(pyrsvgsdl_SurfaceObject),
+	0,
+	(destructor)pyrsvgsdl_delete_surface,
+};
+
+static PyObject *pyrsvgsdl_surface_getattr(PyObject *self, char *name)
+{
+	PyObject *ret = NULL;
+
+	ret = Py_FindMethod(pyrsvgsdl_surface_methods, self, name);
+
+	return ret;
+}
+
+static PyObject *pyrsvgsdl_new_surface(PyObject *self, PyObject *args)
+{
+	pyrsvgsdl_SurfaceObject *surface;
+
+	pyrsvgsdl_SurfaceType.ob_type = &PyType_Type;
+
+	pyrsvgsdl_SurfaceType.tp_methods = (struct PyMethodDef*)pyrsvgsdl_surface_methods;
+	pyrsvgsdl_SurfaceType.tp_getattr = (getattrfunc)pyrsvgsdl_surface_getattr;
+
+	surface = PyObject_New(pyrsvgsdl_SurfaceObject, &pyrsvgsdl_SurfaceType);
+	surface->buf = NULL;
+
+	return (PyObject*)surface;
+}
+
+/*************************************************/
+/* RSVG object methods just like in C            */
+/*************************************************/
 
 static PyObject *pyrsvgsdl_render(PyObject *self, PyObject *args)
 {
 	char *filename;
 	int width, height;
-	int ret;
+	pyrsvgsdl_SurfaceObject *surface;
 
 	GError *error = NULL;
-//	const int width = 128;
-//	const int height = 128;
 
 	if(!PyArg_ParseTuple(args, "sii", &filename, &width, &height)) return NULL;
 
 	g_type_init();
 	GdkPixbuf *buf = rsvg_pixbuf_from_file_at_size(filename, width, height, &error);
 
-/*	if(buf)
-	{
-		guchar *foo = gdk_pixbuf_get_pixels(buf);
-		for(int i = 0; i < width; i++)
-			for(int j = 0; j < height; j++)
-			{
-				unsigned int pixel = *(unsigned int*)(foo + (j * width + i) * 4);
-				int a, r, g, b;
-				r = (pixel >> 24) & 0xFF;
-				g = (pixel >> 16) & 0xFF;
-				b = (pixel >> 8) & 0xFF;
-				a = (pixel >> 0) & 0xFF;
-				pixel = (a << 24) + (r << 16) + (g << 8) + (b << 0);
-				*(unsigned int*)(foo + (j * width + i) * 4) = pixel;
-			}
-	}
-	else tmp = QPixmap(filename + ".png");
-*/
-	ret = 0;
+	surface = (pyrsvgsdl_SurfaceObject*)pyrsvgsdl_new_surface(NULL, NULL);
+	surface->buf = buf;
+	return (PyObject*)surface;
+}
+
+static PyObject *pyrsvgsdl_surface_width(pyrsvgsdl_SurfaceObject *self, PyObject *args)
+{
+	int ret;
+
+	if(!PyArg_ParseTuple(args, "")) return NULL;
+
+	ret = gdk_pixbuf_get_width(self->buf);
+
+	return Py_BuildValue("i", ret);
+}
+
+static PyObject *pyrsvgsdl_surface_height(pyrsvgsdl_SurfaceObject *self, PyObject *args)
+{
+	int ret;
+
+	if(!PyArg_ParseTuple(args, "")) return NULL;
+
+	ret = gdk_pixbuf_get_height(self->buf);
+
+	return Py_BuildValue("i", ret);
+}
+
+static PyObject *pyrsvgsdl_surface_data(pyrsvgsdl_SurfaceObject *self, PyObject *args)
+{
+	GdkPixbuf *buf;
+
+	if(!PyArg_ParseTuple(args, "")) return NULL;
+
+	buf = self->buf;
 
 	return Py_BuildValue("s#",
 		gdk_pixbuf_get_pixels(buf),
@@ -81,6 +152,14 @@ static PyObject *pyrsvgsdl_render(PyObject *self, PyObject *args)
 static PyMethodDef pyrsvgsdl_methods[] =
 {
 	{"render", pyrsvgsdl_render, METH_VARARGS},
+	{NULL, NULL, 0}
+};
+
+static PyMethodDef pyrsvgsdl_surface_methods[] =
+{
+	{"data", pyrsvgsdl_surface_data, METH_VARARGS},
+	{"width", pyrsvgsdl_surface_width, METH_VARARGS},
+	{"height", pyrsvgsdl_surface_height, METH_VARARGS},
 	{NULL, NULL, 0}
 };
 
