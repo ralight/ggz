953850036f4fe1b0adf45a63fd78488988831ba4
josef <devnull@localhost>
2005-03-24 04:05:24 -0800

    [svn] - documentation update for the project server and repository actions

---
+++ playground/maintenance/repository/dynamic.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Maintenance script
+# Create GGZ message catalog archive and statistics file
+
+cd $HOME/ggz-cvs/current
+./update-all.sh
+./create-ggzpots.sh
+./statistics-ggzpos.sh > postats.txt
+
