4a55eea071b51ac85d9eb13980099ab49953ab79
josef <devnull@localhost>
2005-04-16 14:14:05 -0700

    [svn] - large version of the map

---
+++ community/web/map/index.php
@@ -0,0 +1,31 @@
+<?php $global_leftbar = "disabled"; ?>
+<?php $global_rightbar = "disabled"; ?>
+<?php include("top.inc"); ?>
+
+<div id="main">
+	<h1>
+		<span class="itemleader">:: </span>
+		World map
+		<span class="itemleader"> :: </span>
+		<a name="articles"></a>
+	</h1>
+	<div class="text">
+	This is a map showing all players who registered their coordinates.
+	You can do so on your <a href="/my/change.php">personal page</a>
+	if you are logged in.
+	</div>
+	<h2>
+		<span class="itemleader">:: </span>
+		The current map
+		<span class="itemleader"> :: </span>
+		<a name="database"></a>
+	</h2>
+	<div class="text">
+<?php
+include_once("map/mundomap.map");
+?>
+	<img src="mundomap.large.png" usemap="#ggzmap">
+	</div>
+</div>
+
+<?php include("bottom.inc"); ?>
