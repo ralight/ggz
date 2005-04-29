510aecf2d5c88847627185c3c9e99ba4888bbfbe
josef <devnull@localhost>
2005-04-29 11:37:11 -0700

    [svn] - use gmake on BSD - abort properly if there's an error in one of the steps - all steps can now be cached, leading to really quick recompiles
    On NetBSD, we have to look into /usr/pkg for all software. This is not yet
    fixed.

--- playground/maintenance/getggz.sh
+++ playground/maintenance/getggz.sh
@@ -7,15 +7,24 @@ version=0.0.10
 tmp=$HOME/.ggz/getggz-$version
 log=$tmp/getggz.log
 prefix=$HOME/ggz
+makeprog=make
+cache=n
 
+echo "-----------------------"
 echo "GGZ $version Installation"
 echo "-----------------------"
 
+if test -f /usr/bin/gmake || test -f /usr/pkg/bin/gmake; then
+	echo "*** Note: Detected BSD layout"
+	makeprog=gmake
+fi
+echo "*** Note: Log file will be written to $log"
+
 echo "Please select a download mirror:"
-echo "Germany, viaKGT new media (1)"
-echo "USA, via ibiblio          (2)"
-echo "Denmark, via dotsrc       (3)"
-echo "Belgium, via Belnet       (4)"
+echo "Germany, via KGT new media (1)"
+echo "USA, via ibiblio           (2)"
+echo "Denmark, via dotsrc        (3)"
+echo "Belgium, via Belnet        (4)"
 read -p "? " mirror
 
 case $mirror in
@@ -145,42 +154,78 @@ if test "x$method" = "xc"; then
 	fi
 fi
 
+if test -d $tmp; then
+	echo "It appears getggz.sh was ran already."
+	echo "Use cache to save time (y/n)?"
+	read -p "? " cache
+fi
+
 echo "Downloading the following packages:"
 echo $pkg
 echo "From $mirror into $tmp"
 
 mkdir -p $tmp
 cd $tmp
-rm $log
+rm -f $log
 
 echo "Download of all packages"
 for p in $pkg; do
-	echo "+ $p (download)"
 	tarball=$mirror/ggz/$version/$p-$version.tar.gz
-	wget $tarball >>$log 2>&1
+	if test $cache = y && test -f $p-$version.tar.gz; then
+		echo "- $p (download cached)"
+		wget -c $tarball >>$log 2>&1
+	else
+		echo "+ $p (download)"
+		wget $tarball >>$log 2>&1
+	fi
 	if test $? -ne 0; then
-		echo "Downloading $tarball failed!"
+		echo "*** Downloading $tarball failed!"
 		exit
 	fi
 done
 
 echo "Unpacking of all packages"
 for p in $pkg; do
-	echo "+ $p (decompression)"
-	tar xzvf $p-$version.tar.gz >>$log 2>&1
+	if test $cache = y && test -d $p-$version; then
+		echo "- $p (decompression cached)"
+	else
+		echo "+ $p (decompression)"
+		tar xzvf $p-$version.tar.gz >>$log 2>&1
+		if test $? -ne 0; then
+			echo "*** Decompressing $p-$version.tar.gz failed!"
+			exit
+		fi
+	fi
 done
 
 echo "Configuration and compilation of all packages"
 echo "Packages will be installed into $prefix"
 for p in $pkg; do
-	echo "+ $p (configuration)"
-	cd $p-$version
-	./configure --prefix=$prefix >>$log 2>&1
-	echo "+ $p (compilation)"
-	make >>$log 2>&1
-	echo "+ $p (installation)"
-	make install >>$log 2>&1
-	cd $tmp
+	if test $cache = y && test -f $p-$version/.installed; then
+		echo "- $p (configuration/compilation/installation cached)"
+	else
+		echo "+ $p (configuration)"
+		cd $p-$version
+		./configure --prefix=$prefix >>$log 2>&1
+		if test $? -ne 0; then
+			echo "*** Configuration of $p-$version failed!"
+			exit
+		fi
+		echo "+ $p (compilation)"
+		$makeprog >>$log 2>&1
+		if test $? -ne 0; then
+			echo "*** Compilation of $p failed!"
+			exit
+		fi
+		echo "+ $p (installation)"
+		$makeprog install >>$log 2>&1
+		if test $? -ne 0; then
+			echo "*** Installation of $p failed!"
+			exit
+		fi
+		touch .installed
+		cd $tmp
+	fi
 done
 
 echo "Finished! GGZ $version is now installed."
