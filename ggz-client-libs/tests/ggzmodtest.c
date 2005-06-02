792c1769b8b71551e33a6832178fa95f144c4b27
josef <devnull@localhost>
2005-06-02 08:02:10 -0700

    [svn] - migrating the check for ggzmodtest to ggzmod-ggz

--- ggz-client-libs/tests/ggzmodtest.c
+++ ggz-client-libs/tests/ggzmodtest.c
@@ -1,32 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "ggzmod.h"
 #include "ggzmod-ggz.h"
 
 int main(int argc, char **argv)
 {
-	GGZMod *ggz = ggzmod_new(GGZMOD_GGZ);
+	GGZMod *ggz = ggzmod_ggz_new(GGZMOD_GGZ);
 	char *args[] = {"./ggzmodgametest", NULL};
 
 
-	printf("Checking type.\n");
-	if (ggzmod_get_type(ggz) != GGZMOD_GGZ) {
+	/*printf("Checking type.\n");
+	if (ggzmod_ggz_get_type(ggz) != GGZMOD_GGZ) {
 		printf("Wrong type!\n");
 		return -1;
-	}
+	}*/
 
 	printf("Setting module.\n");
-	ggzmod_set_module(ggz, ".", args);
+	ggzmod_ggz_set_module(ggz, ".", args);
 
 	printf("Connecting.\n");
-	if (ggzmod_connect(ggz) < 0) {
+	if (ggzmod_ggz_connect(ggz) < 0) {
 		printf("Can't connect!\n");
 		return -1;
 	}
 
 	printf("Cleaning up.\n");
-	ggzmod_free(ggz);
+	ggzmod_ggz_free(ggz);
 
 	printf("Test succeeded.\n");
 	return 0;
