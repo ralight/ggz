d72e20de3cc099ef858e4eb0757aca09a23f871f
josef <devnull@localhost>
2005-08-06 02:42:55 -0700

    [svn] - make sure the traitors of 'liboggz' don't get into our packages line

--- playground/maintenance/debian-depgraph.sh
+++ playground/maintenance/debian-depgraph.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 apt-cache dotty \
-	`apt-cache search ggz | cut -d " " -f 1` | \
+	`apt-cache search ggz | cut -d " " -f 1 | grep ggz | grep -v oggz` | \
 	egrep '(ggz|^[^"]|})' > ggz-dependencies.deps
 
-dot -Tps -oggz-dependencies.ps ggz-dependencies.deps
-dot -Tpng -oggz-dependencies.png ggz-dependencies.deps
+dot -Tps -oggz-debian-depgraph.ps ggz-dependencies.deps
+dot -Tpng -oggz-debian-depgraph.png ggz-dependencies.deps
 
 rm ggz-dependencies.deps
 
