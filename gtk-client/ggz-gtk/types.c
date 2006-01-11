6031a2cf134cc2bf2039a8ecddff9269713cdf0b
jdorje <devnull@localhost>
2006-01-11 15:35:09 -0800

    [svn] Minor cleanups.

--- gtk-client/ggz-gtk/types.c
+++ gtk-client/ggz-gtk/types.c
@@ -2,7 +2,7 @@
  * File: info.c
  * Author: Justin Zaun
  * Project: GGZ GTK Client
- * $Id: types.c 7718 2006-01-03 06:48:59Z jdorje $
+ * $Id: types.c 7768 2006-01-11 23:35:09Z jdorje $
  *
  * This dialog is used to display information about a selected room to
  * the user.
@@ -84,7 +84,6 @@ static void update_dlg_types(void)
 		menu = gtk_option_menu_get_menu(GTK_OPTION_MENU(tmp));
 		menuitem = gtk_menu_item_new_with_label(name);
 		gtk_menu_append(GTK_MENU(menu), menuitem);
-		gtk_widget_show(menuitem);
 	}
 }
 
@@ -109,9 +108,8 @@ void types_create_or_raise(void)
 		menu = gtk_option_menu_get_menu(GTK_OPTION_MENU(tmp));
 		menuitem = gtk_menu_item_new_with_label(_("None"));
 		gtk_menu_append(GTK_MENU(menu), menuitem);
-		gtk_widget_show(menuitem);
 
-		gtk_widget_show(types_dialog);
+		gtk_widget_show_all(types_dialog);
 	} else {
 		gdk_window_raise(types_dialog->window);
 	}
@@ -199,11 +197,8 @@ static GtkWidget *tree_new(GtkWidget * parent)
 							  NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(tree), column);
 
-	gtk_widget_ref(tree);
-	g_object_set_data_full(G_OBJECT(parent), "types_list",
-			       tree, (GtkDestroyNotify) gtk_widget_unref);
+	g_object_set_data(G_OBJECT(parent), "types_list", tree);
 	g_object_set_data(G_OBJECT(parent), "table_list_store", store);
-	gtk_widget_show(tree);
 	GTK_WIDGET_UNSET_FLAGS(tree, GTK_CAN_FOCUS);
 
 	select = gtk_tree_view_get_selection(GTK_TREE_VIEW(tree));
@@ -234,15 +229,8 @@ GtkWidget *create_dlg_types(void)
 	gtk_window_set_title(GTK_WINDOW(dlg_types), _("Game Types"));
 
 	vbox = GTK_DIALOG(dlg_types)->vbox;
-	gtk_widget_show(vbox);
-
 
 	scrolledwindow = gtk_scrolled_window_new(NULL, NULL);
-	gtk_widget_ref(scrolledwindow);
-	g_object_set_data_full(G_OBJECT(dlg_types), "scrolledwindow",
-			       scrolledwindow,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(scrolledwindow);
 	gtk_box_pack_start(GTK_BOX(vbox), scrolledwindow, TRUE, TRUE, 0);
 	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolledwindow),
 				       GTK_POLICY_AUTOMATIC,
@@ -252,26 +240,14 @@ GtkWidget *create_dlg_types(void)
 	gtk_container_add(GTK_CONTAINER(scrolledwindow), types_list);
 
 	hbox = gtk_hbox_new(FALSE, 4);
-	gtk_widget_ref(hbox);
-	g_object_set_data_full(G_OBJECT(dlg_types), "hbox1", hbox,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(hbox);
 	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
 
 	filter_label = gtk_label_new(_("Room List Filter:"));
-	gtk_widget_ref(filter_label);
-	g_object_set_data_full(G_OBJECT(dlg_types), "filter_label",
-			       filter_label,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(filter_label);
 	gtk_box_pack_start(GTK_BOX(hbox), filter_label, FALSE, FALSE, 0);
 
 	filter_optionmenu = gtk_option_menu_new();
-	gtk_widget_ref(filter_optionmenu);
-	g_object_set_data_full(G_OBJECT(dlg_types), "filter_optionmenu",
-			       filter_optionmenu,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(filter_optionmenu);
+	g_object_set_data(G_OBJECT(dlg_types), "filter_optionmenu",
+			  filter_optionmenu);
 	gtk_box_pack_start(GTK_BOX(hbox), filter_optionmenu, FALSE, FALSE, 0);
 
 	filter_optionmenu_menu = gtk_menu_new();
@@ -279,32 +255,15 @@ GtkWidget *create_dlg_types(void)
 				 filter_optionmenu_menu);
 
 	filter_button = gtk_button_new_with_label(_("Set"));
-	gtk_widget_ref(filter_button);
-	g_object_set_data_full(G_OBJECT(dlg_types), "filter_button",
-			       filter_button,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(filter_button);
 	gtk_box_pack_start(GTK_BOX(hbox), filter_button, FALSE, FALSE, 0);
 
 	action_area = GTK_DIALOG(dlg_types)->action_area;
-	gtk_widget_show(action_area);
-
 
 	button_box = gtk_hbutton_box_new();
-	gtk_widget_ref(button_box);
-	g_object_set_data_full(G_OBJECT(dlg_types), "button_box",
-			       button_box,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(button_box);
 	gtk_box_pack_start(GTK_BOX(action_area), button_box, TRUE, TRUE,
 			   0);
 
 	close_button = gtk_button_new_from_stock(GTK_STOCK_CLOSE);
-	gtk_widget_ref(close_button);
-	g_object_set_data_full(G_OBJECT(dlg_types),
-			       "close_button", close_button,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(close_button);
 	gtk_container_add(GTK_CONTAINER(button_box), close_button);
 	GTK_WIDGET_SET_FLAGS(close_button, GTK_CAN_DEFAULT);
 
@@ -333,19 +292,9 @@ GtkWidget *create_menu1(void)
 	menu1_accels = gtk_menu_ensure_uline_accel_group(GTK_MENU(menu1));
 
 	upgrage_game = gtk_menu_item_new_with_label(_("Upgrage Game"));
-	gtk_widget_ref(upgrage_game);
-	g_object_set_data_full(G_OBJECT(menu1), "upgrage_game",
-			       upgrage_game,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(upgrage_game);
 	gtk_container_add(GTK_CONTAINER(menu1), upgrage_game);
 
 	goto_web_site = gtk_menu_item_new_with_label(_("Goto Web Site"));
-	gtk_widget_ref(goto_web_site);
-	g_object_set_data_full(G_OBJECT(menu1), "goto_web_site",
-			       goto_web_site,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(goto_web_site);
 	gtk_container_add(GTK_CONTAINER(menu1), goto_web_site);
 
 	g_signal_connect(GTK_OBJECT(upgrage_game), "activate",
