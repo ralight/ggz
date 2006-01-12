b2dc15312d93a4d7f698aad8a51863f5259c3828
jdorje <devnull@localhost>
2006-01-11 16:14:00 -0800

    [svn] Minor cleanups.

--- gtk-client/ggz-gtk/roominfo.c
+++ gtk-client/ggz-gtk/roominfo.c
@@ -2,7 +2,7 @@
  * File: info.c
  * Author: Justin Zaun
  * Project: GGZ GTK Client
- * $Id: roominfo.c 7729 2006-01-06 05:14:03Z jdorje $
+ * $Id: roominfo.c 7771 2006-01-12 00:14:00Z jdorje $
  *
  * This dialog is used to display information about a selected room to
  * the user. 
@@ -63,99 +63,55 @@ static GtkWidget *create_dlg_info(GtkWidget *parent)
 					       GTK_WINDOW(parent), 0,
 					       GTK_STOCK_CLOSE,
 					       GTK_RESPONSE_CLOSE, NULL);
-	g_object_set_data(G_OBJECT(dlg_info), "dlg_info", dlg_info);
 
 	dialog_vbox = GTK_DIALOG(dlg_info)->vbox;
-	g_object_set_data(G_OBJECT(dlg_info), "dialog_vbox", dialog_vbox);
-	gtk_widget_show(dialog_vbox);
 
 	display_table = gtk_table_new(4, 3, FALSE);
-	gtk_widget_ref(display_table);
-	g_object_set_data_full(G_OBJECT(dlg_info), "display_table",
-			       display_table,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(display_table);
 	gtk_box_pack_start(GTK_BOX(dialog_vbox), display_table,
 			   FALSE, TRUE, 0);
 
 	game_pixmap = gtk_image_new_from_stock(GTK_STOCK_DIALOG_QUESTION,
 					       GTK_ICON_SIZE_DIALOG);
-	gtk_widget_ref(game_pixmap);
-	g_object_set_data_full(G_OBJECT(dlg_info), "game_pixmap",
-			       game_pixmap,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(game_pixmap);
 	gtk_table_attach(GTK_TABLE(display_table), game_pixmap,
 			 0, 1, 0, 4, 0, 0, 0, 0);
 
 	/* Add 'name' label and text. */
 	name_label = gtk_label_new(_("Game Name:"));
-	gtk_widget_ref(name_label);
-	g_object_set_data_full(G_OBJECT(dlg_info), "name_label",
-			       name_label,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(name_label);
 	gtk_table_attach(GTK_TABLE(display_table), name_label,
 			 1, 2, 0, 1, 0, 0, 0, 0);
 
 	name = gtk_label_new(NULL);
-	gtk_widget_ref(name);
-	g_object_set_data_full(G_OBJECT(dlg_info), "name", name,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(name);
+	g_object_set_data(G_OBJECT(dlg_info), "name", name);
 	gtk_table_attach(GTK_TABLE(display_table), name,
 			 2, 3, 0, 1, GTK_EXPAND, 0, 0, 0);
 
 	/* Add 'author' label and text. */
 	author_label = gtk_label_new(_("Author:"));
-	gtk_widget_ref(author_label);
-	g_object_set_data_full(G_OBJECT(dlg_info), "author_label",
-			       author_label,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(author_label);
 	gtk_table_attach(GTK_TABLE(display_table), author_label,
 			 1, 2, 1, 2, 0, 0, 0, 0);
 
 	author = gtk_label_new(NULL);
-	gtk_widget_ref(author);
-	g_object_set_data_full(G_OBJECT(dlg_info), "author", author,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(author);
+	g_object_set_data(G_OBJECT(dlg_info), "author", author);
 	gtk_table_attach(GTK_TABLE(display_table), author,
 			 2, 3, 1, 2, GTK_EXPAND, 0, 0, 0);
 
 	/* Add 'homepage' label and text. */
 	www_label = gtk_label_new(_("Homepage:"));
-	gtk_widget_ref(www_label);
-	g_object_set_data_full(G_OBJECT(dlg_info), "www_label", www_label,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(www_label);
 	gtk_table_attach(GTK_TABLE(display_table), www_label,
 			 1, 2, 2, 3, 0, 0, 0, 0);
 
 	www = gtk_label_new(NULL);
-	gtk_widget_ref(www);
-	g_object_set_data_full(G_OBJECT(dlg_info), "www", www,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(www);
+	g_object_set_data(G_OBJECT(dlg_info), "www", www);
 	gtk_table_attach(GTK_TABLE(display_table), www,
 			 2, 3, 2, 3, GTK_EXPAND, 0, 0, 0);
 
 	/* Add 'description' label and text. */
 	desc_label = gtk_label_new(_("Room Description:"));
-	gtk_widget_ref(desc_label);
-	g_object_set_data_full(G_OBJECT(dlg_info), "desc_label",
-			       desc_label,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(desc_label);
 	gtk_table_attach(GTK_TABLE(display_table), desc_label,
 			 1, 2, 3, 4, 0, 0, 0, 0);
 
 	desc = gtk_label_new(NULL);
-	gtk_widget_ref(desc);
-	g_object_set_data_full(G_OBJECT(dlg_info), "desc", desc,
-			       (GtkDestroyNotify) gtk_widget_unref);
-	gtk_widget_show(desc);
+	g_object_set_data(G_OBJECT(dlg_info), "desc", desc);
 	gtk_table_attach(GTK_TABLE(display_table), desc,
 			 2, 3, 3, 4, GTK_EXPAND, 0, 0, 0);
 
@@ -185,34 +141,34 @@ void room_info_create_or_raise(GGZRoom * room)
 
 	if (!dialog) {
 		dialog = create_dlg_info(win_main);
-		gtk_widget_show(dialog);
+		gtk_widget_show_all(dialog);
 	} else {
 		gdk_window_show(dialog->window);
 		gdk_window_raise(dialog->window);
 	}
 
-	tmp = g_object_get_data(G_OBJECT(dialog), "name");
+	tmp = lookup_widget(dialog, "name");
 	if (gt)
 		text = ggzcore_gametype_get_name(gt);
 	else
 		text = _("This room has no game");
 	gtk_label_set_text(GTK_LABEL(tmp), text);
 
-	tmp = g_object_get_data(G_OBJECT(dialog), "author");
+	tmp = lookup_widget(dialog, "author");
 	if (gt)
 		text = ggzcore_gametype_get_author(gt);
 	else
 		text = _("N/A");
 	gtk_label_set_text(GTK_LABEL(tmp), text);
 
-	tmp = g_object_get_data(G_OBJECT(dialog), "www");
+	tmp = lookup_widget(dialog, "www");
 	if (gt)
 		text = ggzcore_gametype_get_url(gt);
 	else
 		text = _("N/A");
 	gtk_label_set_text(GTK_LABEL(tmp), text);
 
-	tmp = g_object_get_data(G_OBJECT(dialog), "desc");
+	tmp = lookup_widget(dialog, "desc");
 	text = ggzcore_room_get_name(room);
 	if (!text)
 		text = _("Unknown room");
