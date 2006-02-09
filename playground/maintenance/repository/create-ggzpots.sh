a1cc8f7d2746ffcace4e4e66ec600fedbc68ab61
josef <devnull@localhost>
2006-02-09 04:27:48 -0800

    [svn] - support Java translation for catalog collection generation

--- playground/maintenance/repository/create-ggzpots.sh
+++ playground/maintenance/repository/create-ggzpots.sh
@@ -3,6 +3,7 @@
 rm -rf ggzpots .ggzpots.files ggzpots.tar.gz
 
 find . -name *.pot -exec echo {} > .ggzpots.files \;
+find . -name messages.properties -exec echo {} >> .ggzpots.files \;
 
 mkdir ggzpots
 for i in `cat .ggzpots.files`; do
