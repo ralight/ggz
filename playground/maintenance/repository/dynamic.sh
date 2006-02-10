d412e24d93677f913416b2e6fcbceababb3ddfbf
josef <devnull@localhost>
2006-02-10 05:32:25 -0800

    [svn] - make i18n/art statistics scripts work with SVN checkouts

--- playground/maintenance/repository/dynamic.sh
+++ playground/maintenance/repository/dynamic.sh
@@ -2,9 +2,13 @@
 # Maintenance script
 # Create GGZ message catalog archive and statistics file
 
-cd $HOME/ggz-cvs/current
+cd $HOME/ggz-svn/trunk
+echo "update sources"
 ./update-all.sh
+echo "create translation catalog pack"
 ./create-ggzpots.sh
+echo "generate statistics and create art pack"
+./statistics-art.sh > artstats.txt
 ./statistics-ggzpos.sh > postats.txt
 ./statistics-webpages.sh > wwwstats.txt
 
--- playground/maintenance/repository/install-dev.sh
+++ playground/maintenance/repository/install-dev.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-base=/home/ggz/ggz-cvs/_current/
+base=/home/ggz/ggz-svn/trunk/
 target=/var/www/ggz/www/dev/i18n/
+targetart=/var/www/ggz/www/dev/art/
 
 cp $base/postats.txt $target
 cp $base/wwwstats.txt $target
 cp $base/ggzpots.tar.gz $target
+
+cp $base/artstats.txt $targetart
+cp $base/ggzgraphics-*.tar.gz $targetart
+
