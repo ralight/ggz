6791d43dfa4c15fc0d49315369eb501be1c26f11
josef <devnull@localhost>
2006-03-14 05:55:27 -0800

    [svn] - ladies and gentlemen, the new synchroniser can resolve out-of-sync problems!
    e.g. call synchroniser.py --copy NEWS libggz
    if NEWS is most recent in libggz

--- playground/maintenance/synchoniser.py
+++ playground/maintenance/synchoniser.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 #
 # Checks for file synchronisation consistency in GGZ SVN
+# Enforces consistency per item with --copy <file> <master-position>
 
 import os
+import sys
 
 pos_cc = ("kde-client", "gnome-client", "gtk-client", "txt-client")
 pos_games = ("gtk-games", "kde-games", "sdl-games")
-pos_other = ("libggz", "ggz-client-libs", "ggzd", "docs", "grubby", "python", "utils")
+pos_other = ("libggz", "ggz-client-libs", "ggzd", "docs", "grubby", "python", "utils", "java")
 
 positions_readmeggz = pos_cc + pos_games + pos_other
 positions_quickstartggz = pos_cc + pos_games + ("libggz", "ggz-client-libs", "docs", "python")
@@ -16,10 +18,14 @@ positions_i18nmk = ("ggz-client-libs/m4", "gnome-client/m4", "grubby/m4", "gtk-c
 	"python/po", "txt-client/m4")
 positions_tttai = ("playground/ggz-ruby/ai-ttt", "ggzd/game_servers/tictactoe")
 positions_chessai = ("python/chess", "grubby/games/guru-chess", "ggzd/game_servers/chess")
+positions_news = pos_cc + pos_games + pos_other
+positions_authors = pos_cc + pos_games + pos_other
 
 files = {}
 files["README.GGZ"] = positions_readmeggz
 files["QuickStart.GGZ"] = positions_quickstartggz
+files["NEWS"] = positions_news
+files["AUTHORS"] = positions_authors
 files["m4/ggz.m4"] = positions_ggzm4
 files["autogen.sh"] = positions_autogensh
 files["i18n.mk"] = positions_i18nmk
@@ -31,28 +37,64 @@ files["ai.c"] = positions_chessai
 def filesize(f):
 		size = 0
 		try:
-			size = os.stat(p + "/" + fn)[6]
+			size = os.stat(f)[6]
 		except:
 			size = 0
 		return size
 
-for fn in files:
-	positions = files[fn]
-	print "++", fn
-	hash = {}
+def filecopy(fnsrc, fndst):
+	#print "-- copy!", fnsrc, fndst
+	f = open(fnsrc)
+	f2 = file(fndst, "w")
+	contents = f.read()
+	f2.write(contents)
+	#while(f.read())
+	f.close()
+	f2.close()
+
+def check(files):
+	for (fn, positions) in files.items():
+		print "++", fn
+		hash = {}
+		for p in positions:
+			#print p
+			f = os.path.join(p, fn)
+			size = filesize(f)
+			hash.setdefault(size, [])
+			hash[size].append(f)
+		if len(hash) == 1:
+			print "ok!"
+		else:
+			print "differences detected!"
+			print len(hash), "groups:"
+			for (size, fgroup) in hash.items():
+				print size, ":", fgroup
+
+def copy(copyfile, masterpos, files):
+	if not files.has_key(copyfile):
+		print "error - no such file " + copyfile + "!"
+		return
+	positions = files[copyfile]
+	if not masterpos in positions:
+		print "error - no such master position " + masterpos + "!"
+		return
 	for p in positions:
-		#print p
-		f = p + "/" + fn
-		size = filesize(f)
-		if not hash.has_key(size):
-			hash[size] = []
-		hash[size].append(f)
-	if len(hash) == 1:
-		print "ok!"
-	else:
-		print "differences detected!"
-		print len(hash), "groups:"
-		for size in hash:
-			fgroup = hash[size]
-			print size, ":", fgroup
+		if not p == masterpos:
+			print "... to", p
+			fnsrc = os.path.join(masterpos, copyfile)
+			fndst = os.path.join(p, copyfile)
+			filecopy(fnsrc, fndst)
+
+masterpos = None
+copyfile = None
+
+if len(sys.argv) == 4:
+	if sys.argv[1] == "--copy":
+		copyfile = sys.argv[2]
+		masterpos = sys.argv[3]
+		print "Copy", copyfile, "from", masterpos, "..."
+		copy(copyfile, masterpos, files)
+		sys.exit(0)
+
+check(files)
 
