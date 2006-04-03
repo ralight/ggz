7589c2a2f2e14e4fb02629ff83c18e35112b93a1
josef <devnull@localhost>
2006-04-03 05:22:02 -0700

    [svn] - much nicer display of seat assignments, including highlighting one's own seat - inclusion of spectator seats and game state information

--- python/lib/playertable.py
+++ python/lib/playertable.py
@@ -1,5 +1,5 @@
 # Python library for GGZ common game client functionality
-# Copyright (C) 2004 Josef Spillner <josef@ggzgamingzone.org>
+# Copyright (C) 2004 - 2006 Josef Spillner <josef@ggzgamingzone.org>
 # Published under GNU GPL conditions
 
 import pygame
@@ -15,23 +15,43 @@ import ggzmod
 
 gettext.install("ggzpython", "/usr/local/share/locale", 1)
 
-def rect(surface, color, x1, y1, w, h, bgcolor):
-	surface.fill(bgcolor, ((x1, y1), (w, h)))
-	surface.fill(color, ((x1, y1), (1, h)))
-	surface.fill(color, ((x1, y1), (w, 1)))
-	surface.fill(color, ((x1, y1 + h), (w, 1)))
-	surface.fill(color, ((x1 + w, y1), (1, h)))
+#def rect(surface, color, x1, y1, w, h, bgcolor):
+#	surface.fill(bgcolor, ((x1, y1), (w, h)))
+#	surface.fill(color, ((x1, y1), (1, h)))
+#	surface.fill(color, ((x1, y1), (w, 1)))
+#	surface.fill(color, ((x1, y1 + h), (w, 1)))
+#	surface.fill(color, ((x1 + w, y1), (1, h)))
 
 def menurender(surface):
 	titlefont = pygame.font.SysFont("Vera Sans", 24)
 	font = pygame.font.SysFont("Vera Sans", 12)
 
+	if ggzmod.getNumSeats() == 0:
+		excuse = _("No players found. Looks like a local game.")
+		localstr = font.render(excuse, 1, (255, 255, 255))
+		surface.blit(localstr, (100, 110))
+		return
+
+	offset = 0
+
 	title = titlefont.render(_("Player table"), 1, (255, 255, 255))
-	surface.blit(title, (100, 50))
+	surface.blit(title, (100, 70))
+
+	font.set_italic(1)
+	seatstr = font.render(_("Seat number"), 1, (255, 255, 255))
+	typestr = font.render(_("Type"), 1, (255, 255, 255))
+	namestr = font.render(_("Name"), 1, (255, 255, 255))
+	font.set_italic(0)
+
+	surface.blit(seatstr, (100, 110))
+	surface.blit(typestr, (200, 110))
+	surface.blit(namestr, (300, 110))
 
 	for i in range(ggzmod.getNumSeats()):
-		type = ggzmod.getSeatType(i)
-		name = ggzmod.getSeatName(i)
+		seat = ggzmod.getSeat(i)
+		if not seat:
+			continue
+		(num, type, name) = seat
 
 		typename = ""
 		if type == ggzmod.SEAT_PLAYER:
@@ -42,22 +62,88 @@ def menurender(surface):
 			typename = _("Bot")
 		elif type == ggzmod.SEAT_RESERVED:
 			typename = _("Reserved")
+		elif type == ggzmod.SEAT_ABANDONED:
+			typename = _("Abandoned")
 		elif type == ggzmod.SEAT_NONE:
 			typename = "-"
 
-		seatstr = font.render("#" + str(i), 1, (255, 255, 255))
-		namestr = font.render(name, 1, (255, 255, 255))
-		typestr = font.render(typename, 1, (255, 255, 255))
+		color = (255, 255, 255)
+		player = ggzmod.getPlayer()
+		if player:
+			(playername, isspectator, num) = player
+			if not isspectator and num == i:
+				color = (255, 255, 0)
 
-		surface.blit(seatstr, (100, i * 20 + 100))
-		surface.blit(typestr, (150, i * 20 + 100))
-		surface.blit(namestr, (250, i * 20 + 100))
+		seatstr = font.render("#" + str(i), 1, color)
+		typestr = font.render(typename, 1, color)
+		namestr = font.render(name, 1, color)
 
-	if ggzmod.getNumSeats() == 0:
-		localstr = font.render(_("No players found. Looks like a local game."), 1, (255, 255, 255))
+		surface.blit(seatstr, (100, i * 20 + 130))
+		surface.blit(typestr, (200, i * 20 + 130))
+		surface.blit(namestr, (300, i * 20 + 130))
 
-		surface.blit(localstr, (100, 20 + 100))
-		
+	offset = ggzmod.getNumSeats() * 20 + 130 + 20 - 70
+	infooffset = 0
+
+	title = titlefont.render(_("Spectator table"), 1, (255, 255, 255))
+	surface.blit(title, (100, 70 + offset))
+
+	font.set_italic(1)
+	seatstr = font.render(_("Spectator number"), 1, (255, 255, 255))
+	namestr = font.render(_("Name"), 1, (255, 255, 255))
+	excusestr = font.render(_("No spectators present."), 1, (255, 255, 255))
+	font.set_italic(0)
+
+	if ggzmod.getNumSpectators() == 0:
+		surface.blit(excusestr, (100, 110 + offset))
+	else:
+		surface.blit(seatstr, (100, 110 + offset))
+		surface.blit(namestr, (300, 110 + offset))
+
+	for i in range(ggzmod.getNumSpectators()):
+		spectator = ggzmod.getSpectator(i)
+		if not spectator:
+			continue
+		(num, name) = spectator
+
+		color = (255, 255, 255)
+		player = ggzmod.getPlayer()
+		if player:
+			(name, isspectator, num) = player
+			if not isspectator and num == i:
+				color = (255, 255, 0)
+
+		seatstr = font.render("#" + str(i), 1, color)
+		namestr = font.render(name, 1, color)
+
+		surface.blit(seatstr, (100, i * 20 + 130 + offset))
+		surface.blit(namestr, (300, i * 20 + 130 + offset))
+
+	infooffset = ggzmod.getNumSpectators() * 20 + 130 + 20 + offset
+
+	title = titlefont.render(_("Game information"), 1, (255, 255, 255))
+	surface.blit(title, (100, 0 + infooffset))
+
+	state = _("(unknown)")
+	xstate = ggzmod.getState()
+	if xstate == ggzmod.STATE_CREATED:
+		state = _("Game was launched")
+	elif xstate == ggzmod.STATE_CONNECTED:
+		state = _("Game is connected")
+	elif xstate == ggzmod.STATE_WAITING:
+		state = _("Waiting for players")
+	elif xstate == ggzmod.STATE_PLAYING:
+		state = _("Running game")
+	elif xstate == ggzmod.STATE_DONE:
+		state = _("Game is finished")
+
+	font.set_italic(1)
+	statecaptionstr = font.render(_("Game state:"), 1, (255, 255, 255))
+	font.set_italic(0)
+	statestr = font.render(state, 1, (255, 255, 255))
+
+	surface.blit(statecaptionstr, (100, 40 + infooffset))
+	surface.blit(statestr, (100, 60 + infooffset))
 
 def show():
 	""" Prepare Playerlist """
