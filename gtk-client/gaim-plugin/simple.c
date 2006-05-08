f471dd3d9a3fbca824023916599e9fc66219a7d9
phh <devnull@localhost>
2006-05-08 10:02:27 -0700

    [svn] Thib_G patch (mainly configuration)

--- gtk-client/gaim-plugin/simple.c
+++ gtk-client/gaim-plugin/simple.c
@@ -27,8 +27,11 @@
 #define HEADER "*** Command from the gaim-ggz plugin:"
 #define FOOTER "***"
 #define GGZWRAPPER "ggz-wrapper"
-#define GGZMODULECONFIG "/home/ggz/BUILD/etc/ggz.modules"
+#define GGZMODULECONFIG "/usr/local/etc/ggz.modules"
 #define SERVER "live.ggzgamingzone.org"
+
+GtkWidget *pTable, *pLogin, *pPassword, *pCheckBox, *pServer;
+int LastGamePID;
 
 GaimCmdRet commande(GaimConversation *conv, const gchar *cmd, gchar **args, gchar **error, void *data) {
 	int argc,pid;
@@ -46,9 +49,18 @@ GaimCmdRet commande(GaimConversation *conv, const gchar *cmd, gchar **args, gcha
 	if(pid==0) {
 		char **parms=malloc(4*sizeof(char *));
 		char *sys_parm;
-		parms[0]=joueur;
-		parms[1]=jeu;
-		sys_parm=g_strdup_printf("%s -u %s -g %s -s %s", GGZWRAPPER, parms[0], parms[1], SERVER);
+		if(gaim_prefs_get_bool("/plugins/gtk/ggzgaim/guest"))
+		 {
+			parms[0]=joueur;
+			parms[1]=jeu;
+			sys_parm=g_strdup_printf("%s -u %s -g %s -s %s", GGZWRAPPER, parms[0], parms[1], gaim_prefs_get_string("/plugins/gtk/ggzgaim/server"));
+		 }
+		else
+		 {
+			parms[0]=gaim_prefs_get_string("/plugins/gtk/ggzgaim/login");
+			parms[1]=jeu;
+			sys_parm=g_strdup_printf("%s -u %s -g %s -p %s -s %s", GGZWRAPPER, parms[0], parms[1], gaim_prefs_get_string("/plugins/gtk/ggzgaim/password"), gaim_prefs_get_string("/plugins/gtk/ggzgaim/server"));
+		 }
 		parms[0]="sh";
 		parms[1]="-c";
 		parms[2]=sys_parm;
@@ -57,7 +69,11 @@ GaimCmdRet commande(GaimConversation *conv, const gchar *cmd, gchar **args, gcha
 		execv("/bin/sh", parms);
 		exit(0);
 	} else if(pid>0) {
-		gaim_conv_im_send(GAIM_CONV_IM(conv), g_strdup_printf("%s%s %s %s", HEADER, jeu,joueur, FOOTER));
+		if(gaim_prefs_get_bool("/plugins/gtk/ggzgaim/guest")) gaim_conv_im_send(GAIM_CONV_IM(conv), g_strdup_printf("%s%s %s %s", HEADER, jeu,joueur, FOOTER));
+		else gaim_conv_im_send(GAIM_CONV_IM(conv), g_strdup_printf("%s%s %s %s", HEADER, jeu,gaim_prefs_get_string("/plugins/gtk/ggzgaim/login"), FOOTER));
+
+		LastGamePID = pid;
+
 		return GAIM_CMD_RET_OK;
 	} else {
 		if(error)
@@ -89,7 +105,7 @@ gboolean PopMenu(GtkMenu *menu, GdkEvent *ev) {
 
 static void nouvelle_convers(GaimConversation *conv, void *data) {
 //	printf("Une nouvelle conversation:%s!!\n", gaim_conversation_get_name(conv));
-	GaimGtkConversation *gtkconv=GAIM_GTK_CONVERSATION(conv);
+	GaimGtkConversation *gtkconv = GAIM_GTK_CONVERSATION(conv);
 	GtkWidget *bbox=gtkconv->bbox;
 	GtkWidget *icon;
 	GtkWidget *menu;
@@ -116,7 +132,7 @@ static void nouvelle_convers(GaimConversation *conv, void *data) {
 
 	sg = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
 
-	icon = gaim_gtkconv_button_new("ggzicon", "Play Game", "Pfff", gtkconv->tooltips, GTK_SIGNAL_FUNC(PopMenu), NULL);
+	icon = gaim_gtkconv_button_new("ggzicon", "Jouer", "Pfff", gtkconv->tooltips, GTK_SIGNAL_FUNC(PopMenu), NULL);
 	if(icon == NULL) {
 		printf("Arf :/ \n");
 		return;
@@ -156,25 +172,42 @@ static void nouvelle_convers(GaimConversation *conv, void *data) {
 	gtk_widget_show(icon);
 }
 
-static void message_recu(GaimAccount *acct,char **sender, char **buffer,int flags,  void *data) {
-	printf("Recu un message de %s:%s\n", *sender, *buffer);
-}
-
 
 static void message_recu2(GaimAccount *acct,char **sender, char **buffer,int flags,  GaimConversation *conv,void *data) {
-//	printf("Recu un message de %s:%s\n", *sender, *buffer);
+
+	GtkWidget *pDialog;
 
 	if(*buffer==NULL)
 		return;
 
 	if(strstr(*buffer, HEADER)==NULL)
 		return;
+
 	char *joueur=NULL;
 	char *jeu=NULL;
 	jeu=strstr(*buffer, HEADER);
 	jeu+=strlen(HEADER);
 	jeu[strlen(jeu)-strlen(strstr(jeu, FOOTER))]='\0';
 	joueur=index(jeu, ' ');
+
+	if(strstr(jeu,"refuse_game") != NULL)
+	{
+    		pDialog = gtk_message_dialog_new (NULL,
+        		GTK_DIALOG_MODAL,
+        		GTK_MESSAGE_QUESTION,
+        		GTK_BUTTONS_OK,
+        		g_strdup_printf("Votre partie avec %s a été refusée/annulée !",*sender));
+		gtk_dialog_run(GTK_DIALOG(pDialog));
+		gtk_widget_destroy(pDialog);
+
+		//printf("Test : %d\n",LastGamePID);
+		//kill(LastGamePID, SIGKILL);
+
+		*buffer = NULL;
+
+		return;
+	}
+
 	if(joueur==NULL) {
 		printf("Pas de joueur mais un jeu: %s\n", jeu);
 		return;
@@ -184,15 +217,42 @@ static void message_recu2(GaimAccount *acct,char **sender, char **buffer,int fla
 	if(index(joueur, ' ')) {
 		joueur[strlen(joueur) - strlen(index(joueur, ' '))]='\0';
 	}
+
+    	pDialog = gtk_message_dialog_new (NULL,
+        	GTK_DIALOG_MODAL,
+        	GTK_MESSAGE_QUESTION,
+        	GTK_BUTTONS_YES_NO,
+        	"%s/%s vous propose une partie de %s, voulez vous accepter ?",*sender,joueur,jeu);
+
+	if(gtk_dialog_run(GTK_DIALOG(pDialog)) == GTK_RESPONSE_NO)
+	{
+		gtk_widget_destroy(pDialog);
+		
+		gaim_conv_im_send(GAIM_CONV_IM(gaim_conversation_new(GAIM_CONV_IM,acct,*sender)), g_strdup_printf("%srefuse_game %s", HEADER, FOOTER));
+		*buffer=NULL;
+		return;
+	}
+
+	gtk_widget_destroy(pDialog);
+
 	int pid=0;
 	pid=fork();
 	if(pid==0) {
 		//L'enfant trop fort hein :)
 		char **parms=malloc(4*sizeof(char *));
 		char *sys_parm;
-		parms[1]=g_strdup_printf("guest%d", (int) (999.0*rand()/(RAND_MAX+1.0)));
-		parms[2]=joueur;
-		sys_parm=g_strdup_printf("%s -u %s -d %s -g %s -s %s", GGZWRAPPER, parms[1], parms[2], jeu, SERVER);
+		if(gaim_prefs_get_bool("/plugins/gtk/ggzgaim/guest"))
+		 {
+			parms[1]=g_strdup_printf("guest%d", (int) (999.0*rand()/(RAND_MAX+1.0)));
+			parms[2]=joueur;
+			sys_parm=g_strdup_printf("%s -u %s -d %s -g %s -s %s", GGZWRAPPER, parms[1], parms[2], jeu, gaim_prefs_get_string("/plugins/gtk/ggzgaim/server"));
+		 }
+		else
+		 {
+			parms[1]=gaim_prefs_get_string("/plugins/gtk/ggzgaim/login");
+			parms[2]=joueur;
+			sys_parm=g_strdup_printf("%s -u %s -d %s -g %s -p %s -s %s", GGZWRAPPER, parms[1], parms[2], jeu, gaim_prefs_get_string("/plugins/gtk/ggzgaim/password"), gaim_prefs_get_string("/plugins/gtk/ggzgaim/server"));
+		 }
 		parms[0]="sh";
 		parms[1]="-c";
 		parms[2]=sys_parm;
@@ -202,6 +262,7 @@ static void message_recu2(GaimAccount *acct,char **sender, char **buffer,int fla
 		exit(0);
 	} else if(pid>0) {
 		//Ici c'est le pere je vous apprends beaucoup de choses non? ;)
+		//LastGamePID = pid;
 	} else {
 		//Ici c'est quand y a eu une erreur........
 	}
@@ -217,11 +278,11 @@ plugin_load(GaimPlugin *plugin)
 	void *conv_handle = gaim_conversations_get_handle();
 	gaim_signal_connect(conv_handle, "conversation-created",
 		plugin, GAIM_CALLBACK(nouvelle_convers), NULL);
-        gaim_signal_connect(conv_handle, "receiving-chat-msg",
-		plugin, GAIM_CALLBACK(message_recu), NULL);
+        /*gaim_signal_connect(conv_handle, "receiving-chat-msg",
+		plugin, GAIM_CALLBACK(message_recu), NULL);*/
         gaim_signal_connect(conv_handle, "receiving-im-msg",
 		plugin, GAIM_CALLBACK(message_recu2), NULL);
-	gaim_cmd_register("jeu", "w", GAIM_CMD_P_PLUGIN, GAIM_CMD_FLAG_IM|GAIM_CMD_FLAG_ALLOW_WRONG_ARGS, NULL, GAIM_CMD_FUNC(commande), "jeu <Le nom du jeu>",NULL);
+	gaim_cmd_register("jeu", "w", GAIM_CMD_P_PLUGIN, GAIM_CMD_FLAG_IM|GAIM_CMD_FLAG_ALLOW_WRONG_ARGS, NULL, GAIM_CMD_FUNC(commande), "jeu nom_du_jeu",NULL);
 	return TRUE;
 }
 
@@ -232,6 +293,81 @@ plugin_unload(GaimPlugin *plugin)
 
 	return TRUE;
 }
+
+static void
+ggzgaim_entry_change_cb(GtkObject *obj, gchar *pref)
+{
+	gaim_prefs_set_string(pref,gtk_entry_get_text(GTK_ENTRY(obj)));
+}
+ggzgaim_checkbox_change_cb(GtkObject *obj, gchar *pref)
+{
+	gaim_prefs_set_bool(pref,gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(obj)));
+}
+
+GtkWidget *
+ggzgaim_preferences_get_frame(GaimPlugin *plugin) {
+
+	pTable = gtk_table_new(5,5,TRUE);
+
+	pLogin = gtk_entry_new();
+	pPassword = gtk_entry_new();
+	pCheckBox = gtk_check_button_new();
+	pServer = gtk_entry_new();
+
+	gtk_entry_set_visibility(GTK_ENTRY(pPassword), FALSE);
+
+
+	gtk_entry_set_text(GTK_ENTRY(pLogin),gaim_prefs_get_string("/plugins/gtk/ggzgaim/login"));
+	gtk_entry_set_text(GTK_ENTRY(pPassword),gaim_prefs_get_string("/plugins/gtk/ggzgaim/password"));
+	gtk_entry_set_text(GTK_ENTRY(pServer),gaim_prefs_get_string("/plugins/gtk/ggzgaim/server"));
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(pCheckBox), gaim_prefs_get_bool("/plugins/gtk/ggzgaim/guest"));
+
+
+	gtk_table_attach_defaults(GTK_TABLE(pTable),gtk_label_new("Identification en 'guest' :"),0,2,1,2);
+	gtk_table_attach_defaults(GTK_TABLE(pTable),gtk_label_new("Pseudo :"),0,2,2,3);
+	gtk_table_attach_defaults(GTK_TABLE(pTable),gtk_label_new("Pass :"),0,2,3,4);
+	gtk_table_attach_defaults(GTK_TABLE(pTable),gtk_label_new("Serveur :"),0,2,4,5);
+
+	gtk_table_attach_defaults(GTK_TABLE(pTable),pCheckBox,3,4,1,2);
+	gtk_table_attach_defaults(GTK_TABLE(pTable),pLogin,3,5,2,3);
+	gtk_table_attach_defaults(GTK_TABLE(pTable),pPassword,3,5,3,4);
+	gtk_table_attach_defaults(GTK_TABLE(pTable),pServer,3,5,4,5);
+
+	//gaim_prefs_set_int(pref_url, int);
+
+	//gaim_prefs_add_string(const char *name, const char *value);
+	//gaim_prefs_add_bool(const char *name, gboolean value);
+	//gaim_prefs_add_int(const char *name, int value);
+
+
+	//gboolean gaim_prefs_get_bool(const char *name);
+	//int gaim_prefs_get_int(const char *name);
+
+	//const char *gaim_prefs_get_string(const char *name);
+
+
+	g_signal_connect(GTK_OBJECT(pLogin), "changed", G_CALLBACK(ggzgaim_entry_change_cb), "/plugins/gtk/ggzgaim/login");
+
+	g_signal_connect(GTK_OBJECT(pPassword), "changed", G_CALLBACK(ggzgaim_entry_change_cb), "/plugins/gtk/ggzgaim/password");
+
+	g_signal_connect(GTK_OBJECT(pServer), "changed", G_CALLBACK(ggzgaim_entry_change_cb), "/plugins/gtk/ggzgaim/server");
+
+	g_signal_connect(GTK_OBJECT(pCheckBox), "clicked", G_CALLBACK(ggzgaim_checkbox_change_cb), "/plugins/gtk/ggzgaim/guest");
+
+	gtk_widget_show_all(pTable);
+	return pTable;
+}
+
+void ggzgaim_preferences_add()
+{
+ gaim_prefs_add_none("/plugins/gtk/ggzgaim");
+ gaim_prefs_add_string("/plugins/gtk/ggzgaim/login", "");
+ gaim_prefs_add_string("/plugins/gtk/ggzgaim/password", "");
+ gaim_prefs_add_string("/plugins/gtk/ggzgaim/server", SERVER);
+ gaim_prefs_add_bool("/plugins/gtk/ggzgaim/guest", TRUE);
+}
+
+static GaimGtkPluginUiInfo ui_info = { ggzgaim_preferences_get_frame };
 
 static GaimPluginInfo info =
 {
@@ -259,7 +395,7 @@ static GaimPluginInfo info =
 	plugin_unload,                                    /**< unload         */
 	NULL,                                             /**< destroy        */
 
-	NULL,                                             /**< ui_info        */
+	&ui_info,                                             /**< ui_info        */
 	NULL                                             /**< extra_info     */
 //	NULL,
 //	NULL
@@ -268,6 +404,7 @@ static GaimPluginInfo info =
 static void
 init_plugin(GaimPlugin *plugin)
 {
+	ggzgaim_preferences_add();
 }
 
 GAIM_INIT_PLUGIN(ggz4gaim, init_plugin, info)
