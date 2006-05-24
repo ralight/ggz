25e86ae5cd26922bed4be347f20988b67c9aa8fc
josef <devnull@localhost>
2006-05-24 01:49:47 -0700

    [svn] - new article: "GGZ explained" a.k.a. "GGZ for dummies" - moving all articles to playground as they're only part of our Community instance

---
+++ playground/community/articles/index.php
@@ -0,0 +1,65 @@
+<?php $global_leftbar = "disabled"; ?>
+<?php $global_rightbar = "disabled"; ?>
+<?php include("top.inc"); ?>
+
+<div id="main">
+	<h1>
+		<span class="itemleader">:: </span>
+		Articles
+		<span class="itemleader"> :: </span>
+		<a name="articles"></a>
+	</h1>
+	<div class="text">
+	These articles are written by GGZ players.
+	If you want us to publish one, just send it.
+	</div>
+	<h2>
+		<span class="itemleader">:: </span>
+		GGZ explained
+		<span class="itemleader"> :: </span>
+		<a name="database"></a>
+	</h2>
+	<div class="text">
+	Information to people who just arrived in GGZ land.
+	<br>
+	<a href="ruby/">Read article</a>
+	</div>
+	<h2>
+		<span class="itemleader">:: </span>
+		GGZ and Ruby
+		<span class="itemleader"> :: </span>
+		<a name="database"></a>
+	</h2>
+	<div class="text">
+	An article for people interesting in doing game development
+	for GGZ, using the Ruby programming language.
+	<br>
+	<a href="ruby/">Read article</a>
+	</div>
+	<h2>
+		<span class="itemleader">:: </span>
+		GGZ Packaging
+		<span class="itemleader"> :: </span>
+		<a name="database"></a>
+	</h2>
+	<div class="text">
+	This article is mainly interesting for distributors, but also
+	players who want to have a top-to-bottom view on GGZ.
+	<br>
+	<a href="packaging/">Read article</a>
+	</div>
+	<h2>
+		<span class="itemleader">:: </span>
+		Introduction to GGZBoard
+		<span class="itemleader"> :: </span>
+		<a name="database"></a>
+	</h2>
+	<div class="text">
+	GGZBoard is a new game client framework developed using the Pygame
+	toolkit. It supports games such as Chess, Go and Reversi.
+	<br>
+	<a href="ggzboard/">Read article</a>
+	</div>
+</div>
+
+<?php include("bottom.inc"); ?>
