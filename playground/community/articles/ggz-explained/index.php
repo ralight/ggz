33518603cc87e0739634ab4b254120a8b6c114f1
josef <devnull@localhost>
2006-05-24 01:54:53 -0700

    [svn] - new article wasn't included yet :) - move all frontpage graphics here as well

---
+++ playground/community/articles/ggz-explained/index.php
@@ -0,0 +1,86 @@
+<?php $global_leftbar = "disabled"; ?>
+<?php $global_rightbar = "disabled"; ?>
+<?php include("top.inc"); ?>
+<style type="text/css">
+.article img
+	{
+		margin: 12px;
+	}
+</style>
+
+<div id="main">
+	<h1>
+		<span class="itemleader">:: </span>
+		Article: GGZ explained
+		<span class="itemleader"> :: </span>
+		<a name="articles"></a>
+	</h1>
+	<div class="text">
+	23.05.2006 - written by <a href="mailto:josef@ggzgamingzone.org">Josef Spillner</a>
+	<br><br>
+	Introduction: With GGZ becoming more popular, and targeting new users, many of you
+	are propably unaware of how a project like GGZ works. This short article will
+	present the basics of open source to you and explain what GGZ is and what we do.
+	Any humour found in the text is there purely by accident, for the text was written
+	by a German guy!
+	<br><br>
+	So, let's start. The blackboard will assist us with professional graphics.
+	<p>
+	<img src="chalk1.png" class="article">
+	</p>
+	GGZ, which is an acronym expanding to
+	<a href="http://www.ggzgamingzone.org/">GGZ Gaming Zone</a>, is an
+	open source project which seeks to provide software and services in the area
+	of online gaming. Open source means that the software is for free (gratis!!!),
+	everyone can download the games at no cost and even customise them.
+	It also means that there's no company behind GGZ, we're just a bunch of
+	individuals, mostly young people having other duties during the day and
+	then contributing to GGZ as a hobby of some sort. GGZ developers come from
+	"all over the world" (that's the small country somewhere behind Canada).
+	<p>
+	<img src="chalk2.png" class="article">
+	</p>
+	Now how GGZ works is that its developers also run their own software to
+	provide a permanent server, called GGZ Community. This is the thing you're
+	looking at right now, it contains articles and player statistics and much
+	more! The GGZ server also handles all the games you might want to play
+	in your client, and you can connect to it over a secure connection to
+	fend off secret services.
+	No account is needed, guest logins are fine to start with. No strings
+	attached, GGZ is fine technology at work!
+	This is a difference to many existing gaming sites where you need to
+	pay a lot of money and are trashed with flashy ads. Also, GGZ can never
+	go out of business - once you fall in love with a game it will always
+	be there for you.
+	<p>
+	<img src="chalk3.png" class="article">
+	</p>
+	As GGZ Community is run by GGZ developers, it's unavoidable that a lot
+	of GGZ-specific terms are in it and we're also way too busy to care about
+	every user and his or her login problem. This is why GGZ is also used
+	as a backend technology by people who offer gaming services to users like
+	you. You can be sure that if they run their game service on GGZ, all the
+	good features will be available for you, plus their additional offerings!
+	Also having such services will make it much easier to find other people
+	to chat and play with, since in the normal case GGZ Community doesn't
+	have many people logged in at the same time. If you don't encounter
+	anybody, just stay logged in for a while, that'll help finding someone.
+	Our chat bot will entertain you meanwhile.
+	<p>
+	<img src="chalk4.png" class="article">
+	</p>
+	Now the lesson is over and hopefully you've learnt something.
+	Keep in mind that GGZ develops over time, and will not change the gaming
+	world overnight. Similar to other non-profit grassroots efforts, support
+	us morally or by offering some work, we always need contributions.
+	No matter if you're much into technical things or not, we've got
+	enough
+	<a href="http://dev.ggzgamingzone.org/contribute.php">jobs</a>
+	to get done.
+	<br><br>
+	No further homework for today.
+	But please, use the sponge to clean up the blackboard.
+	</div>
+</div>
+
+<?php include("bottom.inc"); ?>
