e05c0045413b75eccfac5db259c2b493520bd769
josef <devnull@localhost>
2006-05-24 02:17:53 -0700

    [svn] - URIs now conform to new specification (Uri-spec.txt)

--- community/web/games/index.php
+++ community/web/games/index.php
@@ -15,7 +15,7 @@
 	installed.
 	If you're not sure about this, find out
 	<a href="http://www.ggzgamingzone.org/">more information</a>.
-	If you have Java 5 installed, you can now also opt for playing
+	If you have Java installed, you can now also opt for playing
 	GGZ games directly in the web browser! Just click the appropriate
 	links below.
 	</div>
@@ -43,77 +43,81 @@
 	</h1>
 	<div class="text">
 	Use this if you are on Linux/BSD and have GGZ installed.
+	You can find a dynamic view of all game rooms and running games on the
+	<a href="http://metacle.ggzgamingzone.org/">Metacle</a>.
 	<br><br>
-	<a href="ggz://live.ggzgamingzone.org/TicTacToe/">TicTacToe</a>
+	<a href="ggz://live.ggzgamingzone.org/#Tic-Tac-Toe/">Tic-Tac-Toe</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Reversi/">Reversi</a>
+	<a href="ggz://live.ggzgamingzone.org/#Reversi/">Reversi</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Chess/">Chess</a>
+	<a href="ggz://live.ggzgamingzone.org/#Chess/">Chess</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Chinese Checkers/">Chinese Checkers</a>
+	<a href="ggz://live.ggzgamingzone.org/#Chinese%20Checkers/">Chinese Checkers</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Combat/">Combat</a>
+	<a href="ggz://live.ggzgamingzone.org/#Combat/">Combat</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Connect the Dots/">Connect the Dots</a>
+	<a href="ggz://live.ggzgamingzone.org/#Connect%20The%20Dots/">Connect the Dots</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards/">GGZCards</a>
+	<a href="ggz://live.ggzgamingzone.org/#GGZ%20Cards/">GGZCards</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20Bridge/">GGZCards - Bridge</a>
+	<a href="ggz://live.ggzgamingzone.org/#Bridge/">GGZCards - Bridge</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20FortyTwo/">GGZCards - FortyTwo</a>
+	<a href="ggz://live.ggzgamingzone.org/#FortyTwo/">GGZCards - FortyTwo</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20Hearts/">GGZCards - Hearts</a>
+	<a href="ggz://live.ggzgamingzone.org/#Hearts/">GGZCards - Hearts</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20LaPocha/">GGZCards - LaPocha</a>
+	<a href="ggz://live.ggzgamingzone.org/#La%20Pocha/">GGZCards - LaPocha</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20Spades/">GGZCards - Spades</a>
+	<a href="ggz://live.ggzgamingzone.org/#Spades/">GGZCards - Spades</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20Suaro/">GGZCards - Suaro</a>
+	<a href="ggz://live.ggzgamingzone.org/#Suaro/">GGZCards - Suaro</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20Sueco/">GGZCards - Sueco</a>
+	<a href="ggz://live.ggzgamingzone.org/#Sueco/">GGZCards - Sueco</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/GGZCards%20-%20Whist/">GGZCards - Whist</a>
+	<a href="ggz://live.ggzgamingzone.org/#Whist/">GGZCards - Whist</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Hastings/">Hastings</a>
+	<a href="ggz://live.ggzgamingzone.org/#Hastings/">Hastings</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Krosswater/">Krosswater</a>
+	<a href="ggz://live.ggzgamingzone.org/#Krosswater/">Krosswater</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/LaPocha/">LaPocha</a>
+	<a href="ggz://live.ggzgamingzone.org/#Netspades/">Netspades</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Netspades/">Netspades</a>
+	<a href="ggz://live.ggzgamingzone.org/#Muehle/">Muehle</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Muehle/">Muehle</a>
+	<a href="ggz://live.ggzgamingzone.org/#Escape/">Escape</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Escape/">Escape</a>
+	<a href="ggz://live.ggzgamingzone.org/#ConnectX/">ConnectX</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/ConnectX/">ConnectX</a>
+	<a href="ggz://live.ggzgamingzone.org/#Keepalive/">Keepalive</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Keepalive/">Keepalive</a>
+	<a href="ggz://live.ggzgamingzone.org/#Geekgame/">Geekgame</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Geekgame/">Geekgame</a>
+	<a href="ggz://live.ggzgamingzone.org/#GNU%20Go%20(CGoBan%20Client)/">Go</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Go/">Go</a>
+	<a href="ggz://live.ggzgamingzone.org/#Xadrez%20Chines/">Xadrez Chinês</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Xadrez%20Chines/">Xadrez Chinês</a>
+	<a href="ggz://live.ggzgamingzone.org/#Checkers/">Checkers</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Checkers/">Checkers</a>
+	<a href="ggz://live.ggzgamingzone.org/#Hnefatafl/">Hnefatafl</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Hnefatafl/">Hnefatafl</a>
+	<a href="ggz://live.ggzgamingzone.org/#Ludo/">Ludo</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Ludo/">Ludo</a>
-	<br>
-	<a href="ggz://live.ggzgamingzone.org/Arimaa/">Arimaa</a>
+	<a href="ggz://live.ggzgamingzone.org/#Arimaa/">Arimaa</a>
 
 	<br><br>
 	If you have other games with GGZ support installed, the following will work too:
 	<br><br>
-	<a href="ggz://live.ggzgamingzone.org/Tenes%20Empanadas%20Graciela/">Tenes Empanadas Graciela</a>
+	<a href="ggz://live.ggzgamingzone.org/#Tenes%20Empanadas%20Graciela/">Tenes Empanadas Graciela</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Tuxman/">Tuxman</a>
+	<a href="ggz://live.ggzgamingzone.org/#Tuxman/">Tuxman</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Kamikaze/">Kamikaze</a>
+	<a href="ggz://live.ggzgamingzone.org/#Kamikaze/">Kamikaze</a>
 	<br>
-	<a href="ggz://live.ggzgamingzone.org/Widelands/">Widelands</a>
+	<a href="ggz://live.ggzgamingzone.org/#Widelands/">Widelands</a>
+	<br>
+	<a href="ggz://live.ggzgamingzone.org/#Freeciv%202.1/">Freeciv</a>
+	<br>
+	<a href="ggz://live.ggzgamingzone.org/#Freelords%20(experimental)/">Freelords</a>
 	<br>
 	</div>
 </div>
