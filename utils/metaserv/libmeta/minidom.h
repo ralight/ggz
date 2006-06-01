a9fcc65ed25a18431b652dd0f7207b729c72b364
josef <devnull@localhost>
2006-06-01 03:01:13 -0700

    [svn] - the big move: fold minidom library into ggzmeta - finish adaptation of ggzmeta to new ggzmeta:// protocol (makes sense, eh?) - some convenience methods for lists of metaservers and GGZ server queries - basic documentation for all methods
    Planned addition to ggzmeta: asynchronous query mode (not easy to implement)

--- utils/metaserv/libmeta/minidom.h
+++ utils/metaserv/libmeta/minidom.h
@@ -1,11 +1,25 @@
 /*
- * The GGZ Gaming Zone Metaserver Project
- * Copyright (C) 2001 Josef Spillner, dr_maux@users.sourceforge.net
- * Published under GNU GPL conditions.
+ * GGZ Metaserver access library (libggzmeta)
+ * Leightweight XML parser functions
+ * Copyright (C) 2001 - 2006 Josef Spillner <josef@ggzgamingzone.org>
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
  */
 
-#ifndef MINIDOM_H
-#define MINIDOM_H
+#ifndef GGZ_MINIDOM_H
+#define GGZ_MINIDOM_H
 
 #ifdef __cplusplus
 extern "C" {
