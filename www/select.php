31905ed10b6b57fa84e7e2d245f420881a4ee828
josef <devnull@localhost>
2006-07-05 07:44:07 -0700

    [svn] - selector page which is shown on first visit to homepage

---
+++ www/select.php
@@ -0,0 +1,38 @@
+<?php include_once("site/header.php"); ?>
+
+<table class="content" width="100%">
+<tr>
+<td colspan="2">
+<font size="3">
+
+<b>The GGZ Gaming Zone</b><br>
+Welcome! Select your destination first.
+<br>
+
+</font>
+</td>
+</tr>
+<tr>
+<td><i>GGZ Community</i></td><td><i>GGZ Gaming Zone Project</i></td>
+</tr>
+<tr>
+<td>
+If you just want to <b>play</b>, then this is your option. GGZ Community is
+the very first gaming community powered by the GGZ Gaming Zone, although others
+are being created. Go to the <a href="http://members.ii.net/~bredow/ggz/">applet page</a>
+while we're improving the <a href="http://www.ggzcommunity.org/">community webpages</a>.
+Ask questions in the <a href="http://www.ggzcommunity.org/forums/phpBB2/">forum</a>.
+</td>
+<td>
+If you want to <b>inform</b> yourself about the GGZ project, what we do and
+what we offer and who we actually are, then proceed to the
+<a href="index.php">project pages</a>. As a potential contributor (artwork,
+documentation, source code) you can also visit our
+<a href="http://dev.ggzgamingzone.org/">developer area</a> directly
+and subscribe to our mailing lists.
+</td>
+</tr>
+</table>
+
+<?php include_once("site/footer.php"); ?>
+
