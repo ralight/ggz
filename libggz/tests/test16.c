e126e26b50ef2e5e1f13f58efa138a20213f552e
jdorje <devnull@localhost>
2006-08-01 11:17:27 -0700

    [svn] Don't run test16 check if gcrypt isn't used (?).

--- libggz/tests/test16.c
+++ libggz/tests/test16.c
@@ -1,3 +1,31 @@
+/*
+ * File: test16.c
+ * Author: GGZ Dev Team
+ * Project: libggz
+ * Date: 
+ * $Id: easysock.c 8165 2006-06-11 23:01:42Z oojah $
+ *
+ * Copyright (C) 2006 GGZ Development Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,6 +33,7 @@
 
 int main(int argc, char *argv[])
 {
+#ifdef USE_GCRYPT
 	char *enc;
 	char plain[7];
 	char expected[10];
@@ -20,7 +49,7 @@ int main(int argc, char *argv[])
 	if(strcmp(enc, expected)) return 1;
 
 	ggz_free(enc);
+#endif
 
 	return 0;
 }
-
