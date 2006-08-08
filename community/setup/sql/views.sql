97dc75d0d7223703607e16f67c774addb20401d3
josef <devnull@localhost>
2006-08-08 02:06:10 -0700

    [svn] - addition of host players into privileges mask - addition of table_privmsg privileges

--- community/setup/sql/views.sql
+++ community/setup/sql/views.sql
@@ -11,10 +11,12 @@ SELECT users.handle,
 ((users.permissions & 032) >> 5) AS chat_bot,
 ((users.permissions & 064) >> 6) AS no_stats,
 ((users.permissions & 128) >> 7) AS edit_tables;
+((users.permissions & 256) >> 8) AS table_privmsg;
 
 CREATE VIEW permissionmasks AS
 SELECT users.handle,
-((users.permissions & (008 + 016 + 128)) = 008 + 016 + 128) AS admin_mask,
-((users.permissions & (001 + 002 + 064)) = 001 + 002 + 064) AS anon_mask,
-((users.permissions & (001 + 002 + 004)) = 001 + 002 + 004) AS normal_mask;
+((users.permissions & (008 + 016 + 128 + 256)) = 008 + 016 + 128 + 256) AS admin_mask,
+((users.permissions & (128 + 256)) = 128 + 256) AS host_mask,
+((users.permissions & (001 + 002 + 004)) = 001 + 002 + 004) AS normal_mask,
+((users.permissions & (001 + 002 + 064)) = 001 + 002 + 064) AS anon_mask;
 
