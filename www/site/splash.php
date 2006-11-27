4cec90650ffa14e52d701a7b2810e6b449d67d16
josef <devnull@localhost>
2006-11-27 01:51:54 -0800

    [svn] - modernised web design using XHTML and external CSS - includes fancier frontpage

--- www/site/splash.php
+++ www/site/splash.php
@@ -1,1 +1,1 @@
-<a href="http://games.ggzcommunity.org/"><img src="/pics/splash/spaders.png" border="0"></a>
+<a href="http://games.ggzcommunity.org/"><img src="/pics/splash/spaders2.png" border="0"></a>
