6ee563460b8ef30f339b92399521d2306a7c2054
jdorje <devnull@localhost>
2006-12-26 20:05:26 -0800

    [svn] Fix bad coding in ggz_perms_set.

--- libggz/src/ggz_common.c
+++ libggz/src/ggz_common.c
@@ -3,7 +3,7 @@
  * Author: GGZ Dev Team
  * Project: GGZ Common Library
  * Date: 01/13/2002
- * $Id: ggz_common.c 8759 2006-12-27 04:02:24Z jdorje $
+ * $Id: ggz_common.c 8760 2006-12-27 04:05:26Z jdorje $
  *
  * This provides GGZ-specific functionality that is common to
  * some or all of the ggz-server, game-server, ggz-client, and
@@ -462,7 +462,13 @@ void ggz_perms_init_from_list(GGZPermset *perms, GGZPerm *list, size_t listsz)
 void ggz_perms_set(GGZPermset *perms, GGZPerm perm, bool val)
 {
 	if (perm >= 0 || perm < GGZ_PERM_COUNT) {
-		(*perms) |= (1 << perm);
+		if (val) {
+			/* Set bit */
+			(*perms) |= (1 << perm);
+		} else {
+			/* Clear bit. */
+			(*perms) &= ~(1 << perm);
+		}
 	}
 	/* Else fail silently. */
 }
