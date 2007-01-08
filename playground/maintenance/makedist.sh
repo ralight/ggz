881fb764668e99f4aaec45cd49e7eae0434861f0
josef <devnull@localhost>
2007-01-08 05:51:13 -0800

    [svn] - much better automatic tarball creation facility
    [commit from josef@nada]

--- playground/maintenance/makedist.sh
+++ playground/maintenance/makedist.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
-version=0.0.9
+version=0.0.14
 base=$PWD
+
+prefix=/tmp/ggz-dist-$version
+DISTCHECK_CONFIGURE_FLAGS=--prefix=$prefix
 
 echo "GGZ $version Distribution"
 for i in *; do
-	if test -d $i && test -f $i/autogen.sh; then
+	if test -d $i && test -f $i/Makefile.am; then
 		echo $i...
 		cd $i
-		./autogen.sh >/dev/null
-		make DESTDIR=/tmp/dist-$i distcheck >/dev/null
+		./autogen.sh --prefix=$prefix >/dev/null
+		make distcheck >/dev/null
 		cp *-$version.tar.gz ..
+		cd $base
+	elif test -d $i && test -f $i/Makefile; then
+		echo $i...
+		cd $i
+		if test -f autogen.sh; then
+			./autogen.sh --prefix=$prefix >/dev/null
+		fi
+		make dist >/dev/null
+		#cp ../*-$version.tar.gz ..
 		cd $base
 	fi
 done
