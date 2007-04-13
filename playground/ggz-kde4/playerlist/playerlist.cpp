12146cce216646e095cc13ee7844df0bba6e8367
josef <devnull@localhost>
2007-04-13 01:23:27 -0700

    [svn] - copy list of player icons from kggz - display an example of friend players and host/admin separation

--- playground/ggz-kde4/playerlist/playerlist.cpp
+++ playground/ggz-kde4/playerlist/playerlist.cpp
@@ -4,48 +4,72 @@
 // Qt includes
 #include <qtreeview.h>
 #include <qstandarditemmodel.h>
-#include <qheaderview.h>
 #include <qlayout.h>
 #include <qpainter.h>
 #include <qpushbutton.h>
+#include <qlineedit.h>
+#include <qlabel.h>
 
 PlayerList::PlayerList()
 : QWidget()
 {
 	QTreeView *treeview = new QTreeView();
 
+	QLineEdit *searchbox = new QLineEdit();
+	QLabel *searchlabel = new QLabel("Search for:");
+
+	QHBoxLayout *hbox = new QHBoxLayout();
+	hbox->addWidget(searchlabel);
+	hbox->addWidget(searchbox);
+
 	QVBoxLayout *vbox = new QVBoxLayout();
 	vbox->addWidget(treeview);
+	vbox->addLayout(hbox);
 	setLayout(vbox);
 
 	QStandardItemModel *model = new QStandardItemModel();
 	model->setColumnCount(2);
 
+	QStandardItem *item01 = new QStandardItem();
+	item01->setText("Friends");
+	model->appendRow(item01);
+
+	QStandardItem *item02 = new QStandardItem();
+	item02->setText("Others");
+	model->appendRow(item02);
+
 	QStandardItem *item11 = new QStandardItem();
-	item11->setText("foo");
+	item11->setIcon(QIcon("players/admin.png"));
+	item11->setText("someadmin");
 	QStandardItem *item12 = new QStandardItem();
-	item12->setText("foo2");
-	QList<QStandardItem*> list1;
+	item12->setIcon(QIcon("players/lag5.png"));
+	item12->setText("0-1-5");
+	/*QList<QStandardItem*> list1;
 	list1 << item11;
 	list1 << item12;
-	model->appendRow(list1);
+	model->appendRow(list1);*/
 
 	QStandardItem *item21 = new QStandardItem();
-	item21->setText("bar");
+	item21->setIcon(QIcon("players/host.png"));
+	item21->setText("somehost");
 	QStandardItem *item22 = new QStandardItem();
-	item22->setText("bar2");
-	QList<QStandardItem*> list2;
+	item22->setIcon(QIcon("players/lag3.png"));
+	item22->setText("12-4-5");
+	/*QList<QStandardItem*> list2;
 	list2 << item21;
 	list2 << item22;
-	model->appendRow(list2);
+	model->appendRow(list2);*/
 
-	//QHeaderView *header = new QHeaderView();
-	model->setHeaderData(0, Qt::Horizontal, QString("arg"), Qt::DisplayRole);
-	model->setHeaderData(1, Qt::Horizontal, QString("haha"), Qt::DisplayRole);
+	item01->setChild(0, 0, item21);
+	item01->setChild(0, 1, item22);
+
+	item02->setChild(0, 0, item11);
+	item02->setChild(0, 1, item12);
+
+	model->setHeaderData(0, Qt::Horizontal, QString("Player"), Qt::DisplayRole);
+	model->setHeaderData(1, Qt::Horizontal, QString("Properties"), Qt::DisplayRole);
 
 	treeview->setModel(model);
-	//treeview->setEditTriggers(QAbstractItemView::NoEditTriggers);
-	//treeview->setHeader(header);
 
 	setWindowTitle("GGZ gets a more flexible player list!");
 	resize(320, 300);
