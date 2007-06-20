d5cae9b197c1e1bb61797796a47eabf0f2df48e6
josef <devnull@localhost>
2007-06-20 09:47:34 -0700

    [svn] - script improvements for spurious last line removals and deletion of non-relevant translations (saves us many kB or even MB)
    [commit from josef@nada]

--- playground/maintenance/merge-from-gnome.sh
+++ playground/maintenance/merge-from-gnome.sh
@@ -19,10 +19,24 @@ for i in $gnomepolocation/*.po; do
 	echo "# (http://svn.gnome.org/svn/gnome-games/trunk/po/$filename)" >> $filename
 	echo "" >> $filename
 	#grep -v "#~" $filename.tmp >> $filename
-	csplit $filename.tmp /#~/
+	csplit $filename.tmp /#~/ >/dev/null
 	cat xx00 >> $filename
 	rm -f xx00 xx01
 	rm -f $filename.tmp
+
+	lastline=`tail -1 $filename`
+	if [ ! -z "$lastline" ]; then
+		numlines=`wc -l $filename | cut -d " " -f 1`
+		numlines=$[$numlines - 1]
+		head -$numlines $filename > $filename.tmp
+		mv $filename.tmp $filename
+	fi
+
+	numtranslated=`potool -ft -fnf -s $filename`
+	if [ $numtranslated -eq "1" ]; then
+		rm -f $filename
+		svn remove $filename >/dev/null
+	fi
 done
 
 
