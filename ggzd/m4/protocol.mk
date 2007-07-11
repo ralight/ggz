53bca7a83d343dd43a127d0b28b5b51ecf902de2
josef <devnull@localhost>
2007-07-10 23:23:28 -0700

    [svn] - helper build tool to create protocol handler source files
    [commit from josef@nada]

---
+++ ggzd/m4/protocol.mk
@@ -0,0 +1,9 @@
+protocolhandler:
+	$(top_srcdir)/../utils/ggzcomm/ggzcommgen \
+		--language c --interface dio --role server \
+		$(top_srcdir)/../utils/ggzcomm/$(PROTOCOL)
+	mv $(PROTOCOL)_server.c $(SOURCEFILE).c
+	mv $(PROTOCOL)_server.h $(SOURCEFILE).h
+	indent -kr -i8 -nsai -nsaf -cli8 -bl -bli0 $(SOURCEFILE).c $(SOURCEFILE).h
+	sed -i 's/$(PROTOCOL)_server.h/$(SOURCEFILE).h/' $(SOURCEFILE).c
+
