bfcbb3c1e7c849cb32dba8157f328e8b10264194
josef <devnull@localhost>
2007-12-30 08:09:32 -0800

    [svn] - traditional end-of-the-year hack: real-time stats from ggzd!

---
+++ playground/server/ggztop/ggztop.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <ncurses.h>
+
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include "statsrt.h"
+
+#define COL_RED 1
+#define COL_WHITE 2
+
+static int stats_rt_shmid = 0;
+
+int ggztop_init(void)
+{
+	stats_rt_shmid = shmget(STATS_RT_SHMID, sizeof(stats_rt), S_IRUSR | S_IWUSR);
+	if(stats_rt_shmid < 0)
+	{
+		fprintf(stderr, "ggztop: shared memory setup failure (%s)\n",
+			strerror(errno));
+		return 0;
+	}
+
+	return 1;
+}
+
+void ggztop_display_top_screen(stats_rt *rt)
+{
+	int i;
+	int line = 4;
+
+	clear();
+
+	move(0, 0);
+	attron(COLOR_PAIR(COL_WHITE) | A_BOLD);
+	printw("ggztop - real-time statistics from ggzd");
+	attroff(COLOR_PAIR(COL_WHITE) | A_BOLD);
+
+	move(1, 0);
+	printw("Uptime: %i secs", rt->uptime);
+	printw(", Rooms: %i", rt->num_rooms);
+	printw(", Players: %i", rt->num_players);
+	printw(", Tables: %i", rt->num_tables);
+
+	move(3, 0);
+	printw("Room");
+	move(3, 30);
+	printw("Players");
+	move(3, 40);
+	printw("Tables");
+
+	for(i = 0; i < rt->num_rooms; i++)
+	{
+		if(rt->players[i] > 0)
+		{
+			move(line, 0);
+			attron(COLOR_PAIR(COL_RED));
+			printw(rt->rooms[i]);
+			attroff(COLOR_PAIR(COL_RED));
+
+			move(line, 30);
+			printw("%i", rt->players[i]);
+			move(line, 40);
+			printw("%i", rt->tables[i]);
+
+			line++;
+		}
+	}
+}
+
+void ggztop_display_top(stats_rt *rt)
+{
+	printf("\033[2J");
+	initscr();
+	cbreak();
+	noecho();
+	nodelay(stdscr, TRUE);
+	start_color();
+	use_default_colors();
+	init_pair(COL_RED, COLOR_RED, -1);
+	init_pair(COL_WHITE, COLOR_WHITE, -1);
+
+	while(1)
+	{
+		ggztop_display_top_screen(rt);
+		int input = getch();
+		if(input != -1)
+			break;
+		sleep(1);
+	}
+
+	curs_set(1);
+	endwin();
+}
+
+void ggztop_display_text(stats_rt *rt)
+{
+	int i;
+
+	for(i = 0; i < rt->num_rooms; i++)
+	{
+		printf("ROOM %i: %s\n", i, rt->rooms[i]);
+		printf(" - %i players, %i tables\n", rt->players[i], rt->tables[i]);
+	}
+}
+
+void ggztop_read(int textonly)
+{
+	stats_rt *rt;
+
+	rt = (stats_rt*)shmat(stats_rt_shmid, 0, 0);
+
+	if(rt->version != STATS_RT_VERSION)
+	{
+		fprintf(stderr, "ggztop: wrong stats_rt version\n");
+		return;
+	}
+	if(rt->magicversion != sizeof(stats_rt))
+	{
+		fprintf(stderr, "ggztop: wrong stats_rt size\n");
+		return;
+	}
+
+	if(textonly)
+		ggztop_display_text(rt);
+	else
+		ggztop_display_top(rt);
+
+	shmdt(rt);
+}
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	int textonly = 0;
+
+	if((argc == 2) && (!strcmp(argv[1], "-h")))
+	{
+		printf("ggztop [-t]\n");
+		printf(" -t: bare text output, no loop\n");
+		return 0;
+	}
+
+	if((argc == 2) && (!strcmp(argv[1], "-t")))
+		textonly = 1;
+
+	ret = ggztop_init();
+	if(!ret)
+		return -1;
+
+	ggztop_read(textonly);
+
+	return 0;
+}
+
---
+++ playground/server/ggztop/statsrt.h
@@ -0,0 +1,25 @@
+#ifndef GGZ_STATS_RT_H
+#define GGZ_STATS_RT_H
+
+#define STATS_RT_SHMID            0x5688
+#define STATS_RT_VERSION          1
+
+#define STATS_RT_MAX_ROOMS        100
+#define STATS_RT_MAX_ROOMNAME_LEN 32
+
+struct stats_rt_t
+{
+	int version;
+	int magicversion;
+
+	int uptime;
+	int num_rooms;
+	int num_players;
+	int num_tables;
+	char rooms[STATS_RT_MAX_ROOMS][STATS_RT_MAX_ROOMNAME_LEN];
+	int players[STATS_RT_MAX_ROOMS];
+	int tables[STATS_RT_MAX_ROOMS];
+};
+typedef struct stats_rt_t stats_rt;
+
+#endif
