63128c7fde1d3404d4e5052a65b09cc6f555901b
josef <devnull@localhost>
2008-01-19 02:36:27 -0800

    [svn] - improve the robustness of the translatable text extraction tool, in turn making the perl regexps harder to read - merge the Swedish menu translations into a .po file so the separate menu template can be removed

--- community/web/common/po/extract-php.pl
+++ community/web/common/po/extract-php.pl
@@ -11,18 +11,21 @@ my %msgids;
 my $regexp = qr/\_\_\(\"([^\"\)]*)\"\)/;
 
 for my $i(0..$#ARGV){
+	my $line = 1;
 	open(F, $ARGV[$i]);
 	while(<F>){
 		my $s = $_;
 		my @words = ($s =~ /$regexp/g);
 		for $s(@words){
 			if(!$msgids{$s}){
+				print "# PHP: $ARGV[$i]:$line\n";
 				print "msgid \"$s\"\n";
 				print "msgstr \"\"\n";
 				print "\n";
 				$msgids{$s} = 1;
 			}
 		}
+		$line++;
 	}
 	close(F);
 }
--- community/web/common/po/extract.pl
+++ community/web/common/po/extract.pl
@@ -1,34 +1,49 @@
 #!/usr/bin/env perl
 #
 # Extract all _(x) values from *.inc(.*) files.
+# Can now cope with _(x (y)) as well.
 
 use strict;
 
 my ($s, $tag);
 my %msgids;
 
+print "# Pseudo-entry to force the correct encoding\n";
 print "msgid \"\"\n";
 print "msgstr \"Content-Type: text/plain; charset=UTF-8\\n\"\n";
 print "\n";
 
-my $tagexp = qr/[^\_]\_\(([^\)]*\)\([^\)]*)\)/;
-my $regexp = qr/[^\_]\_\(([^\)]*)\)/;
+my $tagexp = qr/(?:[^\_]|^)\_\((?:[^\)]*)\)\((?:[^\)]*)\)/;
+my $regexp = qr/(?:[^\_]|^)\_\((?:[^\)]*)\)/;
+my $regexpparens = qr/(?:[^\_]|^)\_\((?:[^\(\)]*)\((?:[^\)]*)\)(?:[^\)]*)\)/;
+my $finalexp = qr/[^\_]?\_\(((?:[^\(\)]*)\(?(?:[^\)]*)\)?(?:[^\(\)]*))\)(?:\(([^\)]*)\))?/;
 
 for my $i(0..$#ARGV){
 	open(F, $ARGV[$i]);
+	my $line = 1;
 	while(<F>){
 		my $s = $_;
 		my @taggedwords = ($s =~ /$tagexp/g);
-		$s =~ s/$tagexp//;
+		$s =~ s/$tagexp//g;
+		my @wordsparens = ($s =~ /$regexpparens/g);
+		$s =~ s/$regexpparens//g;
 		my @words = ($s =~ /$regexp/g);
-		for $s(@words, @taggedwords){
+		for $s(@wordsparens, @words, @taggedwords){
+			$s =~ s/^[^\_]//;
 			if(!$msgids{$s}){
-				print "msgid \"$s\"\n";
+				my ($word, $tag) = ($s =~ /$finalexp/);
+				print "# Template: $ARGV[$i]:$line\n";
+				if($tag){
+					print "# word: $word, tag: $tag\n";
+					$word = "$word($tag)";
+				}
+				print "msgid \"$word\"\n";
 				print "msgstr \"\"\n";
 				print "\n";
 				$msgids{$s} = 1;
 			}
 		}
+		$line++;
 	}
 	close(F);
 }
