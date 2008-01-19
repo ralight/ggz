3246c553bc279bb3aaabde42841a94f586c5c874
josef <devnull@localhost>
2008-01-19 04:27:02 -0800

    [svn] - bump copyright from 2006 to 2008 - move left navigation bar down so the civ logo becomes fully visible (long live css!)

--- community/web/common/acknowledgements.inc.sv
+++ community/web/common/acknowledgements.inc.sv
@@ -1,10 +1,13 @@
 <div id="acknowledgements">
 	<div class="note" style="float:left;">
-		&copy; 2005, 2006 <a href="http://www.ggzgamingzone.org/">GGZ Gaming Zone</a>
+		&copy; 2005 - 2008 <a href="http://www.ggzgamingzone.org/">GGZ Gaming Zone</a>
 	</div>
 	<div class="note" style="float:right;">
 		Design baserad på Steel-n-Leather &copy; 2004 av
 		<a href="mailto:brian.allbee@gmail.com" title="Comment on this design if you wish!">Brian D. Allbee</a>
+		<br>
+		_(Portal engine powered by GGZ Community) &copy; 2005 - 2008 by
+		<a href="http://www.ggzgamingzone.org/">GGZ Gaming Zone</a>
 	</div>
 	<div class="note" style="float:left;">
 	<?php
