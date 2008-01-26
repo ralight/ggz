d848a05fb2746ac80eb4af4d8642685756545a17
josef <devnull@localhost>
2008-01-26 01:28:51 -0800

    [svn] - synopsis of fortune script - first grubby script to be both embeddable and executable as an external script

--- grubby/grubby/data/fortune.py
+++ grubby/grubby/data/fortune.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
+#
+# Commands:
+# guru joke
+# guru fortune
+# guru fortune linux
+# guru fortune de/mathematiker
 
-import grubby
+try:
+	import grubby
+	embedded = True
+except:
+	embedded = False
 import os
 
-input = grubby.answer
-grubby.answer = ""
+if embedded:
+	input = grubby.answer
+else:
+	input = raw_input("")
+answer = ""
 
 inp = input.split(" ")
 if len(inp) == 2 or len(inp) == 3:
 	command = inp[1]
-	category = "riddles"
+	category = ""
 	if len(inp) == 3:
 		category = inp[2]
 	if command == "joke" or command == "fortune":
@@ -21,5 +34,10 @@ if len(inp) == 2 or len(inp) == 3:
 		for line in lines:
 			xlines += line
 
-		grubby.answer = xlines
+		answer = xlines
+
+if embedded:
+	grubby.answer = answer
+else:
+	print answer
 
