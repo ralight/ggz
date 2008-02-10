e64a8cb23a7e2f281af55c39d9c490ce15f25d26
josef <devnull@localhost>
2008-02-10 00:59:29 -0800

    [svn] - use ggzsettings if available - test results with several games, both from GGZ and external ones

--- playground/ggz-python/launcher/test.sh
+++ playground/ggz-python/launcher/test.sh
@@ -2,5 +2,17 @@
 
 ggzdir=`ggz-config -c`/..
 
-./ggz-faketable $ggzdir/lib/ggzd/ggzd.tictactoe $ggzdir/lib/ggz/ggz.ttt
+# Work fine, more or less:
+#./ggz-faketable $ggzdir/lib/ggzd/ggzd.tictactoe $ggzdir/lib/ggz/ggz.ttt
+#./ggz-faketable $ggzdir/lib/ggzd/ggzd.dots $ggzdir/lib/ggz/ggz.dots
+#./ggz-faketable $ggzdir/lib/ggzd/ggzd.connectx $ggzdir/bin/ggzboard
+
+# Don't work for some reason:
+#./ggz-faketable $ggzdir/lib/ggzd/ggzd.ggzcards $ggzdir/lib/ggz/ggz.ggzcards
+# Reason: unknown
+#./ggz-faketable $ggzdir/bin/civserver $ggzdir/bin/civclient
+# Reason: (<errorsys>) Unable to create file (null)/.ggz/ggz-gtk.rc
+
+# Test this one:
+./ggz-faketable /usr/games/iagnod /usr/games/iagno
 
