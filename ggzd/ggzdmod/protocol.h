5ec0a95bf4359b6633409d9c2be50a6ceddd074a
josef <devnull@localhost>
2008-02-10 10:26:36 -0800

    [svn] - documentation overhaul of ggzdmod's protocol file similar to how it was done for ggzmod - also, elimination of a couple of typos

--- ggzd/ggzdmod/protocol.h
+++ ggzd/ggzdmod/protocol.h
@@ -1,12 +1,8 @@
 /*
- * File: protocols.h
- * Author: Brent Hendricks
- * Project: GGZ
- * Date: 10/18/99
- * Desc: Protocol enumerations, etc.
- * $Id: protocol.h 9665 2008-02-09 17:36:35Z josef $
+ * GGZDMOD - C implementation of the GGZ server-server protocol
  *
- * Copyright (C) 1999 Brent Hendricks.
+ * Copyright (C) 1999 Brent Hendricks
+ * Copyright (C) 2000 - 2008 GGZ Gaming Zone Development Team
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -23,136 +19,161 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-
-#ifndef __GGZ_SERVER_PROTOCOL_H
-#define __GGZ_SERVER_PROTOCOL_H
+#ifndef GGZDMOD_SERVER_PROTOCOL_H
+#define GGZDMOD_SERVER_PROTOCOL_H
 
 /*
- * Protocols for GGZDMOD game server <-> ggzd communication.
+ * Protocol opcode enumerations for the GGZDMOD game server (module) to
+ * GGZ server (ggzd) communication.
+ * This file is equally used by ggzdmod-ggz (ggzd) and ggzdmod-game.
+ * Have a look at the GGZ server-server protocol specification for details
+ * on the messages, their flow and other considerations.
  */
 
-/** Messages sent from the game server to the ggz server. */
+/** Messages sent from the game server to the GGZ server. */
 typedef enum {
-	/** @brief Signals the start of a MSG_LOG packet.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing MSG_LOG.
-	 *    - An easysock-formatted string.
-	 *  The packet may be sent at any time by the game server.  It
-	 *  tells ggzd to log the message string.
+	/* Signals that a message from the game server is to be logged
+	 * by ggzd.
+	 * The packet is composed of:
+	 * - An integer containing MSG_LOG
+	 * - An easysock-formatted string containing the log message
 	 */
 	MSG_LOG,
 
-	/** @brief Signals the start of a REQ_GAME_STATE packet.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing REQ_GAME_STATE.
-	 *    - A single byte (char) containing the new game state.
-	 *      The state is a GGZdModState enumerated value.
-	 *  This packet tells the server of a change in game state.
-	 *  The server will send a RSP_GAME_STATE packet in response.
+	/* Tells the server of a needed change in game state.
+	 * The packet is composed of:
+	 * - An integer containing REQ_GAME_STATE.
+	 * - A single byte (char) containing the new game state, which
+	 *   is a GGZdModState enumerated value
+	 * The server will send a RSP_GAME_STATE packet in response.
 	 */
 	REQ_GAME_STATE,
 
+	/* Tells the server to modify the number of seats at the table.
+	 * The packet is composed of:
+	 * - an integer containing REQ_NUM_SEATS
+	 * - an integer containing the number of seats needed for the table
+	 */
 	REQ_NUM_SEATS,
+
+	/* Tells the server to boot a player from the table.
+	 * The packet is composed of:
+	 * - an integer containing REQ_BOOT
+	 * - an easysock-formatted string containing the name of the player
+	 */
 	REQ_BOOT,
+
+	/* Tells the server to put a bot into an empty seat.
+	 * The packet is composed of:
+	 * - an integer containing REQ_BOT
+	 * - an integer with the seat number where to put the bot
+	 */
 	REQ_BOT,
+
+	/* Tells the server to open up a seat currently occupied by a bot.
+	 * The packet is composed of:
+	 * - an integer containing REQ_OPEN
+	 * - an integer with the seat number of the affected seat
+	 */
 	REQ_OPEN,
 
-	/** @brief Signals the start of a MSG_GAME_REPORT packet.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing MSG_GAME_REPORT.
-	 *    - An integer for the number of players.
-	 *    - For each player:
-	 *      - An easysock-formatted string for the name.
-	 *      - An integer containing the player type.
-	 *      - An integer containing the team number.
-	 *        (If no teams are defined, each player represents
-	 *         a team on his own.)
-	 *      - An integer containing the result, e.g. win or loss.
-	 *      - An integer containing the score.
-	 *  This packet transmits game results and statistics.
+	/* Signals the start of a MSG_GAME_REPORT packet which transmits game
+	 * results and statistics.
+	 * The packet is composed of:
+	 * - An integer containing MSG_GAME_REPORT
+	 * - An integer for the number of players
+	 * > For each player:
+	 *   - An easysock-formatted string for the name
+	 *   - An integer containing the player type
+	 *   - An integer containing the team number
+	 *     If no teams are defined, each player represents
+	 *     a team on his own.
+	 *   - An integer containing the result, e.g. win or loss
+	 *   - An integer containing the score
 	 */
 	MSG_GAME_REPORT,
+
+	/* Informs the server about the location for a potentially continuous
+	 * savegame file for the currently running game.
+	 * The packet is composed of:
+	 * - an integer containing MSG_SAVEGAME_REPORT
+	 * - an easysock-formatted string containing the path of the file
+	 */
 	MSG_SAVEGAME_REPORT
 } TableToControl;
 
 /** Messages sent from the ggz server to the game server. */
 typedef enum {
-	/** @brief Signals the start of a MSG_GAME_LAUNCH packet.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing MSG_GAME_LAUNCH.
-	 *    - A string containing the game's name
-	 *    - An integer containing the number of seats at the table.
-	 *    - An integer containing the number of spectators
-	 *    - Seat data for every seat at the table:
-	 *      - An integer containing the seat type (GGZSeatType).
-	 *        Only GGZ_SEAT_OPEN, GGZ_SEAT_RESERVED and GGZ_SEAT_BOT
-	 *        are allowed.
-	 *      - If the seat type is GGZ_SEAT_RESERVED or GGZ_SEAT_BOT,
-	 *        the name of the player at the seat (as an easysock-
-	 *        formatted string).
-	 *  This packet is sent to the table shortly after the table
-	 *  executable has been launched.  GGZdMod-game, on receiving
-	 *  this packet, updates its internal data and changes the
-	 *  state from CREATED to WAITING.  It then notifies the table
-	 *  and GGZ of these changes.
+	/* Signals the start of a MSG_GAME_LAUNCH packet which is sent
+	 * to the table shortly after the game server executable has been
+	 * launched. GGZdMod-game, on receiving this packet, updates its
+	 * internal data and changes the state from CREATED to WAITING.  It
+	 * then notifies the table and the GGZ server of these changes.
+	 * The packet is composed of:
+	 *  - An integer containing MSG_GAME_LAUNCH
+	 *  - A string containing the game's name
+	 *  - An integer containing the number of seats at the table
+	 *  - An integer containing the number of spectators
+	 *  > Seat data for every seat at the table:
+	 *    - An integer containing the seat type (GGZSeatType)
+	 *      Only GGZ_SEAT_OPEN, GGZ_SEAT_RESERVED and GGZ_SEAT_BOT
+	 *      are allowed.
+	 *    - If the seat type is GGZ_SEAT_RESERVED or GGZ_SEAT_BOT,
+	 *      the name of the player at the seat (as an easysock-
+	 *      formatted string).
 	 */
 	MSG_GAME_LAUNCH,
 
-	/** @brief Signals the start of a REQ_GAME_SEAT packet.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing MSG_GAME_SEAT.
-	 *    - An integer containing the seat number of the changed seat.
-	 *    - An integer containing the GGZ_SEAT_TYPE.
-	 *    - An easysock-formatted string contining the player's name,
-	 *      or "" if the player has no name.
-	 *    - An easysock-formatted FD containing the FD for the
-	 *      player's client socket, IF the seat is a player.
-	 *      Note: This file descriptor carries ancillary data
-	 *      and cannot be read as an integer value.
-	 *  This packet is sent any time a seat is modified.  It gives
-	 *  the table all of the information it needs about the new
-	 *  seat.
+	/* Signals the start of a REQ_GAME_SEAT packet which is sent every
+	 * time a seat is modified. It gives the table all of the information
+	 * it needs about the new seat.
+	 * The packet is composed of:
+	 *  - An integer containing MSG_GAME_SEAT
+	 *  - An integer containing the seat number of the changed seat
+	 *  - An integer containing the GGZ_SEAT_TYPE
+	 *  - An easysock-formatted string contining the player's name,
+	 *    or "" if the player has no name.
+	 *  - A file descriptor (as integer with ancillary data) containing
+	 *    the already opened connection to the player's client socket,
+	 *    iff the seat is occupied by a player.
+	 *    Note: This file descriptor carries ancillary data
+	 *    and cannot be read as an integer value.
 	 */
 	MSG_GAME_SEAT,
 
-	/** @brief Signals the start of a MSG_GAME_SPECTATOR_SEAT packet.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing MSG_GAME_SPECTATOR_SEAT.
-	 *    - An integer containing the spectator seat number of the
-	 *      changed spectator.
-	 *    - An easysock-formatted string containing the spectator's name,
-	 *      or "" for an empty seat.
-	 *    - An easysock-formatted FD containing thee FD for the
-	 *      spectator's client socket, IF the spectator seat is occupied.
+	/* Signals the start of a MSG_GAME_SPECTATOR_SEAT packet.
+	 * The packet is composed of:
+	 *  - An integer containing MSG_GAME_SPECTATOR_SEAT
+	 *  - An integer containing the spectator seat number of the
+	 *    changed spectator
+	 *  - An easysock-formatted string containing the spectator's name,
+	 *    or "" for an empty seat
+	 *  - A file descriptor (as integer with ancillary data) containing
+	 *    the already opened connection to the spectator's client socket.
+	 *    Note: This file descriptor carries ancillary data
+	 *    and cannot be read as an integer value.
 	 */
 	MSG_GAME_SPECTATOR_SEAT,
 
-	/** @brief Signals a player changing seats.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing MSG_GAME_RESEAT.
-	 *    - An integer, boolean, whether the player *was* a spectator.
-	 *    - An integer, the (spectator) seat the player *was* at.
-	 *    - An integer, boolean, whether the player *will be* a spectator.
-	 *    - An integer, the (speectator) seat the player *will be* at.
+	/* Signals a player changing seats.
+	 * The packet is composed of:
+	 *  - An integer containing MSG_GAME_RESEAT.
+	 *  - An integer, boolean, whether the player *was* a spectator.
+	 *  - An integer, the (spectator) seat the player *was* at.
+	 *  - An integer, boolean, whether the player *will be* a spectator.
+	 *  - An integer, the (spectator) seat the player *will be* at.
 	 */
 	MSG_GAME_RESEAT,
 
-	/** @brief Signals the start of a RSP_GAME_STATE packet.
-	 *
-	 *  The packet is composed of:
-	 *    - An integer containing RSP_GAME_STATE.
+	/* Signals the start of a RSP_GAME_STATE packet, which is sent in
+	 * response to a REQ_GAME_STATE. Its only effect is to let the table
+	 * know the information has been received.
+	 * The packet is composed of:
+	 *   - An integer containing RSP_GAME_STATE.
 	 *  This packet is sent in response to a REQ_GAME_STATE.  It's
 	 *  only effect is to let the table know the information has
-	 *  been received.
 	 */
 	RSP_GAME_STATE,
 } ControlToTable;
 
-#endif /* __GGZ_SERVER_PROTOCOL_H */
+#endif /* GGZDMOD_SERVER_PROTOCOL_H */
