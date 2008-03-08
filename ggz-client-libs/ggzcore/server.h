3906a04d6b95ab1eec9596bb353209b34165671e
josef <devnull@localhost>
2008-03-07 23:32:10 -0800

    [svn] - use the correct (versioned) catalogue for looking up messages

--- ggz-client-libs/ggzcore/server.h
+++ ggz-client-libs/ggzcore/server.h
@@ -3,7 +3,7 @@
  * Author: Brent Hendricks
  * Project: GGZ Core Client Lib
  * Date: 1/19/01
- * $Id: server.h 9570 2008-01-20 10:47:37Z josef $
+ * $Id: server.h 9784 2008-03-08 07:32:10Z josef $
  *
  * Code for handling server connection state and properties
  *
@@ -37,7 +37,7 @@
 #include <locale.h>
 #include <libintl.h>
 #define N_(x) (x)
-#define _(x) dgettext("ggzcore", x)
+#define _(x) dgettext("ggzcore_" PACKAGE_VERSION, x)
 
 GGZServer *_ggzcore_server_new(void);
 
