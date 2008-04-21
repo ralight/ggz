3d0a5401824ef1a1173a00bd5e286931681d7645
oojah <devnull@localhost>
2008-04-21 02:08:24 -0700

    [svn] Set lastupdate to "now" when starting up.

--- playground/server/ggztop/ggztop.c
+++ playground/server/ggztop/ggztop.c
@@ -393,6 +393,7 @@ int main(int argc, char *argv[])
 	if((argc == 2) && (!strcmp(argv[1], "-x")))
 		runmode = RUNMODE_XMLONLY;
 
+	lastupdate = time(NULL);
 	ret = ggztop_init();
 	if(!ret)
 		return -1;
