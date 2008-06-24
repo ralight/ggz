a8cf7fd25bce34a527c41536991e2df8b80dd59a
josef <devnull@localhost>
2008-06-24 13:04:58 -0700

    [svn] - fix function to really reset the pass-by-reference parameter to NULL on empty messages - this solves the nasty spectator-related bug that we saw, and probably others - however, since it comes from r9691, it's unlikely to have caused all the odo crashes!

--- libggz/src/easysock.c
+++ libggz/src/easysock.c
@@ -3,7 +3,7 @@
  * Author: Brent Hendricks
  * Project: libeasysock
  * Date: 4/16/98
- * $Id: easysock.c 10060 2008-06-24 17:26:10Z oojah $
+ * $Id: easysock.c 10062 2008-06-24 20:04:58Z josef $
  *
  * A library of useful routines to make life easier while using 
  * sockets
@@ -709,7 +709,7 @@ int ggz_read_string_alloc_null(const int sock, char **message)
 
 	ret = ggz_read_string_alloc(sock, message);
 	if (ret == 0)
-		if (message[0] == '\0')
+		if (*message[0] == '\0')
 		{
 			ggz_free(*message);
 			*message = NULL;
