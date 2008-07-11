488838ef7b5f87e15e1ea8ed26e1abb724470493
josef <devnull@localhost>
2008-07-11 13:06:44 -0700

    [svn] - ability to process multiple files at once - ignore ggz_attribute forcefully since ctags-exuberant gets confused by it

--- playground/maintenance/makesymbols.sh
+++ playground/maintenance/makesymbols.sh
@@ -3,20 +3,22 @@
 # Generates symbol map from function declarations in header files
 # which can be used with --Wl,--version-script=linker.map to restrict
 # list of exported symbols to those in the header file.
-#
-# FIXME: only one header can be specified right now, i.e. not
-# ggz.h and ggz_common.h at the same time!
 
 if [ -z $1 ]; then
-	echo "Syntax: makesymbols.sh <headerfile>" >&2
+	echo "Syntax: makesymbols.sh <headerfile(s)>" >&2
 	exit 1
 fi
 
-header=$1
+symbols=""
+lb=`echo -e '\n '`
+while [ ! -z $1 ]; do
+	header=$1
+	newsymbols=`ctags-exuberant -x -I ggz__attribute --c-kinds=p $1 | cut -d " " -f 1`
+	symbols="$symbols$lb$newsymbols"
+	shift
+done
 
 rm -f linker.map
-symbols=`ctags-exuberant -x --fields=t --c-kinds=p $1 | cut -d " " -f 1`
-
 echo "VERSION {" >> linker.map
 echo "global:" >> linker.map
 for symbol in $symbols; do
