ddfd833f1982a51303dba1800a21b7973828912d
josef <devnull@localhost>
2008-08-15 05:37:39 -0700

    [svn] - some fake bots and buddies

--- playground/ggz-kde4/kggzlib-tests/table-config/main.cpp
+++ playground/ggz-kde4/kggzlib-tests/table-config/main.cpp
@@ -17,6 +17,11 @@ int main(int argc, char **argv)
 
 	TableConfiguration dlg(NULL);
 	dlg.initLauncher("josef", 10, 5);
+	dlg.addGrubby("Grubby");
+	dlg.addBot("Bot 1", "easy");
+	dlg.addBot("Bot 2", "hard");
+	dlg.addBuddy("Buddy 1");
+	dlg.addBuddy("Buddy 2");
 	dlg.exec();
 
 	return a.exec();
