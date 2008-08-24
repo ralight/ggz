d1c437aa63095f1ad478ee6f92b5dcbb0941bdbc
josef <devnull@localhost>
2008-08-24 05:01:40 -0700

    [svn] - configure dialogue by passing it a fake GGZ gametype object

--- playground/ggz-kde4/kggzlib-tests/table-config/main.cpp
+++ playground/ggz-kde4/kggzlib-tests/table-config/main.cpp
@@ -1,6 +1,9 @@
 // Connection dialog includes
 #include <kggzlib/tabledialog.h>
 
+// KGGZ includes
+#include <kggzcore/gametype.h>
+
 // Qt includes
 #include <qapplication.h>
 
@@ -14,13 +17,21 @@ int main(int argc, char **argv)
 
 	KComponentData data("tabledialog");
 
-	TableDialog dlg(NULL);
-	/*dlg.initLauncher("josef", 10, 5);
-	dlg.addGrubby("Grubby");
-	dlg.addBot("Bot 1", "easy");
-	dlg.addBot("Bot 2", "hard");
+	KGGZCore::GameType gametype;
+
+	gametype.setMaxPlayers(10);
+	gametype.setMaxBots(5);
+	gametype.addNamedBot("Bot 1", "easy");
+	gametype.addNamedBot("Bot 2", "hard");
+	// FIXME: buddies and grubbies
+
+	/*dlg.addGrubby("Grubby");
 	dlg.addBuddy("Buddy 1");
 	dlg.addBuddy("Buddy 2");*/
+
+	TableDialog dlg(NULL);
+	dlg.setGameType(gametype);
+	dlg.setIdentity("josef");
 	return dlg.exec();
 }
 
