b64e35223781adb3b09fe94f068a9685a05d076f
josef <devnull@localhost>
2008-12-30 01:10:57 -0800

    [svn] - improve cmake macros to include hooks for game server .dsc/.room installation - more understandable advice in case of prefix conflicts

--- playground/starterpacks/ggz-starterpack-c++/common/GGZ.cmake
+++ playground/starterpacks/ggz-starterpack-c++/common/GGZ.cmake
@@ -4,7 +4,8 @@
 
 ###########################################################
 # Documentation
-# - call register_ggz_module(module.dsc) in your game's CMakeLists.txt
+# - call register_ggz_module(module.dsc) in your game client's CMakeLists.txt
+# - use GGZ_{GAME/ROOM}DESC_INSTALL_DIR in your game server's CMakeLists.txt
 # - set -DGGZ_NOREGISTRY=/usr/share/ggz/modules/kdegames when building binary packages
 #   (mostly obsolete now)
 # - set -DGGZ_FORCEREGISTRY=1 to force registration in out-of-prefix installations
@@ -20,7 +21,7 @@ if(GGZCONFIG_EXECUTABLE)
     MESSAGE(STATUS "GGZ: Success: ggz-config was found as ${GGZCONFIG_EXECUTABLE}.")
 else(GGZCONFIG_EXECUTABLE)
     MESSAGE(STATUS "GGZ: Warning: ggz-config not found, game modules will not be registered!")
-    MESSAGE(STATUS " Source package containing ggz-config: 'ggz-client-libs'")
+    MESSAGE(STATUS " Source package containing ggz-config: 'ggz-base-libs'")
     MESSAGE(STATUS " Binary package containing ggz-config: 'ggzcore-bin' (or similar)")
 endif(GGZCONFIG_EXECUTABLE)
 
@@ -34,6 +35,14 @@ endif(GGZCONFIG_EXECUTABLE)
 # FIXME: Bug in cmake? REG needs to be declared whenever
 #        -DGGZ_NOREGISTRY is given. However, why is that, especially since
 #        the error occurs in the 'else' tree which isn't used in that case.
+# FIXME: should we promote KDE4_PREFIX to FindKDE4Internal.cmake?
+
+exec_program(${KDE4_KDECONFIG_EXECUTABLE} ARGS "--prefix" OUTPUT_VARIABLE KDE4_PREFIX)
+if(NOT KDE4_PREFIX STREQUAL "${CMAKE_INSTALL_PREFIX}")
+    MESSAGE(STATUS "GGZ: Warning: Out-of-KDE-prefix installation, make sure to include ${KDE4_PREFIX} in $KDEDIRS at runtime.")
+    MESSAGE(STATUS " KDE Games prefix: ${KDE4_PREFIX}")
+    MESSAGE(STATUS " Prefix for this package: ${CMAKE_INSTALL_PREFIX}")
+endif(NOT KDE4_PREFIX STREQUAL "${CMAKE_INSTALL_PREFIX}")
 
 if(GGZCONFIG_FOUND)
     if(GGZ_NOREGISTRY)
@@ -42,24 +51,30 @@ if(GGZCONFIG_FOUND)
         MESSAGE(STATUS "GGZ: Installing game module information to ${GGZ_NOREGISTRY}.")
         MESSAGE(STATUS "GGZ: Post-registration hooks need to be called by distributors.")
     else(GGZ_NOREGISTRY)
-        EXECUTE_PROCESS(COMMAND ${GGZCONFIG_EXECUTABLE} -c OUTPUT_VARIABLE REG)
+        EXECUTE_PROCESS(COMMAND ${GGZCONFIG_EXECUTABLE} --configdir OUTPUT_VARIABLE REG)
         STRING(REPLACE "\n" "" REG "${REG}")
         if(SYSCONF_INSTALL_DIR STREQUAL "${REG}")
             MESSAGE(STATUS "GGZ: Registering game module information into ${REG}.")
         else(SYSCONF_INSTALL_DIR STREQUAL "${REG}")
             if(GGZ_FORCEREGISTRY)
-              MESSAGE(STATUS "GGZ: Forced registration into ${REG}.")
+                MESSAGE(STATUS "GGZ: Forced registration into ${REG}.")
             else(GGZ_FORCEREGISTRY)
-              set(SKIP_GGZCONFIG TRUE BOOL)
-              MESSAGE(STATUS "GGZ: Warning: Out-of-prefix installation, no registration unless GGZ_FORCEREGISTRY is given.")
-              MESSAGE(STATUS " KDE Games configuration gets installed into: ${SYSCONF_INSTALL_DIR}")
-              MESSAGE(STATUS " GGZ configuration is expected in: ${REG}")
+                set(SKIP_GGZCONFIG TRUE BOOL)
+                MESSAGE(STATUS "GGZ: Warning: Out-of-GGZ-prefix installation, no registration unless -DGGZ_FORCEREGISTRY=1 is given.")
+                MESSAGE(STATUS " KDE Games configuration gets installed into: ${SYSCONF_INSTALL_DIR}")
+                MESSAGE(STATUS " GGZ configuration is expected in: ${REG}")
             endif(GGZ_FORCEREGISTRY)
         endif(SYSCONF_INSTALL_DIR STREQUAL "${REG}")
     endif(GGZ_NOREGISTRY)
 else(GGZCONFIG_FOUND)
     set(SKIP_GGZCONFIG TRUE BOOL)
 endif(GGZCONFIG_FOUND)
+
+if(NOT EXISTS "${SYSCONF_INSTALL_DIR}/ggzd/ggzd.motd")
+    MESSAGE(STATUS "GGZ: Warning: No GGZ server found in ${SYSCONF_INSTALL_DIR}/ggzd, game server files may not be found.")
+endif(NOT EXISTS "${SYSCONF_INSTALL_DIR}/ggzd/ggzd.motd")
+set(GGZ_GAMEDESC_INSTALL_DIR "${SYSCONF_INSTALL_DIR}/ggzd/games")
+set(GGZ_ROOMDESC_INSTALL_DIR "${SYSCONF_INSTALL_DIR}/ggzd/rooms")
 
 if(SKIP_GGZCONFIG)
     macro(register_ggz_module MODFILE)
