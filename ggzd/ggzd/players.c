f38619d31ae1cff8df1fb7e6a0adb325871c978e
oojah <devnull@localhost>
2009-01-01 17:51:10 -0800

    [svn] Always use a format argument for printf style functions, even if the only other argument is a string.

--- ggzd/ggzd/players.c
+++ ggzd/ggzd/players.c
@@ -4,7 +4,7 @@
  * Project: GGZ Server
  * Date: 10/18/99
  * Desc: Functions for handling players
- * $Id: players.c 10635 2008-12-23 19:24:59Z josef $
+ * $Id: players.c 10774 2009-01-02 01:51:10Z oojah $
  *
  * Desc: Functions for handling players.  These functions are all
  * called by the player handler thread.  Since this thread is the only
@@ -974,7 +974,7 @@ GGZPlayerHandlerStatus player_table_info(GGZPlayer *player, int seat_num)
 
 	for (i = first; i < last; i++) {
 		if (table->seat_types[i] == GGZ_SEAT_PLAYER) {
-			snprintf(entry.handle, sizeof(entry.handle), player->name);
+			snprintf(entry.handle, sizeof(entry.handle), "%s", player->name);
 			status = ggzdb_player_get(&entry);
 
 			do_send = 1;
@@ -985,7 +985,7 @@ GGZPlayerHandlerStatus player_table_info(GGZPlayer *player, int seat_num)
 			if (status == GGZDB_NO_ERROR) {
 				realname = ggz_strdup(entry.name);
 
-				snprintf(extentry.handle, sizeof(extentry.handle), player->name);
+				snprintf(extentry.handle, sizeof(extentry.handle), "%s", player->name);
 				status = ggzdb_player_get_extended(&extentry);
 				if (status == GGZDB_NO_ERROR) {
 					photo = ggz_strdup(extentry.photo);
@@ -1585,7 +1585,7 @@ GGZPlayerHandlerStatus player_perms_admin(GGZPlayer *player,
 	if (ggz_perms_is_set(rcvr->perms, perm) != set) {
 		ggzdbPlayerEntry entry;
 
-		snprintf(entry.handle, sizeof(entry.handle), rcvr->name);
+		snprintf(entry.handle, sizeof(entry.handle), "%s", rcvr->name);
 		if (ggzdb_player_get(&entry) != GGZDB_NO_ERROR) {
 			pthread_rwlock_unlock(&rcvr->lock);
 			if (net_send_admin_result(player->client->net,
