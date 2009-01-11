0c12354de9349ffadb2dcbeac19e17b58dedcb8a
josef <devnull@localhost>
2009-01-11 07:12:28 -0800

    [svn] - new option --playmode to recognise what the player actually wants on a table

--- playground/server/ggzcoreconn/ggzcoreconn.c
+++ playground/server/ggzcoreconn/ggzcoreconn.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
 
 #include "ggzcore_mainloop.h"
 
@@ -15,24 +16,30 @@ static GGZCoreMainLoop parse(int argc, char **argv)
 		.reconnect = 0,
 		.debug = 0,
 		.loop = 0,
-		.func = NULL
+		.func = NULL,
+		.mode = GGZCORE_MAINLOOP_CHATONLY
 	};
 
 	struct option opts[] = {
-		{"tls", 0, &mainloop.tls, 1},
-		{"reconnect", 0, &mainloop.reconnect, 1},
-		{"async", 0, &mainloop.async, 1},
-		{"debug", 0, &mainloop.debug, 1},
-		{"loop", 0, &mainloop.loop, 1},
+		{"tls", no_argument, &mainloop.tls, 1},
+		{"reconnect", no_argument, &mainloop.reconnect, 1},
+		{"async", no_argument, &mainloop.async, 1},
+		{"debug", no_argument, &mainloop.debug, 1},
+		{"loop", no_argument, &mainloop.loop, 1},
+		{"playmode", required_argument, NULL, 'p'},
 		{0, 0, 0, }
 	};
 	int index;
 	int valid = 1;
 
+	char *playmode = NULL;
+
 	while(1) {
 		int ret = getopt_long(argc, argv, "", opts, &index);
 		if(ret == -1) {
 			break;
+		} else if(ret == 'p') {
+			playmode = optarg;
 		} else if(ret == 0) {
 			/* mainloop configuration */
 		} else {
@@ -47,12 +54,26 @@ static GGZCoreMainLoop parse(int argc, char **argv)
 		valid = 0;
 	}
 
+	if(!playmode) {
+		/* use defaults */
+	} else if(!strcmp(playmode, "launch")) {
+		mainloop.mode = GGZCORE_MAINLOOP_LAUNCH;
+	} else if(!strcmp(playmode, "join")) {
+		mainloop.mode = GGZCORE_MAINLOOP_JOIN;
+	} else if(!strcmp(playmode, "spectate")) {
+		mainloop.mode = GGZCORE_MAINLOOP_SPECTATE;
+	} else {
+		fprintf(stderr, "ERROR: Invalid playmode '%s'.\n", playmode);
+		valid = 0;
+	}
+
 	if(!valid) {
-		fprintf(stderr, "Syntax: ggzcoreconn [--tls] [--reconnect] [--async] [--loop] [--debug] <GGZ URI>\n");
+		fprintf(stderr, "Syntax: ggzcoreconn [--tls] [--reconnect] [--async] [--loop] [--debug] [--playmode <mode>] <GGZ URI>\n");
+		fprintf(stderr, "Play modes: launch, join, spectate\n");
 		mainloop.uri = NULL;
 	} else {
-		printf("Connection: tls=%i async=%i reconnect=%i loop=%i uri=%s\n",
-			mainloop.tls, mainloop.async, mainloop.reconnect, mainloop.loop, mainloop.uri);
+		printf("Connection: tls=%i async=%i reconnect=%i loop=%i uri=%s mode=%s\n",
+			mainloop.tls, mainloop.async, mainloop.reconnect, mainloop.loop, mainloop.uri, playmode);
 	}
 
 	return mainloop;
