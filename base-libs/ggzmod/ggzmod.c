bbb50a920601a5b42f0378416a72a997db8636d3
nasenbaer <devnull@localhost>
2009-06-21 13:28:13 -0700

    [svn] change my last commit for using ggz_free and ggz_strbuild

--- base-libs/ggzmod/ggzmod.c
+++ base-libs/ggzmod/ggzmod.c
@@ -148,11 +148,9 @@ static char *ggz_getenv(const char *name)
 static void ggz_setenv(const char *name, const char *value)
 {
 #ifdef WIN32
-char *str = ggz_malloc(strlen(name) + 1 + strlen(value) + 1);
-sprintf(str, "%s=%s", name, value);
-if (putenv(str) != EXIT_SUCCESS) {
-	free(str);
-}
+char * str = ggz_strbuild("%s=%s", name, value)
+if (putenv(str) != EXIT_SUCCESS)
+	ggz_free(str);
 #else
 #ifdef HAVE_SETENV
 	setenv(name, value, 1);
